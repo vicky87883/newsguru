daa1c2e168630892717920fcb84f8ab558e7434a
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-05-31 20:07:21 +0530

    commit updated

---
+++ china-counters-indias-rafale-with-stealth-jet-deployment-near-sikkim.php
@@ -0,0 +1,315 @@
+<?php
+require_once('dbcon.php');
+$query = "SELECT * FROM `frontload`   ORDER BY `id` DESC;";
+$query2 = "SELECT * FROM `mostpopular`   ORDER BY `id` DESC;";
+$query3 = "SELECT * FROM `dontmiss`   ORDER BY `id` DESC;";
+$query4 = "SELECT * FROM `toptrending`   ORDER BY `id` DESC;";
+$result = mysqli_query($con,$query);
+$result2 = mysqli_query($con,$query2);
+$result3 = mysqli_query($con,$query3);
+$result4 = mysqli_query($con,$query4);
+?><!DOCTYPE html>
+<html class=no-js lang=en>
+<head>
+<meta charset=utf-8>
+<meta http-equiv=x-ua-compatible content="ie=edge">
+<title>China Uses a Stealth Jet to Counter India's Rafale in the Sikkim Area</title>
+<meta name=news_keywords content="China Uses a Stealth Jet to Counter India's Rafale in the Sikkim Area" itemprop="keywords"/>
+<meta name=description content="China Uses a Stealth Jet to Counter India's Rafale in the Sikkim Area" itemprop="description"/>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<link rel=canonical href="https://www.newsguru.live/china-counters-indias-rafale-with-stealth-jet-deployment-near-sikkim.php" />
+<link rel="shortcut icon" type=image/x-icon href=assets/imgs/favicon.svg>
+<link rel=stylesheet href=assets/css/style.css>
+<link rel=stylesheet href=assets/css/widgets.css>
+<link rel=stylesheet href=assets/css/color.css>
+<link rel=stylesheet href=assets/css/responsive.css>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V6HH2RKGTW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V6HH2RKGTW');
+</script>
+</head>
+<body>
+<div id=preloader-active>
+<div class="preloader d-flex align-items-center justify-content-center">
+<div class="preloader-inner position-relative">
+<div class=text-center>
+<img class="jump mb-50" src=assets/imgs/loading.svg alt>
+<h6>Now Loading</h6>
+<div class=loader>
+<div class="bar bar1"></div>
+<div class="bar bar2"></div>
+<div class="bar bar3"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class=main-wrap>
+<aside id=sidebar-wrapper class="custom-scrollbar offcanvas-sidebar position-right">
+<button class=off-canvas-close><i class=ti-close></i></button>
+<div class=sidebar-inner>
+<div class="siderbar-widget mb-50 mt-30">
+<form action=# method=get class="search-form position-relative">
+<input type=text class=search_field placeholder=Search value name=s>
+<span class=search-icon><i class="ti-search mr-5"></i></span>
+</form>
+</div>
+<div class="sidebar-widget mb-50">
+<div class="widget-header mb-30">
+<h5 class=widget-title>Top <span>Trending</span></h5>
+</div>
+<div class=post-aside-style-2>
+<ul class=list-post>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-2.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Vancouver woman finds pictures and videos of herself online</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>K. Marry</a></span>
+<span class=post-on>4m ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-3.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>4 Things Emotionally Intelligent People Don’t Do</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Mr. John</a></span>
+<span class=post-on>3h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-5.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Reflections from a Token Black Friend</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Kenedy</a></span>
+<span class=post-on>4h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-7.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>How to Identify a Smart Person in 3 Minutes</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Steven</a></span>
+<span class=post-on>5h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-8.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Blackface Minstrel Songs Don’t Belong in Children’s Music Class</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>J.Smith</a></span>
+<span class=post-on>5h30 ago</span>
+</div>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<div class="sidebar-widget widget_tag_cloud mb-50">
+<div class="widget-header tags-close mb-20">
+<h5 class="widget-title mt-5">Tags Cloud</h5>
+</div>
+<div class=tagcloud>
+<a href=#>Beauty</a>
+<a href=#>Book</a>
+<a href=#>Design</a>
+<a href=#>Fashion</a>
+<a href=#>Lifestyle</a>
+<a href=#>Travel</a>
+<a href=#>Science</a>
+<a href=#>Health</a>
+<a href=#>Sports</a>
+<a href=#>Arts</a>
+<a href=#>Books</a>
+<a href=#>Style</a>
+</div>
+</div>
+
+</div>
+</aside>
+<?php include('header.php') ?>
+<main class=position-relative>
+<div class=container>
+<div class="entry-header entry-header-1 mb-30 mt-50">
+<div class="entry-meta meta-0 font-small mb-30"><a href=#><span class="post-cat bg-success color-white">Global News</span></a></div>
+<h1 class="post-title mb-30">
+China Uses a Stealth Jet to Counter India's Rafale in the Sikkim Area
+</h1>
+<div class="entry-meta meta-1 font-x-small color-grey text-uppercase">
+<span class=post-by>By <a href=#>Newsguru Editors</a></span>
+<span class=post-on>7:30 pm
+Friday, 31 May 2024</span>
+<span class=time-reading>8 mins read</span>
+</div>
+</div>
+<div class="row mb-50">
+<div class="col-lg-8 col-md-12">
+<div class="single-social-share single-sidebar-share mt-30">
+<ul>
+<li><a class="social-icon facebook-icon text-xs-center" target=_blank href=#><i class=ti-facebook></i></a></li>
+<li><a class="social-icon twitter-icon text-xs-center" target=_blank href=#><i class=ti-twitter-alt></i></a></li>
+<li><a class="social-icon pinterest-icon text-xs-center" target=_blank href=#><i class=ti-pinterest></i></a></li>
+<li><a class="social-icon instagram-icon text-xs-center" target=_blank href=#><i class=ti-instagram></i></a></li>
+<li><a class="social-icon linkedin-icon text-xs-center" target=_blank href=#><i class=ti-linkedin></i></a></li>
+<li><a class="social-icon email-icon text-xs-center" target=_blank href=#><i class=ti-email></i></a></li>
+</ul>
+</div>
+<div class="bt-1 border-color-1 mb-30"></div>
+<figure class="single-thumnail mb-30">
+<img src=assets/imgs/news/jet.webp alt=blog width=100%>
+<div class="credit mt-15 font-small color-grey">
+<i class="ti-credit-card mr-5"></i><span>Image credit: additional resources.</span>
+</div>
+</figure>
+<div class=single-excerpt>
+<p>A number of J-20s, China's most sophisticated stealth fighters, are seen arriving at a significant high-altitude Tibetan air base that is less than 150 kilometers from the Sikkim border, according to new, high-resolution satellite footage made available to
+newsguru.
+</p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<p>
+Additionally, the Indian Air Force's facility in Hasimara, Bengal, which is home to the IAF's second squadron of sixteen Rafale fighter aircraft, is less than 300 kilometers away from Shigatse Airfield. The duty of guarding Eastern India's Himalayan frontier falls to these Rafales.  </p>
+<p>India now has two Rafale squadrons with thirty-six aircraft. It is estimated that China has produced over 250 J-20 stealth aircraft to date. Up until recently, the J-20 was mostly used to protect the Pacific coast on its eastern frontier.  </p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<p>The presence of at least seven J-20s at Shigatse has major military implications for India.</p>
+<p>Located at an altitude of 12,408 feet, Shigatse, the second largest city in Tibet, has a dual-use airport for civil and military use. The deployment of the J-20 shows its ability to operate from the harsh environment of ultra-high altitude Tibetan air bases. 
+</p>
+<p>
+The presence of the fighters also indicate that the Chinese Air Force may have deployed infrastructure to sustain operations of the fighters in the area before a possible full-fledged, permanent deployment.
+</p>
+<p>
+The Indian Air Force has declined a request for comment on the significance of the J-20 deployment. </p>
+<p>The Indian Air Force faces an obvious and present threat from the J-20's forward deployment in Tibetan airfields. The CEO of NewSpace Research, which is creating drones for the Indian Armed Forces, Sameer Joshi, a veteran fighter pilot, says, "This erodes the advantage enjoyed by IAF assets like the Rafale and imposes caution against the PLAAF [Chinese Air Force]." This capability "clearly negates the numerical advantage in modern jets that the IAF has enjoyed for some time," along with an increase in the number of modern fighters available in the Chinese inventory. 
+</p>
+<p>As additional J-20s taxi to the flight-line, the photographs included in this report, taken on May 27, depict a J-20 using its drogue parachute to slow down upon landing in Shigatse. In another picture, a J-20 is seen passing two Chinese F-7s, which are licensed versions of Russia's storied MiG-21. In 1964, China started producing the J-7 on its own soil. 2011 saw the first flight of its new J-20 stealth fighter.</p>
+<p>"All Source Analysis's Sim Tack, a geospatial analyst, first reported the deployment of the J-20s earlier this week in Shigatse. These aircraft are primarily based in the eastern provinces of China, and the J-20 stealth fighter is China's most advanced operational fighter aircraft to date." "These aircraft are being deployed outside of their usual areas of operation and in close proximity to the Indian border," said the statement upon seeing them arrive at Shigatse in Tibet.
+</p>
+<p>
+The J-20 has already been used in Tibet once before. Between 2020 and 2023, the jets were seen in the Hotan prefecture of Xinjiang, China. However, based on publicly accessible satellite images, this is thought to be the greatest deployment of the J-20.
+</p>
+<p>According to retired Commodore TJ Maolankar, a former Chief Test Pilot on India's Tejas fighter, which he first touched down on the aircraft carrier INS Vikramaditya, China's deployment of the J-20 at Shigatse is intended to convey a message to India.  
+</p>
+<p>
+From one perspective, it is evident that the Chinese have effectively integrated combat aircraft into their frontline capabilities while operating over the Himalayas. Conversely, it highlights once more the inherent susceptibility of permanent airfields. Is it because they don't think we have the power or the will to attack them first that the Chinese don't mind us flaunting them in this way?"
+</p>
+<p>From one perspective, it is evident that the Chinese have effectively integrated combat aircraft into their frontline capabilities while operating over the Himalayas. Conversely, it highlights once more the inherent susceptibility of permanent airfields. Is it because they don't think we have the power or the will to attack them first that the Chinese don't mind us flaunting them in this way?"
+</p>
+<p>2017 saw the entry into service of the Chengdu J-20, popularly referred to as the Mighty Dragon, a twin-engine stealth fighter. China became the third nation in the world to operationally field stealth jets with the introduction of the fighter. The aircraft is always being updated, and it has several sensors installed. Its main function is to combat air superiority, and it is equipped with China's most sophisticated air-to-air missiles, such as the PL-15 long-range missile, which is thought to be capable of hitting targets up to 300 kilometers away.</p>
+<p>''Deploying quickly over large swathes of Tibet in a fused awareness spectrum, helping operationalise for the first time, a 'intelligentised' large area defensive doctrine for the PLAAF,'' says Sameer Joshi of the J-20's ability to work in tandem with drones, electronic warning platforms, and airborne warning aircraft.  To put it simply, this means that the J-20 can function as a vital component of a highly networked and electronically enabled force that makes use of a variety of new aviation assets, such as cutting-edge drones, fighters of the latest generation, and older assets that are electronically connected through data-links to allow the force to deploy as a unified warfighting unit.
+
+</p>
+<p>Over the past five years, China has gradually increased its air power capability in Tibet and other regions close to India. According to Sim Tack, this has mostly involved building new air bases and modernizing the facilities of already-existing air bases. At least temporarily, China has also started sending planes like its J-20 and H-6 nuclear-capable bombers to these border areas. 
+</p>
+<p>
+In response to China's airfield expansion, India has upgraded its own airbases considerably, adding hardened shelters for its aircraft and strengthening its tier-by-tier surface-to-air missile defense system, which includes the S-400 long-range surface-to-air missile system, which was built in Russia, stationed in Eastern India. 
+</p>
+<p>The goal of the S-400 system's deployment along the Line of Actual Control is to prevent aggressive Chinese fighter-sweeps. It is purported to be able to track stealth platforms. 
+</p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<p>
+The S-400, which India purchased from Russia in 2018 for ₹ 40,000 crore ($5.43 billion), is an important component of the country's defenses, however it has been demonstrated to be weak while under attack during the conflict in Ukraine. The most current footage shows an S-400 unit, which is made up of radars, missile launchers, and other vehicles, being destroyed by ATACMS ballistic missiles fired by Ukraine that it was obviously unable to detect.
+</p>
+<p>
+Ironically, the S-400 missile system, which is supplied from Russia, is used by both China and India.
+</p>
+</div>
+	<?php include('sociallink.php') ?>
+	<?php include('relatedpost.php') ?>
+</div>
+<?php include('rightside.php') ?></div>
+</div>
+	
+</main>
+<?php include('footer.php') ?>
+</div>
+<div class=dark-mark></div>
+<script src=assets/js/vendor/modernizr-3.6.0.min.js></script>
+<script src=assets/js/vendor/jquery-3.6.0.min.js></script>
+<script src=assets/js/vendor/popper.min.js></script>
+<script src=assets/js/vendor/bootstrap.min.js></script>
+<script src=assets/js/vendor/jquery.slicknav.js></script>
+<script src=assets/js/vendor/owl.carousel.min.js></script>
+<script src=assets/js/vendor/slick.min.js></script>
+<script src=assets/js/vendor/wow.min.js></script>
+<script src=assets/js/vendor/animated.headline.js></script>
+<script src=assets/js/vendor/jquery.magnific-popup.js></script>
+<script src=assets/js/vendor/jquery.ticker.js></script>
+<script src=assets/js/vendor/jquery.vticker-min.js></script>
+<script src=assets/js/vendor/jquery.scrollUp.min.js></script>
+<script src=assets/js/vendor/jquery.nice-select.min.js></script>
+<script src=assets/js/vendor/jquery.sticky.js></script>
+<script src=assets/js/vendor/perfect-scrollbar.js></script>
+<script src=assets/js/vendor/waypoints.min.js></script>
+<script src=assets/js/vendor/jquery.counterup.min.js></script>
+<script src=assets/js/vendor/jquery.theia.sticky.js></script>
+<script src=../../ionicons%405.0.0/dist/ionicons.js></script>
+<script src=assets/js/main.js></script>
+</body>
+</html>
