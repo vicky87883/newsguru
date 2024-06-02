af0ca65aef94b201b72a3627e2ab83d604193e66
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-02 22:37:51 +0530

    commit updated

---
+++ wi-vs-png-live-score-t20-world-cup-2024-match-2-west-indies-vs-papua-new-guinea-live-score-updates-in-hindi.php
@@ -0,0 +1,374 @@
+<?php
+require_once('dbcon.php');
+$query = "SELECT * FROM `frontload`   ORDER BY `id` DESC;";
+$query2 = "SELECT * FROM `mostpopular`   ORDER BY `id` DESC;";
+$query3 = "SELECT * FROM `dontmiss`   ORDER BY `id` DESC;";
+$query4 = "SELECT * FROM `toptrending`   ORDER BY `id` DESC;";
+$result = mysqli_query($con,$query);
+$result2 = mysqli_query($con,$query2);
+$result3 = mysqli_query($con,$query3);
+$result4 = mysqli_query($con,$query4);
+?><!DOCTYPE html>
+<html class=no-js lang=en>
+<head>
+<meta charset=utf-8>
+<meta http-equiv=x-ua-compatible content="ie=edge">
+<title>WI vs PNG T20 विश्व कप 2024 के लाइव स्कोर: बारिश ने मैच में खलल डाला, जिससे मैच रुका।</title>
+<meta name=news_keywords content="WI vs PNG T20 विश्व कप 2024 के लाइव स्कोर: बारिश ने मैच में खलल डाला, जिससे मैच रुका।" itemprop="keywords"/>
+<meta name=description content="WI vs PNG T20 विश्व कप 2024 के लाइव स्कोर: बारिश ने मैच में खलल डाला, जिससे मैच रुका।" itemprop="description"/>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<link rel=canonical href="https://www.newsguru.live/wi-vs-png-live-score-t20-world-cup-2024-match-2-west-indies-vs-papua-new-guinea-live-score-updates-in-hindi.php" />
+<link rel="shortcut icon" type=image/x-icon href=assets/imgs/favicon.svg>
+<link rel=stylesheet href=assets/css/style.css>
+<link rel=stylesheet href=assets/css/widgets.css>
+<link rel=stylesheet href=assets/css/color.css>
+<link rel=stylesheet href=assets/css/responsive.css>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V6HH2RKGTW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V6HH2RKGTW');
+</script>
+</head>
+<body>
+<div id=preloader-active>
+<div class="preloader d-flex align-items-center justify-content-center">
+<div class="preloader-inner position-relative">
+<div class=text-center>
+<img class="jump mb-50" src=assets/imgs/loading.svg alt>
+<h6>Now Loading</h6>
+<div class=loader>
+<div class="bar bar1"></div>
+<div class="bar bar2"></div>
+<div class="bar bar3"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class=main-wrap>
+<aside id=sidebar-wrapper class="custom-scrollbar offcanvas-sidebar position-right">
+<button class=off-canvas-close><i class=ti-close></i></button>
+<div class=sidebar-inner>
+<div class="siderbar-widget mb-50 mt-30">
+<form action=# method=get class="search-form position-relative">
+<input type=text class=search_field placeholder=Search value name=s>
+<span class=search-icon><i class="ti-search mr-5"></i></span>
+</form>
+</div>
+<div class="sidebar-widget mb-50">
+<div class="widget-header mb-30">
+<h5 class=widget-title>Top <span>Trending</span></h5>
+</div>
+<div class=post-aside-style-2>
+<ul class=list-post>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-2.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Vancouver woman finds pictures and videos of herself online</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>K. Marry</a></span>
+<span class=post-on>4m ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-3.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>4 Things Emotionally Intelligent People Don’t Do</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Mr. John</a></span>
+<span class=post-on>3h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-5.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Reflections from a Token Black Friend</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Kenedy</a></span>
+<span class=post-on>4h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-7.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>How to Identify a Smart Person in 3 Minutes</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Steven</a></span>
+<span class=post-on>5h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-8.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Blackface Minstrel Songs Don’t Belong in Children’s Music Class</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>J.Smith</a></span>
+<span class=post-on>5h30 ago</span>
+</div>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<div class="sidebar-widget widget_tag_cloud mb-50">
+<div class="widget-header tags-close mb-20">
+<h5 class="widget-title mt-5">Tags Cloud</h5>
+</div>
+<div class=tagcloud>
+<a href=#>Beauty</a>
+<a href=#>Book</a>
+<a href=#>Design</a>
+<a href=#>Fashion</a>
+<a href=#>Lifestyle</a>
+<a href=#>Travel</a>
+<a href=#>Science</a>
+<a href=#>Health</a>
+<a href=#>Sports</a>
+<a href=#>Arts</a>
+<a href=#>Books</a>
+<a href=#>Style</a>
+</div>
+</div>
+
+</div>
+</aside>
+<?php include('header.php') ?>
+<main class=position-relative>
+<div class=container>
+<div class="entry-header entry-header-1 mb-30 mt-50">
+<div class="entry-meta meta-0 font-small mb-30"><a href=#><span class="post-cat bg-success color-white">Global News</span></a></div>
+<h1 class="post-title mb-30">
+WI vs PNG T20 विश्व कप 2024 के लाइव स्कोर: बारिश ने मैच में खलल डाला, जिससे मैच रुका।
+</h1>
+<div class="entry-meta meta-1 font-x-small color-grey text-uppercase">
+<span class=post-by>By <a href=#>Newsguru Editors</a></span>
+<span class=post-on>10:11 pm
+Sunday, 2 June 2024</span>
+<span class=time-reading>8 mins read</span>
+</div>
+</div>
+<div class="row mb-50">
+<div class="col-lg-8 col-md-12">
+<div class="single-social-share single-sidebar-share mt-30">
+<ul>
+<li><a class="social-icon facebook-icon text-xs-center" target=_blank href=#><i class=ti-facebook></i></a></li>
+<li><a class="social-icon twitter-icon text-xs-center" target=_blank href=#><i class=ti-twitter-alt></i></a></li>
+<li><a class="social-icon pinterest-icon text-xs-center" target=_blank href=#><i class=ti-pinterest></i></a></li>
+<li><a class="social-icon instagram-icon text-xs-center" target=_blank href=#><i class=ti-instagram></i></a></li>
+<li><a class="social-icon linkedin-icon text-xs-center" target=_blank href=#><i class=ti-linkedin></i></a></li>
+<li><a class="social-icon email-icon text-xs-center" target=_blank href=#><i class=ti-email></i></a></li>
+</ul>
+</div>
+<div class="bt-1 border-color-1 mb-30"></div>
+<figure class="single-thumnail mb-30">
+<img src=assets/imgs/news/cricpng.webp alt=blog width=100%>
+<div class="credit mt-15 font-small color-grey">
+<i class="ti-credit-card mr-5"></i><span>Image credit: additional resources.</span>
+</div>
+</figure>
+<div class=single-excerpt>
+<p><mark>वेस्टइंडीज, जो दो बार विश्व कप विजेता था, का पिछला वर्ल्ड कप बहुत बुरा था। ये टीम भी मुख्य दौर में नहीं जा सकी। वेस्टइंडीज इस बार टी20 विश्व कप का संयुक्त मेजबान है और उसकी कोशिश होगी कि अपने घर में सूखे को खत्म करे। वह मंजिल तक पहुंचने के लिए एक अच्छी शुरुआत की जरूरत है, और वेस्टइंडीज को ऐसा करने का मौका है।</mark></p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<p>खेल डेस्क, नई दिल्ली। 2024 टी20 वर्ल्ड कप में दो बार की चैंपियन वेस्टइंडीज आज पापुआ न्यू गिनी से खेलेगा। वेस्टइंडीज की टीम ने वार्मअप मैच में ऑस्ट्रेलिया को हराया और अपने शानदार खेल का प्रदर्शन किया। PGN की टीम बहुत मजबूत नहीं है, इसलिए गयाना के प्रोविडेंस स्टेडियम में खेले जाने वाले इस मैच में विंडीज के बल्लेबाज रनों की बड़ी संख्या बना सकते हैं।</p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<div><strong>WI vs PNG live score T20 world cup 2024: बारिश ने रोका मैच</strong></div>
+<p>जब वेस्टइंडीज की पारी का दूसरा ओवर चल रहा था, तो बारिश हो गई, जिससे मैच रुकना पड़ा। मैदान पर वर्तमान में कवर्स हैं। </p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<div><strong>WI vs PNG live score T20 world cup 2024: विंडीज को लगा पहला झटका</strong></div>
+<p>वेस्टइंडीज टीम ने अपना पहला झटका लिया है। जॉन्सन ने दूसरे ओवर की पहली गेंद पर चार्ल्स को एलबीडब्ल्यू कर दिया। उन्हें एली नाओ ने खाता तक नहीं खोलने दिया। विंडीज ने प्रतिक्रिया दी, लेकिन अंपार्यस का फोन आया, जिससे चार्ल्स को जाना पड़ा। </p>
+</div>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<div><strong>WI vs PNG live score T20 world cup 2024: वेस्टइंडीज की पारी शुरू</strong></div>
+<p>वेस्टइंडीज ने खेलना शुरू कर दिया है। जॉन्सन चार्ल्स और ब्रेंडन किंग ने पारी की शुरुआत की है। विंडीज जीतने के लिए 137 रन बनाने चाहिए। पहले ओवर में विंडीज ने आठ रन बनाए।</p>
+<div><strong>WI vs PNG live score T20 world cup 2024: पीएनजी ने बनाए 136 रन</strong></div>
+<p>PGJ की पारी समाप्त हो गई है। 20 ओवर खेलने के बाद, टीम ने सेसे बाऊ के 50 और डोरिगा के नाबाद 27 रनों के दम पर आठ विकेट खोकर 136 रन बनाए हैं। </p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<div><strong>WI vs PNG live score T20 world cup 2024: पीएनजी का 7वां विकेट गिरा</strong></div>
+<p>पीएनजी के बल्लेबाज सोपर को रसेल ने एक शानदार यॉर्कर से बोल्ड कर दिया। 19वें ओवर की अंतिम गेंद पर उनका विकेट गिरा। </p>
+<div><strong>WI vs PNG live score T20 world cup 2024: बाऊ आउट</strong></div>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<p>17वें ओवर की तीसरी गेंद पर बाऊ ने अर्धशतक ठोक दिया है। अल्जारी जोसेफ ने उन्हें बाहर निकाला। जोसेफ ने धीमी गेंद को नहीं समझा और बोल्ड हो गया। 43 गेंदों पर उन्होंने 50 रनों की पारी खेली। वह अपने देश में टी20 वर्ल्ड कप में अर्धशतक जमाने वाले दूसरे खिलाड़ी हैं। </p>
+<div><strong>WI vs PNG live score T20 world cup 2024: अमिनी आउट, पीएनजी ने खोया पांचवां विकेट</strong></div>
+<p>आंद्रे रसेल ने पीएनजी को पांचवां झटका लगाया है। 15वें ओवर की आखिरी गेंद पर, उन्होंने अमिनी को विकेटकीपर पूरन के हाथों कैच करा दिया। वह सिर्फ बारह रन बना सका। </p>
+<div><strong>WI vs PNG live score T20 world cup 2024: हिरी आउट, पीएनजी का चौथा विकेट गिरा</strong></div>
+<p>PANJI ने चौथी बार विकेट खो दिया है। हिरी बाहर हैं। पावेल ने गुड्केश मोती की गेंद पर उनका कैच लपका। </p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<div><strong>WI vs PNG live score T20 world cup 2024: पीएनजी का तीसरा विकेट गिरा</strong></div>
+<p>Pranji अपना तीसरा विकेट खो चुका है। उन्हें छठे ओवर की अंतिम गेंद पर अल्जारी जोसेफ ने रोस्टन चेस के हाथों कैच कराया। प्रधान कप्तान असाद वाला ने 21 रन बनाए।  </p>
+<div><strong>WI vs PNG live score T20 world cup 2024: पीएनजी ने खोया दूसरा विकेट</strong></div>
+<p>अकील हुसैन ने पीएनजी को एक और धक्का लगाया है। तीसरे ओवर की पहली गेंद पर, उन्होंने लेगा सियाका को बोल्ड कर दिया। </p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<div><strong>WI vs PNG live score T20 world cup 2024: पीएनजी का पहला विकेट गिरा</strong></div>
+<p>Pranji अपना पहला विकेट खो चुका है। दूसरे ओवर में रोमारियो शेफर्ड ने टोनी ओरा को पांचवीं गेंद पर आउट कर दिया। विकेटकीपर निकोलस पूरन ने उनका कैच पकड़ा। </p>
+<div><strong>WI vs PNG live score T20 world cup 2024: मैच शुरू, उरा-असद मैदान पर</strong></div>
+<p>वेस्टइंडीज और PNG का खेल शुरू हुआ है। वेस्टइंडीज की ओवर बाएं हाथ की स्पिनर अकील हुसैन पहली बार खेल रहे हैं। PANJI के टोनी ओरा और असद वाला पारी की शुरुआत करेंगे। </p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<div><strong>WI vs PNG live score T20 world cup 2024: दोनों टीमें की प्लेइंग-11</strong></div>
+<p>वेस्टइंडीज लीग-11: कप्तान रोवमैन पावेल, विकेटकीपर निकोलस पूरन, जॉनसन चार्ल्स, ब्रैंडन किंग, रोस्टन चेस, शेरफेन रदरफोर्ड, आंद्रे रसेल, रोमारियो शेफर्ड, अकील हुसैन, अल्जारी जोसेफ, गुडाकेश मोती
+
+PGN प्लेइंग-11: असद वाला (कप्तान), टोनी उरा, सेसे बाउ, हिरी हिरी, चार्ल्स अमिनी, किपलिंग डोरिगा, एली नाओ, चाड सोपर, काबुआ मोरिया और जॉन कारिको
+</p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<div><strong>WI vs PNG live score T20 world cup 2024: वेस्टइंडीज की पहले गेंदबाजी</strong></div>
+<p>वेस्टइंडीज के कप्तान रोवमैन पावेल ने पीएनजी के खिलाफ टॉस जीतकर पहली गेंदबाजी करने का निर्णय लिया है।</p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<div><strong>WI vs PNG live score T20 world cup 2024: जीत के साथ शुरुआत पर नजरें</strong></div>
+<p>दो बार की विजेता वेस्टइंडीज आज टी20 विश्व कप खेलने जा रहा है। उसका पहला मैच पापुआ न्यू गिनी से होगा। पीएनजी की टीम इस मैच को गयाना के प्रोविडेंस स्टेडियम में खेलेगी, जहां वेस्टइंडीज जीत चाहेगा। </p>
+</div>
+<?php include('rightside.php') ?></div>
+</div>
+	
+</main>
+<?php include('footer.php') ?>
+</div>
+<div class=dark-mark></div>
+<script src=assets/js/vendor/modernizr-3.6.0.min.js></script>
+<script src=assets/js/vendor/jquery-3.6.0.min.js></script>
+<script src=assets/js/vendor/popper.min.js></script>
+<script src=assets/js/vendor/bootstrap.min.js></script>
+<script src=assets/js/vendor/jquery.slicknav.js></script>
+<script src=assets/js/vendor/owl.carousel.min.js></script>
+<script src=assets/js/vendor/slick.min.js></script>
+<script src=assets/js/vendor/wow.min.js></script>
+<script src=assets/js/vendor/animated.headline.js></script>
+<script src=assets/js/vendor/jquery.magnific-popup.js></script>
+<script src=assets/js/vendor/jquery.ticker.js></script>
+<script src=assets/js/vendor/jquery.vticker-min.js></script>
+<script src=assets/js/vendor/jquery.scrollUp.min.js></script>
+<script src=assets/js/vendor/jquery.nice-select.min.js></script>
+<script src=assets/js/vendor/jquery.sticky.js></script>
+<script src=assets/js/vendor/perfect-scrollbar.js></script>
+<script src=assets/js/vendor/waypoints.min.js></script>
+<script src=assets/js/vendor/jquery.counterup.min.js></script>
+<script src=assets/js/vendor/jquery.theia.sticky.js></script>
+<script src=../../ionicons%405.0.0/dist/ionicons.js></script>
+<script src=assets/js/main.js></script>
+</body>
+</html>
