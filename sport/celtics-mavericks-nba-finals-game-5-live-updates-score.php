8e36997a5355852972e79a93b26df94bf9a9f562
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-18 11:00:04 +0530

    commit updated

---
+++ sport/celtics-mavericks-nba-finals-game-5-live-updates-score.php
@@ -0,0 +1,387 @@
+<!DOCTYPE html>
+<html class=no-js lang=en>
+<head>
+<meta charset=utf-8>
+<meta http-equiv=x-ua-compatible content="ie=edge">
+<title>A record 18 NBA titles are won by the Celtics, led by Jayson Tatum and Jaylen Brown.</title>
+<meta name=news_keywords content="A record 18 NBA titles are won by the Celtics, led by Jayson Tatum and Jaylen Brown." itemprop="keywords"/>
+<meta name=description content="A record 18 NBA titles are won by the Celtics, led by Jayson Tatum and Jaylen Brown." itemprop="description"/>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<link rel=canonical href=https://www.newsguru.live/celtics-mavericks-nba-finals-game-5-live-updates-score.php />
+<link rel="shortcut icon" type=image/x-icon href=../assets/imgs/favicon.svg>
+<link rel=stylesheet href=assets/css/style.css>
+<link rel=stylesheet href=assets/css/widgets.css>
+<link rel=stylesheet href=assets/css/color.css>
+<link rel=stylesheet href=assets/css/responsive.css>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V6HH2RKGTW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V6HH2RKGTW');
+</script>
+</head>
+<body>
+<div id=preloader-active>
+<div class="preloader d-flex align-items-center justify-content-center">
+<div class="preloader-inner position-relative">
+<div class=text-center>
+<img class="jump mb-50" src=../assets/imgs/loading.svg alt>
+<h6>Now Loading</h6>
+<div class=loader>
+<div class="bar bar1"></div>
+<div class="bar bar2"></div>
+<div class="bar bar3"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class=main-wrap>
+<aside id=sidebar-wrapper class="custom-scrollbar offcanvas-sidebar position-right">
+<button class=off-canvas-close><i class=ti-close></i></button>
+<div class=sidebar-inner>
+<div class="siderbar-widget mb-50 mt-30">
+<form action=# method=get class="search-form position-relative">
+<input type=text class=search_field placeholder=Search value name=s>
+<span class=search-icon><i class="ti-search mr-5"></i></span>
+</form>
+</div>
+<div class="sidebar-widget mb-50">
+<div class="widget-header mb-30">
+<h5 class=widget-title>Top <span>Trending</span></h5>
+</div>
+<div class=post-aside-style-2>
+<ul class=list-post>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-2.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Vancouver woman finds pictures and videos of herself online</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>K. Marry</a></span>
+<span class=post-on>4m ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-3.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>4 Things Emotionally Intelligent People Don’t Do</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Mr. John</a></span>
+<span class=post-on>3h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-5.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Reflections from a Token Black Friend</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Kenedy</a></span>
+<span class=post-on>4h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-7.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>How to Identify a Smart Person in 3 Minutes</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Steven</a></span>
+<span class=post-on>5h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-8.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Blackface Minstrel Songs Don’t Belong in Children’s Music Class</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>J.Smith</a></span>
+<span class=post-on>5h30 ago</span>
+</div>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<div class="sidebar-widget widget_tag_cloud mb-50">
+<div class="widget-header tags-close mb-20">
+<h5 class="widget-title mt-5">Tags Cloud</h5>
+</div>
+<div class=tagcloud>
+<a href=#>Beauty</a>
+<a href=#>Book</a>
+<a href=#>Design</a>
+<a href=#>Fashion</a>
+<a href=#>Lifestyle</a>
+<a href=#>Travel</a>
+<a href=#>Science</a>
+<a href=#>Health</a>
+<a href=#>Sports</a>
+<a href=#>Arts</a>
+<a href=#>Books</a>
+<a href=#>Style</a>
+</div>
+</div>
+</div>
+</aside>
+<?php include('header.php') ?>
+<main class=position-relative>
+<div class=container>
+<div class="entry-header entry-header-1 mb-30 mt-50">
+<div class="entry-meta meta-0 font-small mb-30"><a href=#><span class="post-cat bg-success color-white">Global News</span></a></div>
+<h1 class="post-title mb-30">
+A record 18 NBA titles are won by the Celtics, led by Jayson Tatum and Jaylen Brown.
+</h1>
+<div class="entry-meta meta-1 font-x-small color-grey text-uppercase">
+<span class=post-by>By Newsguru Editors</span>
+<span class=post-on>10:53 am
+Tuesday, 18 June 2024</span>
+<span class=time-reading>8 mins read</span>
+</div>
+</div>
+<div class="row mb-50">
+<div class="col-lg-8 col-md-12">
+<div class="single-social-share single-sidebar-share mt-30">
+<ul>
+<li><a class="social-icon facebook-icon text-xs-center" target=_blank href=#><i class=ti-facebook></i></a></li>
+<li><a class="social-icon twitter-icon text-xs-center" target=_blank href=#><i class=ti-twitter-alt></i></a></li>
+<li><a class="social-icon pinterest-icon text-xs-center" target=_blank href=#><i class=ti-pinterest></i></a></li>
+<li><a class="social-icon instagram-icon text-xs-center" target=_blank href=#><i class=ti-instagram></i></a></li>
+<li><a class="social-icon linkedin-icon text-xs-center" target=_blank href=#><i class=ti-linkedin></i></a></li>
+<li><a class="social-icon email-icon text-xs-center" target=_blank href=#><i class=ti-email></i></a></li>
+</ul>
+</div>
+<div class="bt-1 border-color-1 mb-30"></div>
+<figure class="single-thumnail mb-30">
+<img src=assets/imgs/news/nba.jpg alt=blog width=100%>
+<div class="credit mt-15 font-small color-grey">
+<i class="ti-credit-card mr-5"></i><span>Image credit: additional resources.</span>
+</div>
+</figure>
+<div class=single-excerpt>
+	<p>Boston: The Boston Celtics won their 18th NBA title exactly sixteen years ago, on this date last year. 
+</p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<p>
+Monday's Game 5 victory by the Celtics against Dallas, 106-88, completed the picture of a champion. 
+</p>
+<p>
+The NBA has a new champion for the sixth straight year as Boston had the best regular-season record (64-18) and cruised through the playoffs with a 16-3 record, never needing more than five games to win a series.
+</p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<p>
+The Celtics have won more championships than any other team in NBA history, ending a tie with the Lakers for the most all-time. 
+</p>
+<p>Throughout the regular season, Boston was the greatest team in terms of offense, defense, and 3-point shooting. This performance continued into the playoffs, particularly in the Finals and particularly in Game 5. 
+
+Boston outscored Dallas with 3-point shooting, going 12-for-23 on threes at one point in the third quarter. They also kept Dallas to fewer than 100 points (on 44.9% shooting) for the fourth time in the series. 
+</p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<p>With 9:10 remaining in the third quarter, the Celtics lead 78-52, and the rest of the game was a Boston fan celebration. 
+
+With 31 points, 8 rebounds, 11 assists, and Jayson Tatum in the lead, Jaylen Brown led the Celtics with 31 points, 8 rebounds, and 6 assists. Derrick White scored 14 points and eight rebounds, Jrue Holiday scored 15 points, 11 rebounds, and four assists, and both Celtics starters finished in double figures. 
+</p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<p>At 35, Joe Mazzulla of the Celtics became the youngest coach to win a title since Boston's Bill Russell in 1969. "It was a joy watching the guys just grow as a team throughout the year but also like really work at it," Mazzulla said. "There's a group of guys in the locker room that decided they wanted to win day one, and credit to them."
+</p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<p>
+Luka Doncic resumed Kyrie Irving's horror show at the Garden with 28 points on 12-for-25 shooting, 12 rebounds, and five assists. However, he was just 2 for 9 on 3-pointers. With only 15 points, he was 3-for-9 on threes and 5-for-16 from the field. Give Boston's adaptable defense credit for making it difficult for Doncic and Irving to score.  
+</p>
+</div>
+
+
+</div>
+<div class="col-lg-4 col-md-12 sidebar-right sticky-sidebar">
+<div class=pl-lg-50>
+<div class="sidebar-widget mb-30">
+<div class="widget-header position-relative mb-30">
+<div class=row>
+<div class=col-7>
+<h4 class="widget-title mb-0">Don't <span>miss</span></h4>
+</div>
+<div class="col-5 text-right">
+<h6 class="font-medium pr-15">
+<a class="text-muted font-small" href=#>View all</a>
+</h6>
+</div>
+</div>
+</div>
+<div class="post-aside-style-1 border-radius-10 p-20 bg-white">
+<ul class=list-post>
+<li class=mb-20>
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-4.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Federal arrests show no sign that antifa plotted protests</a></h6>
+</div>
+</div>
+</li>
+<li class=mb-20>
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-15.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>How line-dried laundry gets that fresh smell</a></h6>
+</div>
+</div>
+</li>
+<li class=mb-20>
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-16.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Traveling tends to magnify all human emotions</a></h6>
+</div>
+</div>
+</li>
+<li>
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-15.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>How line-dried laundry gets that fresh smell</a></h6>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<div class="sidebar-widget widget_newsletter border-radius-10 p-20 bg-white mb-30">
+<div class="widget-header widget-header-style-1 position-relative mb-15">
+<h5 class=widget-title>Newsletter</h5>
+</div>
+<div class=newsletter>
+<p class=font-medium>Heaven fruitful doesn't over les idays appear creeping</p>
+<form target=_blank action=# method=get class="subscribe_form relative mail_part">
+<div class=form-newsletter-cover>
+<div class="form-newsletter position-relative">
+<input type=email name=EMAIL placeholder="Put your email here" required>
+<button type=submit>
+<i class="ti ti-email"></i>
+</button>
+</div>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</main>
+<?php include('../footer.php') ?>
+</div>
+<div class=dark-mark></div>
+<script src=assets/js/vendor/modernizr-3.6.0.min.js></script>
+<script src=assets/js/vendor/jquery-3.6.0.min.js></script>
+<script src=assets/js/vendor/popper.min.js></script>
+<script src=assets/js/vendor/bootstrap.min.js></script>
+<script src=assets/js/vendor/jquery.slicknav.js></script>
+<script src=assets/js/vendor/owl.carousel.min.js></script>
+<script src=assets/js/vendor/slick.min.js></script>
+<script src=assets/js/vendor/wow.min.js></script>
+<script src=assets/js/vendor/animated.headline.js></script>
+<script src=assets/js/vendor/jquery.magnific-popup.js></script>
+<script src=assets/js/vendor/jquery.ticker.js></script>
+<script src=assets/js/vendor/jquery.vticker-min.js></script>
+<script src=assets/js/vendor/jquery.scrollUp.min.js></script>
+<script src=assets/js/vendor/jquery.nice-select.min.js></script>
+<script src=assets/js/vendor/jquery.sticky.js></script>
+<script src=assets/js/vendor/perfect-scrollbar.js></script>
+<script src=assets/js/vendor/waypoints.min.js></script>
+<script src=assets/js/vendor/jquery.counterup.min.js></script>
+<script src=assets/js/vendor/jquery.theia.sticky.js></script>
+<script src=../ionicons%405.0.0/dist/ionicons.js></script>
+<script src=assets/js/main.js></script>
+</body>
+</html>
