26c38db1a64a283498267f7b197458c72f0f18fe
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-18 13:04:33 +0530

    commit updated

---
+++ how-old-is-trump-biden-2024-candidates.php
@@ -0,0 +1,310 @@
+<?php
+require_once('dbcon.php');
+$query = "SELECT * FROM `frontload`   ORDER BY `id` DESC;";
+$query2 = "SELECT * FROM `mostpopular`   ORDER BY `id` DESC;";
+$query3 = "SELECT * FROM `dontmiss`   ORDER BY `id` DESC;";
+$query4 = "SELECT * FROM `toptrending`   ORDER BY `id` DESC;";
+$result = mysqli_query($con,$query);
+$result2 = mysqli_query($con,$query2);
+$result3 = mysqli_query($con,$query3);
+$result4 = mysqli_query($con,$query4);
+?><!DOCTYPE html>
+<html class=no-js lang=en>
+<head>
+<meta charset=utf-8>
+<meta http-equiv=x-ua-compatible content="ie=edge">
+<title>In 2024, voting on age will be held. These are the ages of each contender.</title>
+<meta name=news_keywords content="In 2024, voting on age will be held. These are the ages of each contender.
+" itemprop="keywords"/>
+<meta name=description content="In 2024, voting on age will be held. These are the ages of each contender.
+" itemprop="description"/>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<link rel=canonical href="https://www.newsguru.live/sport/how-old-is-trump-biden-2024-candidates.php" />
+<link rel="shortcut icon" type=image/x-icon href=assets/imgs/favicon.svg>
+<link rel=stylesheet href=assets/css/style.css>
+<link rel=stylesheet href=assets/css/widgets.css>
+<link rel=stylesheet href=assets/css/color.css>
+<link rel=stylesheet href=assets/css/responsive.css>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V6HH2RKGTW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V6HH2RKGTW');
+</script>
+</head>
+<body>
+<div id=preloader-active>
+<div class="preloader d-flex align-items-center justify-content-center">
+<div class="preloader-inner position-relative">
+<div class=text-center>
+<img class="jump mb-50" src=assets/imgs/loading.svg alt>
+<h6>Now Loading</h6>
+<div class=loader>
+<div class="bar bar1"></div>
+<div class="bar bar2"></div>
+<div class="bar bar3"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class=main-wrap>
+<aside id=sidebar-wrapper class="custom-scrollbar offcanvas-sidebar position-right">
+<button class=off-canvas-close><i class=ti-close></i></button>
+<div class=sidebar-inner>
+<div class="siderbar-widget mb-50 mt-30">
+<form action=# method=get class="search-form position-relative">
+<input type=text class=search_field placeholder=Search value name=s>
+<span class=search-icon><i class="ti-search mr-5"></i></span>
+</form>
+</div>
+<div class="sidebar-widget mb-50">
+<div class="widget-header mb-30">
+<h5 class=widget-title>Top <span>Trending</span></h5>
+</div>
+<div class=post-aside-style-2>
+<ul class=list-post>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-2.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Vancouver woman finds pictures and videos of herself online</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>K. Marry</a></span>
+<span class=post-on>4m ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-3.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>4 Things Emotionally Intelligent People Don’t Do</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Mr. John</a></span>
+<span class=post-on>3h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-5.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Reflections from a Token Black Friend</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Kenedy</a></span>
+<span class=post-on>4h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-7.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>How to Identify a Smart Person in 3 Minutes</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Steven</a></span>
+<span class=post-on>5h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-8.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Blackface Minstrel Songs Don’t Belong in Children’s Music Class</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>J.Smith</a></span>
+<span class=post-on>5h30 ago</span>
+</div>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<div class="sidebar-widget widget_tag_cloud mb-50">
+<div class="widget-header tags-close mb-20">
+<h5 class="widget-title mt-5">Tags Cloud</h5>
+</div>
+<div class=tagcloud>
+<a href=#>Beauty</a>
+<a href=#>Book</a>
+<a href=#>Design</a>
+<a href=#>Fashion</a>
+<a href=#>Lifestyle</a>
+<a href=#>Travel</a>
+<a href=#>Science</a>
+<a href=#>Health</a>
+<a href=#>Sports</a>
+<a href=#>Arts</a>
+<a href=#>Books</a>
+<a href=#>Style</a>
+</div>
+</div>
+
+</div>
+</aside>
+<?php include('header.php') ?>
+<main class=position-relative>
+<div class=container>
+<div class="entry-header entry-header-1 mb-30 mt-50">
+<div class="entry-meta meta-0 font-small mb-30"><a href=#><span class="post-cat bg-success color-white">Global News</span></a></div>
+<h1 class="post-title mb-30">
+In 2024, voting on age will be held. These are the ages of each contender.
+</h1>
+<div class="entry-meta meta-1 font-x-small color-grey text-uppercase">
+<span class=post-by>By <a href=#>Newsguru Editors</a></span>
+<span class=post-on>11:28 am
+Tuesday, 18 June 2024</span>
+<span class=time-reading>9 mins read</span>
+</div>
+</div>
+<div class="row mb-50">
+<div class="col-lg-8 col-md-12">
+<div class="single-social-share single-sidebar-share mt-30">
+<ul>
+<li><a class="social-icon facebook-icon text-xs-center" target=_blank href=#><i class=ti-facebook></i></a></li>
+<li><a class="social-icon twitter-icon text-xs-center" target=_blank href=#><i class=ti-twitter-alt></i></a></li>
+<li><a class="social-icon pinterest-icon text-xs-center" target=_blank href=#><i class=ti-pinterest></i></a></li>
+<li><a class="social-icon instagram-icon text-xs-center" target=_blank href=#><i class=ti-instagram></i></a></li>
+<li><a class="social-icon linkedin-icon text-xs-center" target=_blank href=#><i class=ti-linkedin></i></a></li>
+<li><a class="social-icon email-icon text-xs-center" target=_blank href=#><i class=ti-email></i></a></li>
+</ul>
+</div>
+<div class="bt-1 border-color-1 mb-30"></div>
+<figure class="single-thumnail mb-30">
+<img src=assets/imgs/news/trump.webp alt=blog width=100%>
+<div class="credit mt-15 font-small color-grey">
+<i class="ti-credit-card mr-5"></i><span>Image credit: additional resources.</span>
+</div>
+</figure>
+<div class=single-excerpt>
+<p>There are now seven candidates running for president in 2024; all but Chase Oliver were alive during the administration of President Dwight D. Eisenhower, are free to access 401(k) accounts without penalty, and are qualified to receive Social Security benefits.</p>
+<ul>
+        <li>Joe Biden, 81</li>
+        <li>Donald Trump, 78</li>
+        <li>Jill Stein, 74</li>
+        <li>Marianne Williamson, 71</li>
+        <li>Cornel West, 71</li>
+        <li>Robert F. Kennedy Jr., 70</li>
+        <li>Chase Oliver, 38</li>
+    </ul>
+    <p>As the last two commanders in chief have shown, there is a minimum age requirement to run for president, but no upper age limit. During their respective administrations, former President Donald Trump and President Joe Biden both set records for the oldest elected president.</p>
+    <div><strong>How old is Donald Trump?</strong></div>
+    <p>On Friday, the Republican front-runner celebrated his 78th birthday. He frequently criticizes Biden, implying that the incumbent is unsuitable to be president despite their close age difference of three years.
+</p>
+<p>
+Get ready for the polls by using our voter guide to compare the positions of the presidential candidates on important issues.
+</p>
+<p>
+As previously reported by USA TODAY, Trump's team has continued to use Biden's age as a campaign point, claiming that the oldest president in American history had shown signs of "cognitive decline" while "wandering around" during a photo opportunity at the G7 in Italy.
+</p>
+<div><strong>How old are the Democratic candidates?</strong></div>
+<br>
+<img src="assets/imgs/news/biden.JPG" alt="blog" width="100%" height="100%">
+<br>
+<p>At 81 years old, Biden was born in Scranton, Pennsylvania, on November 20, 1942. He is now the oldest elected U.S. president. He would be 82 years old when he takes office and 86 years old at the conclusion of his term if he wins in November.
+</p>
+<p>
+Biden has to fight low approval ratings, which are worse among younger people, and persuade them that he is more qualified for office than Trump, who is only three years his junior.
+</p>
+<p>
+In the most recent State of the Union speech, the president addressed age-related concerns head-on. "At my age, some things become more obvious than they ever have," Biden remarked.
+</p>
+<img src="assets/imgs/news/mariana.JPG" alt="blog" width="100%" height="100%">
+<p>Born in Houston, Texas on July 8, 1952, best-selling author Marianne Williamson is 71 years old and will be 72 just before the general election on November 5. Less than a month after withdrawing, she announced her long-shot 2024 presidential campaign earlier this year, emphasizing "justice and love."
+</p>
+<p>
+She had earlier suspended it because she was losing "the horse race," but in a statement from February, she said she was returning to oppose Trump's "dark and authoritarian vision," according to Reuters.
+</p>
+<div><strong>How old are the third-party candidates?</strong></div>
+<img src="assets/imgs/news/robert.JPG" alt="blog" width="100%" height="100%">
+
+<p>The age of Robert F. Kennedy Jr. is seventy. He was born in Washington, D.C. on January 17, 1954, making him this year's second-youngest presidential contender. In October 2023, he changed from seeking the Democratic nomination to the independent one.
+
+On October 9, 2023, Robert F. Kennedy Jr. declares his intention to run as an independent in the 2024 presidential contest in Philadelphia, Pennsylvania.</p>
+<p>
+Kennedy's campaign declared that he will be officially running in a few states, such as Utah, Michigan, and California. However, he still has to fight a difficult and expensive battle to be listed in all 50 states.
+</p>
+<img src="assets/imgs/news/cornel.JPG" alt="blog" width="100%" height="100%">
+<p>The philosopher, scholar, and political activist Cornel West is running as a third-party candidate for president in an attempt to win over progressive, Democratic-leaning voters.
+</p>
+<p>
+West, 71, ran as a Green Party candidate at first, but he later proclaimed himself an independent in October, saying that people "want good policies over partisan politics." He has pledged to provide housing security and eradicate poverty.
+</p>
+<img src="assets/imgs/news/jilstein.JPG" alt="blog" width="100%" height="100%">
+<p>In 2024, 74-year-old doctor Jill Stein, who ran for the Green Party in 2016, plans to run again.
+
+Accusing Democrats of repeatedly breaking their pledges "for working people, youth, and the climate - while Republicans don't even make such promises in the first place" is how she introduced her current campaign.
+</p>
+<p>
+Millions of funds were raised by Stein for recounts following Trump's unexpected 2016 win. Only one electoral analysis in Wisconsin concluded that Trump had won as a result of her accusations.
+</p>
+<p>Although Kennedy and Trump were invited to speak at the Libertarian Party convention in late May, Chase Oliver, 38, was finally chosen. Oliver received 2% of the vote in his 2022 race for a Georgia state Senate seat.
+</p>
+<div><strong>What’s the minimum age to run for U.S. president?</strong></div>
+<p>A natural-born citizen of the United States must be at least 35 years old to run for president, per the U.S. Constitution. Theodore Roosevelt, who took office at age 42, was the youngest U.S. president to date. Joe Biden, who took office at age 78 and is the only president to have served in their 80s, is the oldest.
+</p>
+</div>
+	<?php include('sociallink.php') ?>
+	<?php include('relatedpost.php') ?>
+</div>
+<?php include('rightside.php') ?></div>
+</div>
+	
+</main>
+<?php include('footer.php') ?>
+</div>
+<div class=dark-mark></div>
+<script src=assets/js/vendor/modernizr-3.6.0.min.js></script>
+<script src=assets/js/vendor/jquery-3.6.0.min.js></script>
+<script src=assets/js/vendor/popper.min.js></script>
+<script src=assets/js/vendor/bootstrap.min.js></script>
+<script src=assets/js/vendor/jquery.slicknav.js></script>
+<script src=assets/js/vendor/owl.carousel.min.js></script>
+<script src=assets/js/vendor/slick.min.js></script>
+<script src=assets/js/vendor/wow.min.js></script>
+<script src=assets/js/vendor/animated.headline.js></script>
+<script src=assets/js/vendor/jquery.magnific-popup.js></script>
+<script src=assets/js/vendor/jquery.ticker.js></script>
+<script src=assets/js/vendor/jquery.vticker-min.js></script>
+<script src=assets/js/vendor/jquery.scrollUp.min.js></script>
+<script src=assets/js/vendor/jquery.nice-select.min.js></script>
+<script src=assets/js/vendor/jquery.sticky.js></script>
+<script src=assets/js/vendor/perfect-scrollbar.js></script>
+<script src=assets/js/vendor/waypoints.min.js></script>
+<script src=assets/js/vendor/jquery.counterup.min.js></script>
+<script src=assets/js/vendor/jquery.theia.sticky.js></script>
+<script src=../../ionicons%405.0.0/dist/ionicons.js></script>
+<script src=assets/js/main.js></script>
+</body>
+</html>
