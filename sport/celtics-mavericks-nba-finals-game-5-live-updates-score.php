4fe6670e2ff992b443619acc25f8d4ff67848560
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-18 11:11:27 +0530

    commit updated

--- sport/celtics-mavericks-nba-finals-game-5-live-updates-score.php
+++ sport/celtics-mavericks-nba-finals-game-5-live-updates-score.php
@@ -189,7 +189,7 @@ Tuesday, 18 June 2024</span>
 </div>
 <div class="bt-1 border-color-1 mb-30"></div>
 <figure class="single-thumnail mb-30">
-<img src=assets/imgs/news/nba.jpg alt=blog width=100%>
+<img src=assets/imgs/news/nba.JPG alt=blog width=100%>
 <div class="credit mt-15 font-small color-grey">
 <i class="ti-credit-card mr-5"></i><span>Image credit: additional resources.</span>
 </div>
