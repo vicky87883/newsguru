9200cc1452ecc404e3fd425681814dce74a4363d
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-18 10:12:23 +0530

    commit updated

---
+++ sport/jaylen-brown-contract-celtics-finals-mvp.php
@@ -0,0 +1,509 @@
+<?php
+require_once('dbcon.php');
+$query = "SELECT * FROM `frontload`   ORDER BY `id` DESC;";
+$query2 = "SELECT * FROM `mostpopular`   ORDER BY `id` DESC;";
+$query3 = "SELECT * FROM `sports`   ORDER BY `id` DESC;";
+$query4 = "SELECT * FROM `toptrending`   ORDER BY `id` DESC;";
+$result = mysqli_query($con,$query);
+$result2 = mysqli_query($con,$query2);
+$result3 = mysqli_query($con,$query3);
+$result4 = mysqli_query($con,$query4);
+?>
+<!DOCTYPE html>
+<html class=no-js lang=en>
+<head>
+<meta charset=utf-8>
+<meta http-equiv=x-ua-compatible content="ie=edge">
+<title>Details of Jaylen Brown's contract: Celtics player wins a championship and is named Finals MVP, living up to historic extend </title>
+<meta name=news_keywords content="Details of Jaylen Brown's contract: Celtics player wins a championship and is named Finals MVP, living up to historic extend " itemprop="keywords"/>
+<meta name=description content="Details of Jaylen Brown's contract: Celtics player wins a championship and is named Finals MVP, living up to historic extend " itemprop="description"/>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<link rel=canonical href="https://www.newsguru.live/jaylen-brown-contract-celtics-finals-mvp.php" />
+<link rel="shortcut icon" type=image/x-icon href=assets/imgs/favicon.svg>
+<link rel=stylesheet href=assets/css/style.css>
+<link rel=stylesheet href=assets/css/widgets.css>
+<link rel=stylesheet href=assets/css/color.css>
+<link rel=stylesheet href=assets/css/responsive.css>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V6HH2RKGTW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V6HH2RKGTW');
+</script>
+<style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 10px;
+            border: 1px solid #ddd;
+            text-align: left;
+        }
+        th {
+            background-color: #f4f4f4;
+        }
+        tr:nth-child(even) {
+            background-color: #f9f9f9; /* Light brown */
+        }
+        tr:nth-child(odd) {
+            background-color: #ffffff; /* White */
+        }
+</style>
+</head>
+<body>
+<div id=preloader-active>
+<div class="preloader d-flex align-items-center justify-content-center">
+<div class="preloader-inner position-relative">
+<div class=text-center>
+<img class="jump mb-50" src=../assets/imgs/loading.svg alt>
+<h6>Now Loading</h6>
+<div class=loader>
+<div class="bar bar1"></div>
+<div class="bar bar2"></div>
+<div class="bar bar3"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class=main-wrap>
+<aside id=sidebar-wrapper class="custom-scrollbar offcanvas-sidebar position-right">
+<button class=off-canvas-close><i class=ti-close></i></button>
+<div class=sidebar-inner>
+<div class="siderbar-widget mb-50 mt-30">
+<form action=# method=get class="search-form position-relative">
+<input type=text class=search_field placeholder=Search value name=s>
+<span class=search-icon><i class="ti-search mr-5"></i></span>
+</form>
+</div>
+<div class="sidebar-widget mb-50">
+<div class="widget-header mb-30">
+<h5 class=widget-title>Top <span>Trending</span></h5>
+</div>
+<div class=post-aside-style-2>
+<ul class=list-post>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-2.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Vancouver woman finds pictures and videos of herself online</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>K. Marry</a></span>
+<span class=post-on>4m ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-3.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>4 Things Emotionally Intelligent People Don’t Do</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Mr. John</a></span>
+<span class=post-on>3h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-5.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Reflections from a Token Black Friend</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Kenedy</a></span>
+<span class=post-on>4h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-7.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>How to Identify a Smart Person in 3 Minutes</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Steven</a></span>
+<span class=post-on>5h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-8.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Blackface Minstrel Songs Don’t Belong in Children’s Music Class</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>J.Smith</a></span>
+<span class=post-on>5h30 ago</span>
+</div>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<div class="sidebar-widget widget_tag_cloud mb-50">
+<div class="widget-header tags-close mb-20">
+<h5 class="widget-title mt-5">Tags Cloud</h5>
+</div>
+<div class=tagcloud>
+<a href=#>Beauty</a>
+<a href=#>Book</a>
+<a href=#>Design</a>
+<a href=#>Fashion</a>
+<a href=#>Lifestyle</a>
+<a href=#>Travel</a>
+<a href=#>Science</a>
+<a href=#>Health</a>
+<a href=#>Sports</a>
+<a href=#>Arts</a>
+<a href=#>Books</a>
+<a href=#>Style</a>
+</div>
+</div>
+</div>
+</aside>
+<?php include('header.php') ?>
+<main class=position-relative>
+<div class=container>
+<div class="entry-header entry-header-1 mb-30 mt-50">
+<div class="entry-meta meta-0 font-small mb-30"><a href=#><span class="post-cat bg-success color-white">Global News</span></a></div>
+<h1 class="post-title mb-30">
+Details of Jaylen Brown's contract: Celtics player wins a championship and is named Finals MVP, living up to historic extend
+</h1>
+<div class="entry-meta meta-1 font-x-small color-grey text-uppercase">
+<span class=post-by>By Newsguru Editors</span>
+<span class=post-on>9:35 am
+Tuesday, 18 June 2024</span>
+<span class=time-reading>12 mins read</span>
+</div>
+</div>
+<div class="row mb-50">
+<div class="col-lg-8 col-md-12">
+<div class="single-social-share single-sidebar-share mt-30">
+<ul>
+<li><a class="social-icon facebook-icon text-xs-center" target=_blank href=#><i class=ti-facebook></i></a></li>
+<li><a class="social-icon twitter-icon text-xs-center" target=_blank href=#><i class=ti-twitter-alt></i></a></li>
+<li><a class="social-icon pinterest-icon text-xs-center" target=_blank href=#><i class=ti-pinterest></i></a></li>
+<li><a class="social-icon instagram-icon text-xs-center" target=_blank href=#><i class=ti-instagram></i></a></li>
+<li><a class="social-icon linkedin-icon text-xs-center" target=_blank href=#><i class=ti-linkedin></i></a></li>
+<li><a class="social-icon email-icon text-xs-center" target=_blank href=#><i class=ti-email></i></a></li>
+</ul>
+</div>
+<div class="bt-1 border-color-1 mb-30"></div>
+<figure class="single-thumnail mb-30">
+<img src=assets/imgs/news/jeylen.JPG alt=blog width=100%>
+<div class="credit mt-15 font-small color-grey">
+<i class="ti-credit-card mr-5"></i><span>Image credit: additional resources.</span>
+</div>
+</figure>
+<div class=single-excerpt>
+	<p>Now is the time to acknowledge Jaylen Brown's accomplishments.</p>
+    <ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+    <p>
+Brown completed an amazing season by guiding the Celtics to the 2024 NBA title and winning Finals MVP less than a year after agreeing to the largest contract in NBA history. In addition to being awarded MVP of Boston's Eastern Conference Finals victory, Brown is receiving his second big playoff honor.
+</p>
+<p>In the NBA Playoffs, Brown performed admirably when his team most needed him. Brown's worth was evident at the most important times, as evidenced by his clutch three to force overtime in Game 1 of the Eastern Conference Finals and his composure in the closing seconds of Game 3 of the NBA Finals.
+</p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<p>
+Although Brown's record-breaking contract hasn't started yet, the 2023–24 campaign highlights the significance of Boston's historic commitment to the 27-year-old. This is an overview of the agreement that Brown has previously fulfilled.
+</p>
+<div><strong>Jaylen Brown contract details</strong></div>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<p>Brown agreed to a five-year, $303.7 million contract extension in July 2023 in order to play for the Celtics at least through the 2028–2029 campaign.
+</p>
+<p>
+Officially valued at $286.2 million over five years, Brown's deal comes after he was not selected to the All-NBA team in 2023–24.
+</p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<p>
+Brown's deal with Boston runs until his age-32 season, and he will make $49.4 million in 2024–25.
+</p>
+<table>
+        <thead>
+            <tr>
+                <th>Year</th>
+                <th>Age</th>
+                <th>Salary</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>2024-25</td>
+                <td>28</td>
+                <td>$49,350,000</td>
+            </tr>
+            <tr>
+                <td>2025-26</td>
+                <td>29</td>
+                <td>$53,298,000</td>
+            </tr>
+            <tr>
+                <td>2026-27</td>
+                <td>30</td>
+                <td>$57,246,000</td>
+
+            </tr>
+            <tr>
+                <td>2027-28</td>
+                <td>31</td>
+                <td>$61,194,000</td>
+
+            </tr>
+            <tr>
+                <td>2028-29</td>
+                <td>32</td>
+                <td>$65,142,000</td>
+
+            </tr>
+        </tbody>
+    </table>
+    <div><strong>Jaylen Brown 2023-24 stats, accolades</strong></div>
+    <p>Throughout the regular season, Brown put up a ton of numbers, but in the NBA Playoffs, he really elevated his game. In four games, he scored thirty points or more, including a huge 40-point performance in Game 2 of the Eastern Conference Finals and a crucial 30-point effort in Game 3 of the NBA Finals.</p>
+    <table>
+        <thead>
+            <tr>
+                <th></th>
+                <th>Regular season</th>
+                <th>NBA Playoffs</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+
+                <th>PPG</th>
+                <td>23.0</td>
+                <td>24.9</td>
+            </tr>
+            <tr>
+            <th>RPG</th>
+                <td>5.5</td>
+                <td>5.8</td>
+            </tr>
+            <tr>
+            <th>APG</th>
+                <td>3.6</td>
+                <td>3.1</td>
+
+            </tr>
+            <tr>
+            <th>SPG</th>
+                <td>1.2</td>
+                <td>1.2</td>
+
+            </tr>
+            <tr>
+            <th>BPG</th>
+                <td>0.5</td>
+                <td>0.7</td>
+
+            </tr>
+            <tr>
+            <th>TO/G</th>
+                <td>2.4</td>
+                <td>2.8</td>
+
+            </tr>
+            <tr>
+            <th>FG%</th>
+                <td>49.9</td>
+                <td>53.2</td>
+
+            </tr>
+            <tr>
+            <th>3P%</th>
+                <td>35.4</td>
+                <td>33.7</td>
+
+            </tr><tr>
+            <th>FT%</th>
+                <td>70.3</td>
+                <td>64.9</td>
+
+            </tr>
+        </tbody>
+    </table>
+    <ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+    <p>Although Brown did not receive All-NBA or All-Defensive Team recognition during the regular season, he did receive several other awards, such as his third career All-Star selection and the noteworthy postseason recognition.</p>
+
+
+<div><strong>MVP of the NBA Finals</strong></div>
+<div><strong>MVP of the Eastern Conference Finals</strong></div>
+<div><strong>NBA All-Star</strong></div>
+
+</div>
+<div class=related-posts>
+<h3 class=mb-30>Related posts</h3>
+<div class=row>
+<article class=col-lg-4>
+<div class="background-white border-radius-10 p-10 mb-30">
+<div class="post-thumb d-flex mb-15 border-radius-15 img-hover-scale">
+<a href=#>
+<img class=border-radius-15 src=../assets/imgs/news-2.jpg alt>
+</a>
+</div>
+<div class="pl-10 pr-10">
+<div class="entry-meta mb-15 mt-10">
+<a class="entry-meta meta-2" href=#><span class="post-in text-primary font-x-small">Politic</span></a>
+</div>
+<h5 class="post-title mb-15">
+<span class=post-format-icon>
+<ion-icon name=image-outline role=img class="md hydrated" aria-label="image outline"></ion-icon>
+</span>
+<a href=#>The litigants on the screen are not actors</a></h5>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase mb-10">
+<span class=post-by>By <a href=#>John Nathan</a></span>
+<span class=post-on>8m ago</span>
+</div>
+</div>
+</div>
+</article>
+<article class=col-lg-4>
+<div class="background-white border-radius-10 p-10 mb-30">
+<div class="post-thumb d-flex mb-15 border-radius-15 img-hover-scale">
+<a href=#>
+<img class=border-radius-15 src=../assets/imgs/news-5.jpg alt>
+</a>
+</div>
+<div class="pl-10 pr-10">
+<div class="entry-meta mb-15 mt-10">
+<a class="entry-meta meta-2" href=#><span class="post-in text-success font-x-small">Tech</span></a>
+</div>
+<h5 class="post-title mb-15">
+<span class=post-format-icon>
+<ion-icon name=headset-outline role=img class="md hydrated" aria-label="headset outline"></ion-icon>
+</span>
+<a href=#>Essential Qualities of Highly Successful Music</a></h5>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase mb-10">
+<span class=post-by>By <a href=#>K. Steven</a></span>
+<span class=post-on>24m ago</span>
+</div>
+</div>
+</div>
+</article>
+<article class=col-lg-4>
+<div class="background-white border-radius-10 p-10">
+<div class="post-thumb d-flex mb-15 border-radius-15 img-hover-scale">
+<a href=#>
+<img class=border-radius-15 src=../assets/imgs/news-7.jpg alt>
+</a>
+</div>
+<div class="pl-10 pr-10">
+<div class="entry-meta mb-15 mt-10">
+<a class="entry-meta meta-2" href=#><span class="post-in text-danger font-x-small">Global</span></a>
+</div>
+<h5 class="post-title mb-15">
+<a href=#>Essential Qualities of Highly Successful Music</a></h5>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase mb-10">
+<span class=post-by>By <a href=#>K. Jonh</a></span>
+<span class=post-on>24m ago</span>
+</div>
+</div>
+</div>
+</article>
+</div>
+</div>
+</div>
+	<?php include('../rightside.php') ?>
+</div>
+</div>
+</main>
+<?php include('../footer.php') ?>
+</div>
+<div class=dark-mark></div>
+<script src=assets/js/vendor/modernizr-3.6.0.min.js></script>
+<script src=assets/js/vendor/jquery-3.6.0.min.js></script>
+<script src=assets/js/vendor/popper.min.js></script>
+<script src=assets/js/vendor/bootstrap.min.js></script>
+<script src=assets/js/vendor/jquery.slicknav.js></script>
+<script src=assets/js/vendor/owl.carousel.min.js></script>
+<script src=assets/js/vendor/slick.min.js></script>
+<script src=assets/js/vendor/wow.min.js></script>
+<script src=assets/js/vendor/animated.headline.js></script>
+<script src=assets/js/vendor/jquery.magnific-popup.js></script>
+<script src=assets/js/vendor/jquery.ticker.js></script>
+<script src=assets/js/vendor/jquery.vticker-min.js></script>
+<script src=assets/js/vendor/jquery.scrollUp.min.js></script>
+<script src=assets/js/vendor/jquery.nice-select.min.js></script>
+<script src=assets/js/vendor/jquery.sticky.js></script>
+<script src=assets/js/vendor/perfect-scrollbar.js></script>
+<script src=assets/js/vendor/waypoints.min.js></script>
+<script src=assets/js/vendor/jquery.counterup.min.js></script>
+<script src=assets/js/vendor/jquery.theia.sticky.js></script>
+<script src=../ionicons%405.0.0/dist/ionicons.js></script>
+<script src=assets/js/main.js></script>
+</body>
+</html>
