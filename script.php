09d0a8254cf6eb70849fbd1f43c4be54184d25d7
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-20 18:05:48 +0530

    commit updated

--- script.php
+++ script.php
@@ -1,22 +1,33 @@
 <?php
-$mysqli = new mysqli('localhost','vikram','Parjapat@123','coder');
-if($mysqli->connect_errno !=0)
-{
-    echo $mysqli->connect_error;
+$mysqli = new mysqli('localhost', 'vikram', 'Parjapat@123', 'coder');
+
+// Check for connection error
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
     exit();
 }
+
+// Set the character set to utf8mb4
+if (!$mysqli->set_charset("utf8mb4")) {
+    echo "Error loading character set utf8mb4: " . $mysqli->error;
+    exit();
+}
+
 $start = 0;
-// Setting the number of rows to display in a page
+// Setting the number of rows to display on a page
 $rows_per_page = 9;
-$records = $mysqli->query("SELECT * FROM `frontload`   ORDER BY `id` DESC");
+$records = $mysqli->query("SELECT * FROM `frontload` ORDER BY `id` DESC");
 $nr_of_rows = $records->num_rows;
-// calculating of nr of pages
+
+// Calculating the number of pages
 $pages = ceil($nr_of_rows / $rows_per_page);
-// if the user clicks on the pagination buttons we set a new starting point.
-if(isset($_GET['page-nr']))
-{
-    $page = $_GET['page-nr']-1;
+
+// If the user clicks on the pagination buttons, set a new starting point
+if (isset($_GET['page-nr'])) {
+    $page = $_GET['page-nr'] - 1;
     $start = $page * $rows_per_page;
 }
-$result = $mysqli->query("SELECT * FROM `frontload`   ORDER BY `id` DESC LIMIT $start,$rows_per_page");
+
+// Fetching the required rows from the database with a limit for pagination
+$result = $mysqli->query("SELECT * FROM `frontload` ORDER BY `id` DESC LIMIT $start, $rows_per_page");
 ?>
