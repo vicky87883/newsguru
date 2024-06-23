2b8c97bf64bb44ebaef3809e1283be46ed44fc24
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-23 11:11:31 +0530

    commit updated

---
+++ health/foods-that-help-with-bloating.php
@@ -0,0 +1,321 @@
+<?php
+require_once('dbcon.php');
+$query2 = "SELECT * FROM `health`   ORDER BY `id` DESC;";
+$query3 = "SELECT * FROM `health`   ORDER BY `id` DESC;";
+$query4 = "SELECT * FROM `health`   ORDER BY `id` DESC;";
+$result2 = mysqli_query($con,$query2);
+$result3 = mysqli_query($con,$query3);
+$result4 = mysqli_query($con,$query4);
+?><!DOCTYPE html>
+<html class=no-js lang=en>
+<head>
+<meta charset=utf-8>
+<meta http-equiv=x-ua-compatible content="ie=edge">
+<title>Which meals and beverages help lessen bloating?
+</title>
+<meta name=news_keywords content="Which meals and beverages help lessen bloating?
+" itemprop="keywords"/>
+<meta name=description content=" Which meals and beverages help lessen bloating?
+" itemprop="description"/>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<link rel=canonical href="https://www.newsguru.live/health/foods-that-help-with-bloating.php" />
+<link rel="shortcut icon" type=image/x-icon href=assets/imgs/favicon.svg>
+<link rel=stylesheet href=assets/css/style.css>
+<link rel=stylesheet href=assets/css/widgets.css>
+<link rel=stylesheet href=assets/css/color.css>
+<link rel=stylesheet href=assets/css/responsive.css>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V6HH2RKGTW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V6HH2RKGTW');
+</script>
+</head>
+<body>
+<div id=preloader-active>
+<div class="preloader d-flex align-items-center justify-content-center">
+<div class="preloader-inner position-relative">
+<div class=text-center>
+<img class="jump mb-50" src=assets/imgs/loading.svg alt>
+<h6>Now Loading</h6>
+<div class=loader>
+<div class="bar bar1"></div>
+<div class="bar bar2"></div>
+<div class="bar bar3"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class=main-wrap>
+<aside id=sidebar-wrapper class="custom-scrollbar offcanvas-sidebar position-right">
+<button class=off-canvas-close><i class=ti-close></i></button>
+<div class=sidebar-inner>
+<div class="siderbar-widget mb-50 mt-30">
+<form action=# method=get class="search-form position-relative">
+<input type=text class=search_field placeholder=Search value name=s>
+<span class=search-icon><i class="ti-search mr-5"></i></span>
+</form>
+</div>
+<div class="sidebar-widget mb-50">
+<div class="widget-header mb-30">
+<h5 class=widget-title>Top <span>Trending</span></h5>
+</div>
+<div class=post-aside-style-2>
+<ul class=list-post>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-2.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Vancouver woman finds pictures and videos of herself online</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>K. Marry</a></span>
+<span class=post-on>4m ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-3.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>4 Things Emotionally Intelligent People Don’t Do</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Mr. John</a></span>
+<span class=post-on>3h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-5.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Reflections from a Token Black Friend</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Kenedy</a></span>
+<span class=post-on>4h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-7.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>How to Identify a Smart Person in 3 Minutes</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Steven</a></span>
+<span class=post-on>5h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-8.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Blackface Minstrel Songs Don’t Belong in Children’s Music Class</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>J.Smith</a></span>
+<span class=post-on>5h30 ago</span>
+</div>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<div class="sidebar-widget widget_tag_cloud mb-50">
+<div class="widget-header tags-close mb-20">
+<h5 class="widget-title mt-5">Tags Cloud</h5>
+</div>
+<div class=tagcloud>
+<a href=#>Beauty</a>
+<a href=#>Book</a>
+<a href=#>Design</a>
+<a href=#>Fashion</a>
+<a href=#>Lifestyle</a>
+<a href=#>Travel</a>
+<a href=#>Science</a>
+<a href=#>Health</a>
+<a href=#>Sports</a>
+<a href=#>Arts</a>
+<a href=#>Books</a>
+<a href=#>Style</a>
+</div>
+</div>
+</div>
+</aside>
+<?php include('header.php') ?>
+<main class=position-relative>
+<div class=container>
+<div class="entry-header entry-header-1 mb-30 mt-50">
+<div class="entry-meta meta-0 font-small mb-30"><a href=#><span class="post-cat bg-success color-white">Health News</span></a></div>
+<h1 class="post-title mb-30">
+Which meals and beverages help lessen bloating?
+</h1>
+<div class="entry-meta meta-1 font-x-small color-grey text-uppercase">
+<span class=post-by>By Newsguru Editors</span>
+<span class=post-on>8:44 am
+Sunday, 23 June 2024</span>
+<span class=time-reading>5 mins read</span>
+</div>
+</div>
+<div class="row mb-50">
+<div class="col-lg-8 col-md-12">
+<div class="single-social-share single-sidebar-share mt-30">
+<ul>
+<li><a class="social-icon facebook-icon text-xs-center" target=_blank href=#><i class=ti-facebook></i></a></li>
+<li><a class="social-icon twitter-icon text-xs-center" target=_blank href=#><i class=ti-twitter-alt></i></a></li>
+<li><a class="social-icon pinterest-icon text-xs-center" target=_blank href=#><i class=ti-pinterest></i></a></li>
+<li><a class="social-icon instagram-icon text-xs-center" target=_blank href=#><i class=ti-instagram></i></a></li>
+<li><a class="social-icon linkedin-icon text-xs-center" target=_blank href=#><i class=ti-linkedin></i></a></li>
+<li><a class="social-icon email-icon text-xs-center" target=_blank href=#><i class=ti-email></i></a></li>
+</ul>
+</div>
+<div class="bt-1 border-color-1 mb-30"></div>
+<figure class="single-thumnail mb-30">
+<img src=assets/imgs/news/bloating.JPG alt=blog width=100%>
+<div class="credit mt-15 font-small color-grey">
+<i class="ti-credit-card mr-5"></i><span>Image credit: additional resources.</span>
+</div>
+</figure>
+<div class=single-excerpt>
+	<p>Certain foods and drinks can lessen gastrointestinal (GI) discomfort and minimize bloating. Whole grains, fruits, and a variety of veggies can all aid in reducing bloating.
+	</p>
+	<p>Bloating is often brought on by constipation, indigestion, and gas. By concentrating on resolving these problems, bloating can be reduced.
+</p>
+<p>
+Eating adequate plant foods high in fiber can help alleviate constipation. Fruits, vegetables, and whole grains can be some of these. But doctors advise introducing fiber to the diet gradually because including it too rapidly can result in bloating.
+</p>
+<p>
+Consuming foods like kimchi and kefir, which are rich in probiotic bacteria, can improve gut health. This may lessen and even stop bloating.
+</p>
+<p>
+Consuming nutritious foods is important, but drinking enough water can also help avoid constipation and water retention.
+</p>
+<div><strong>15 foods and drinks that help with bloating</strong></div>
+<p>Below are fruits, vegetables, whole grains, and beverages that can reduce bloating.</p>
+<p><b>1. Citrus fruits</b>
+<p>
+Oranges are a wonderful source of water and fiber, which can aid in promoting regular bowel movements and preventing water retention. Additionally, they contain a lot of vitamin C, which could improve the bacterial community's health in the gut.
+</p>
+<p>
+	<b>
+2. Peeled bananas</b>
+Bananas are high in potassium and a good source of fiber. Potassium can assist control salt levels and reduce water retention.
+
+A brief 2011 investigationFurthermore, according to Trusted Source, eating bananas can result in less bloating. It was discovered that women who consumed two bananas daily experienced less bloating than those who did not.
+
+The findings suggest that regular banana consumption may encourage the development of a helpful strain of gut bacteria, according to the authors.
+</p>
+<p><b>3. Apples</b>
+Pineapples are high in fiber (1.4 g)Trusted Source per serving), but they also contain enzymes called bromelain. In traditional medicine, this enzyme is frequently used to treat digestive issues, although its effectiveness has not been well studied.
+
+An earlier 2010 studyIn animals with inflammatory bowel illness, pineapple juice decreased inflammation, according to Trusted Source. The authors suggested doing clinical trials to see if it had a comparable impact on others because of the positive outcomes.
+</p>
+<p><b>
+4. Berries</b>
+Each serving of berries has 2.1 g of fiber (Trusted Source), which is beneficial for digestive health. Additionally, they are a great source of antioxidants, which support intestinal health (Trusted Source).
+</p>
+<p><b>5. Papayas</b>
+A 2013 investigationPapayas are occasionally used as a homemade treatment for digestive issues, according to Trusted Source. The effects of taking a papaya supplement and a placebo were examined during the course of the 40-day research. It was found that those who consumed the papaya experienced decreased bloating and constipation.
+</p>
+<p><b>
+6. Cherry tomatoes</b>
+Prebiotics are an indigestible dietary ingredient found in tomatoes that aid in the development of good gut flora. Tomatoes include prebiotics that are good for persons with Crohn's disease, malabsorption, and a few other illnesses that cause bloating.
+</p>
+<p><b>7. Grass</b>
+Like other legumes, lentils have a high fiber content (Trusted Source). Additionally, they are rich in resistant starches, a type of carbohydrate that aids in gastrointestinal health by preventing digestion.
+</p>
+<p><b>
+8. Broccoli</b> Due to its high fiber content, broccoli may help to improve digestive health and lessen bloating. According to a 2017 study, mice that consumed more broccoli than mice that did not fared better when it came to gastrointestinal problems.
+
+The scientists came to the conclusion that the vegetable includes a substance that might support the bacterial community's health in the gut.
+
+But additional human study is required to fully comprehend broccoli's impacts.
+</p>
+<p><b>9. Root vegetables</b>
+Vitamin A is abundant in carrots, and this vitamin is crucial for preserving the gut's homeostasis. Additionally, according to ResearchTrusted Source, vitamin A from food sources increases the number of good bacteria in the gut strands.
+</p>
+<p><b>
+10. Celery</b>
+A 2017 investigationCelery is rich in apigenin, a flavonoid that naturally exists in plants, according to Trusted Source. According to study, celery can enhance the diversity and proliferation of gut flora, which may help with digestion.
+</p>
+<p><b>11. Lettuce</b>
+A vegetable rich in nutrients and high in fiber is spinach (Trusted Source). InvestigateAdditionally, according to Trusted Source, it might boost the number of Lactobacillus, a beneficial bacterial strain found in the stomach.
+</p>
+<p><b>
+12. Grats</b>
+OatsTrusted Source are a pleasant whole grain source that also have a substantial fiber content. Additionally, they include a kind of carbohydrate known as beta-glucan, which has the potential to reduce colon inflammation, or Trusted Source colitis.
+</p>
+<p><b>13. Fermented foods</b>
+Many fermented foods, such as kefir, kombucha, and sauerkraut, contain probiotics, which are “friendly” strains of bacteria that are beneficial for gut health.
+
+Although little research has explored their value, some test-tube studies have shown promising results. These studiesTrusted Source suggest the need for further research.</p>
+<p><b>14. Tea with ginger</b>
+A popular over-the-counter treatment for bloating, constipation, nausea, and other GI issues is gingertrusted source. Many individuals like drinking ginger tea, and while additional research is required to confirm its efficacy, it has no known negative effects.
+</p>
+<p><b>
+15. Tea with peppermint leaves</b>
+GI issues can be treated at home with peppermint tea. Although more studies are required to confirm its efficacy, it might lessen irritable bowel syndrome symptoms.
+</p>
+</div>
+<?php include('../sociallink.php') ?>
+	<?php include('../relatedpost.php') ?>
+
+</div>
+<?php include('../rightside.php') ?></div>
+</div>
+</main>
+<?php include('../footer.php') ?>
+</div>
+<div class=dark-mark></div>
+<script src=assets/js/vendor/modernizr-3.6.0.min.js></script>
+<script src=assets/js/vendor/jquery-3.6.0.min.js></script>
+<script src=assets/js/vendor/popper.min.js></script>
+<script src=assets/js/vendor/bootstrap.min.js></script>
+<script src=assets/js/vendor/jquery.slicknav.js></script>
+<script src=assets/js/vendor/owl.carousel.min.js></script>
+<script src=assets/js/vendor/slick.min.js></script>
+<script src=assets/js/vendor/wow.min.js></script>
+<script src=assets/js/vendor/animated.headline.js></script>
+<script src=assets/js/vendor/jquery.magnific-popup.js></script>
+<script src=assets/js/vendor/jquery.ticker.js></script>
+<script src=assets/js/vendor/jquery.vticker-min.js></script>
+<script src=assets/js/vendor/jquery.scrollUp.min.js></script>
+<script src=assets/js/vendor/jquery.nice-select.min.js></script>
+<script src=assets/js/vendor/jquery.sticky.js></script>
+<script src=assets/js/vendor/perfect-scrollbar.js></script>
+<script src=assets/js/vendor/waypoints.min.js></script>
+<script src=assets/js/vendor/jquery.counterup.min.js></script>
+<script src=assets/js/vendor/jquery.theia.sticky.js></script>
+<script src=../../ionicons%405.0.0/dist/ionicons.js></script>
+<script src=assets/js/main.js></script>
+</body>
+</html>
