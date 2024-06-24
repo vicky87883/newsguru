4337a0e399b63948ce3341179240f98c53e504eb
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-24 08:54:01 +0530

    commit updated

---
+++ parliament-session-18-lok-sabha-general-elections-pm-modi-amit-shah-other-mps-to-take-oath-neet-ug-2024.php
@@ -0,0 +1,272 @@
+<?php
+require_once('dbcon.php');
+$query = "SELECT * FROM `frontload`   ORDER BY `id` DESC;";
+$query2 = "SELECT * FROM `mostpopular`   ORDER BY `id` DESC;";
+$query3 = "SELECT * FROM `dontmiss`   ORDER BY `id` DESC;";
+$query4 = "SELECT * FROM `toptrending`   ORDER BY `id` DESC;";
+$result = mysqli_query($con,$query);
+$result2 = mysqli_query($con,$query2);
+$result3 = mysqli_query($con,$query3);
+$result4 = mysqli_query($con,$query4);
+?><!DOCTYPE html>
+<html class=no-js lang=en>
+<head>
+<meta charset=utf-8>
+<meta http-equiv=x-ua-compatible content="ie=edge">
+<title>Today is the first Parl session following the LS elections. other MPs and PM Modi to take an oath; NEET-UG 2024 dispute, with an emphasis on the speaker election | Latest News India - Newsguru.live</title>
+	<meta name="news_keywords" content="paytm share,paytm share price,paytm, paytm software company,paytm moneypaytm customer care" itemprop="keywords"/>
+	<meta name="description" content="Today is the first Parl session following the LS elections. other MPs and PM Modi to take an oath; NEET-UG 2024 dispute, with an emphasis on the speaker election" itemprop="description"/>
+<meta name=viewport content="width=device-width, initial-scale=1">
+	<link rel=canonical href=https://www.newsguru.live/parliament-session-18-lok-sabha-general-elections-pm-modi-amit-shah-other-mps-to-take-oath-neet-ug-2024.php />
+<link rel="shortcut icon" type=image/x-icon href=assets/imgs/favicon.svg>
+<link rel=stylesheet href=assets/css/style.css>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+<link rel=stylesheet href=assets/css/widgets.css>
+<link rel=stylesheet href=assets/css/color.css>
+<link rel=stylesheet href=assets/css/responsive.css>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V6HH2RKGTW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V6HH2RKGTW');
+</script>
+</head>
+<body>
+<div id=preloader-active>
+<div class="preloader d-flex align-items-center justify-content-center">
+<div class="preloader-inner position-relative">
+<div class=text-center>
+<img class="jump mb-50" src=assets/imgs/loading.svg alt>
+<h6>Now Loading</h6>
+<div class=loader>
+<div class="bar bar1"></div>
+<div class="bar bar2"></div>
+<div class="bar bar3"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class=main-wrap>
+<aside id=sidebar-wrapper class="custom-scrollbar offcanvas-sidebar position-right">
+<button class=off-canvas-close><i class=ti-close></i></button>
+<div class=sidebar-inner>
+<div class="siderbar-widget mb-50 mt-30">
+<form action=# method=get class="search-form position-relative">
+<input type=text class=search_field placeholder=Search value name=s>
+<span class=search-icon><i class="ti-search mr-5"></i></span>
+</form>
+</div>
+<div class="sidebar-widget mb-50">
+<div class="widget-header mb-30">
+<h5 class=widget-title>Top <span>Trending</span></h5>
+</div>
+<div class=post-aside-style-2>
+<ul class=list-post>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-2.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Vancouver woman finds pictures and videos of herself online</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>K. Marry</a></span>
+<span class=post-on>4m ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-3.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>4 Things Emotionally Intelligent People Don’t Do</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Mr. John</a></span>
+<span class=post-on>3h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-5.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Reflections from a Token Black Friend</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Kenedy</a></span>
+<span class=post-on>4h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-7.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>How to Identify a Smart Person in 3 Minutes</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Steven</a></span>
+<span class=post-on>5h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-8.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Blackface Minstrel Songs Don’t Belong in Children’s Music Class</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>J.Smith</a></span>
+<span class=post-on>5h30 ago</span>
+</div>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<div class="sidebar-widget widget_tag_cloud mb-50">
+<div class="widget-header tags-close mb-20">
+<h5 class="widget-title mt-5">Tags Cloud</h5>
+</div>
+<div class=tagcloud>
+<a href=#>Beauty</a>
+<a href=#>Book</a>
+<a href=#>Design</a>
+<a href=#>Fashion</a>
+<a href=#>Lifestyle</a>
+<a href=#>Travel</a>
+<a href=#>Science</a>
+<a href=#>Health</a>
+<a href=#>Sports</a>
+<a href=#>Arts</a>
+<a href=#>Books</a>
+<a href=#>Style</a>
+</div>
+</div>
+<div class="sidebar-widget widget-ads mb-30">
+<div class="widget-header tags-close mb-20">
+<h5 class="widget-title mt-5">Your Ads Here</h5>
+</div>
+<a href=assets/imgs/news-1.jpg class=play-video data-animate=zoomIn data-duration=1.5s data-delay=0.1s>
+<img class=border-radius-10 src=assets/imgs/ads-1.jpg alt>
+</a>
+</div>
+</div>
+</aside>
+<?php include('header.php') ?>
+<main class=position-relative>
+<div class=container>
+<div class="entry-header entry-header-1 mb-30 mt-50">
+<div class="entry-meta meta-0 font-small mb-30"><a href=#><span class="post-cat bg-success color-white">Global News</span></a></div>
+<h1 class="post-title mb-30">
+Today is the first Parl session following the LS elections. other MPs and PM Modi to take an oath; NEET-UG 2024 dispute, with an emphasis on the speaker election
+</h1>
+<div class="entry-meta meta-1 font-x-small color-grey text-uppercase">
+<span class=post-by>By Newsguru Editors</span>
+<span class=post-on>8:30 am
+Monday, 24 June 2024</span>
+<span class=time-reading>8 mins read</span>
+</div>
+</div>
+<div class="row mb-50">
+<div class="col-lg-8 col-md-12">
+<div class="single-social-share single-sidebar-share mt-30">
+<ul>
+<li><a class="social-icon facebook-icon text-xs-center" target=_blank href=#><i class=ti-facebook></i></a></li>
+<li><a class="social-icon twitter-icon text-xs-center" target=_blank href=#><i class=ti-twitter-alt></i></a></li>
+<li><a class="social-icon pinterest-icon text-xs-center" target=_blank href=#><i class=ti-pinterest></i></a></li>
+<li><a class="social-icon instagram-icon text-xs-center" target=_blank href=#><i class=ti-instagram></i></a></li>
+<li><a class="social-icon linkedin-icon text-xs-center" target=_blank href=#><i class=ti-linkedin></i></a></li>
+<li><a class="social-icon email-icon text-xs-center" target=_blank href=#><i class=ti-email></i></a></li>
+</ul>
+</div>
+<div class="bt-1 border-color-1 mb-30"></div>
+<figure class="single-thumnail mb-30">
+<img src=assets/imgs/news/sansad.JPG alt="blog" width="100%">
+<div class="credit mt-15 font-small color-grey">
+<i class="ti-credit-card mr-5"></i><span>Image credit: additional resources.</span>
+</div>
+</figure>
+<div class=single-excerpt>
+<p>The freshly elected members of the 18th Lok Sabha, including Prime Minister Narendra Modi, are scheduled to take an oath of office today, marking the start of the first session. Following the recently held Lok Sabha elections, in which the BJP won 240 seats and the NDA won 293 seats, this is the first session of the Parliament.
+</p>
+<p>The opposition is anticipated to confront the BJP-led NDA government during the first session on the Speaker election, talks about leaks of classified material and other anomalies in NEET-UG and UGC-NET, and a dispute over the pro-tem Speaker's appointment.
+</p>
+<p>
+Bhartruhari Mahtab, a seven-term member of parliament, will take the oath of office as the pro-tem speaker of the Lok Sabha from President Droupadi Murmu. Mahtab will then call on PM Modi to take the oath of office as a member of the House and to supervise its business. In addition, the President designated Sudip Bandyopadhyay, Thalikkottai Rajuthevar Baalu, Radha Mohan Singh, Faggan Singh Kulaste, and Suresh Kodikunnil to help him administer the oath to the recently elected members.
+</p>
+<p>Additionally, today at 10 a.m., the newly elected Congress party MPs will convene in the CPP office in Parliament. Congressmen Priyanka Gandhi and Rahul Gandhi took aim at the BJP over the anomalies in the NEET-UG 2024 exam and declared their support for the students. It is also anticipated that a dispute over the pro-tem Speaker's selection may surface in the Parliament. 
+</p>
+<p>The Opposition has taken issue with the BJP's decision to choose Bhartruhari Mahtab over Congress MP K Suresh, claiming that the administration disregarded Suresh and that the BJP did not adhere to parliamentary tradition. 
+</p>
+<p>
+"We are advocating that the pro-tem Speaker be an eighth-term Member of Parliament...They made a mistake, and as a result, the BJP government's choice is being criticized nationwide," K Suresh stated.
+</p>
+<p>In response, Minister of Parliamentary Affairs Kiren Rijiju stated that while Mahtab had served as speaker of the Lok Sabha for seven consecutive times, K Suresh had lost elections in 1998 and 2004, meaning that he was serving his fourth consecutive term as speaker of the Lok Sabha. K Suresh had previously won lower house elections in 1989, 1991, 1996, and 1999. 
+</p>
+<p>On Monday in Delhi, there will also be a Parliamentary meeting of the Samajwadi Party, presided over by Akhilesh Yadav. Following today's meeting, President Droupadi Murmu will address both Houses on June 27 and the Lok Sabha speaker will be elected on June 26. 
+</p>
+<p>
+The oath-taking of newly elected MPs will take place during the first session of the 18th Lok Sabha, which runs from June 24 to July 3. The Rajya Sabha's 264th session is scheduled to begin on June 27 and conclude on July 3. The 17th Lok Sabha convened its penultimate session, which coincided with the budget session, from January 31 to February 10, 2024. 
+</p>
+</div>
+
+<?php include('sociallink.php') ?>
+	<?php include('relatedpost.php') ?>
+
+</div>
+<?php include('rightside.php') ?></div>
+
+</div>
+</main>
+<?php include('footer.php') ?>
+</div>
+<div class=dark-mark></div>
+<script src=assets/js/vendor/modernizr-3.6.0.min.js></script>
+<script src=assets/js/vendor/jquery-3.6.0.min.js></script>
+<script src=assets/js/vendor/popper.min.js></script>
+<script src=assets/js/vendor/bootstrap.min.js></script>
+<script src=assets/js/vendor/jquery.slicknav.js></script>
+<script src=assets/js/vendor/owl.carousel.min.js></script>
+<script src=assets/js/vendor/slick.min.js></script>
+<script src=assets/js/vendor/wow.min.js></script>
+<script src=assets/js/vendor/animated.headline.js></script>
+<script src=assets/js/vendor/jquery.magnific-popup.js></script>
+<script src=assets/js/vendor/jquery.ticker.js></script>
+<script src=assets/js/vendor/jquery.vticker-min.js></script>
+<script src=assets/js/vendor/jquery.scrollUp.min.js></script>
+<script src=assets/js/vendor/jquery.nice-select.min.js></script>
+<script src=assets/js/vendor/jquery.sticky.js></script>
+<script src=assets/js/vendor/perfect-scrollbar.js></script>
+<script src=assets/js/vendor/waypoints.min.js></script>
+<script src=assets/js/vendor/jquery.counterup.min.js></script>
+<script src=assets/js/vendor/jquery.theia.sticky.js></script>
+<script src=../../ionicons%405.0.0/dist/ionicons.js></script>
+<script src=assets/js/main.js></script>
+</body>
+</html>
