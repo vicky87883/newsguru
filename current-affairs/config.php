79c1d20e1556938617e5509e20ab404dfe8b186f
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-24 12:54:25 +0530

    commit updated

---
+++ current-affairs/config.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'coder';
+$username = 'vikram';
+$password = 'Parjapat@123';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Could not connect to the database $dbname :" . $e->getMessage());
+}
+?>
