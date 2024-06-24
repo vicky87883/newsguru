620ee9c4e13a485806f77aed706f4ba30ced4c64
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-24 10:03:20 +0530

    commit updated

---
+++ health/japan-deadly-infections-group-a-strep-bacteria.php
@@ -0,0 +1,188 @@
+<?php
+require_once('dbcon.php');
+$query = "SELECT * FROM `health`   ORDER BY `id` DESC;";
+$query3 = "SELECT * FROM `health`   ORDER BY `id` DESC;";
+$query4 = "SELECT * FROM `health`   ORDER BY `id` DESC;";
+$result = mysqli_query($con,$query);
+$result3 = mysqli_query($con,$query3);
+$result4 = mysqli_query($con,$query4);
+?><!DOCTYPE html>
+<html class=no-js lang=en>
+<head>
+<meta charset=utf-8>
+<meta http-equiv=x-ua-compatible content="ie=edge">
+<title>A remarkable surge of fatal infections in Japan has refocused attention on the enigmas surrounding strep</title>
+<meta name=news_keywords content="A remarkable surge of fatal infections in Japan has refocused attention on the enigmas surrounding strep
+" itemprop="keywords"/>
+<meta name=description content="A remarkable surge of fatal infections in Japan has refocused attention on the enigmas surrounding strep
+" itemprop="description"/>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<link rel=canonical href="https://www.newsguru.live/health/japan-deadly-infections-group-a-strep-bacteria.php" />
+<link rel="shortcut icon" type=image/x-icon href=assets/imgs/favicon.svg>
+<link rel=stylesheet href=assets/css/style.css>
+<link rel=stylesheet href=assets/css/widgets.css>
+<link rel=stylesheet href=assets/css/color.css>
+<link rel=stylesheet href=assets/css/responsive.css>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V6HH2RKGTW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V6HH2RKGTW');
+</script>
+</head>
+<body>
+<div id=preloader-active>
+<div class="preloader d-flex align-items-center justify-content-center">
+<div class="preloader-inner position-relative">
+<div class=text-center>
+<img class="jump mb-50" src=assets/imgs/loading.svg alt>
+<h6>Now Loading</h6>
+<div class=loader>
+<div class="bar bar1"></div>
+<div class="bar bar2"></div>
+<div class="bar bar3"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class=main-wrap>
+<aside id=sidebar-wrapper class="custom-scrollbar offcanvas-sidebar position-right">
+<button class=off-canvas-close><i class=ti-close></i></button>
+<div class=sidebar-inner>
+<div class="siderbar-widget mb-50 mt-30">
+<form action=# method=get class="search-form position-relative">
+<input type=text class=search_field placeholder=Search value name=s>
+<span class=search-icon><i class="ti-search mr-5"></i></span>
+</form>
+</div>
+<div class="sidebar-widget mb-50">
+<div class="widget-header mb-30">
+<h5 class=widget-title>Top <span>Trending</span></h5>
+</div>
+</div>
+</div>
+</aside>
+<?php include('header.php') ?>
+<main class=position-relative>
+<div class=container>
+<div class="entry-header entry-header-1 mb-30 mt-50">
+<div class="entry-meta meta-0 font-small mb-30"><a href=#><span class="post-cat bg-success color-white">Health News</span></a></div>
+<h1 class="post-title mb-30">
+A remarkable surge of fatal infections in Japan has refocused attention on the enigmas surrounding strep
+</h1>
+<div class="entry-meta meta-1 font-x-small color-grey text-uppercase">
+<span class=post-by>By Newsguru Editors</span>
+<span class=post-on>9:03 am
+Monday, 24 June 2024</span>
+<span class=time-reading>5 mins read</span>
+</div>
+</div>
+<div class="row mb-50">
+<div class="col-lg-8 col-md-12">
+<div class="single-social-share single-sidebar-share mt-30">
+<ul>
+<li><a class="social-icon facebook-icon text-xs-center" target=_blank href=#><i class=ti-facebook></i></a></li>
+<li><a class="social-icon twitter-icon text-xs-center" target=_blank href=#><i class=ti-twitter-alt></i></a></li>
+<li><a class="social-icon pinterest-icon text-xs-center" target=_blank href=#><i class=ti-pinterest></i></a></li>
+<li><a class="social-icon instagram-icon text-xs-center" target=_blank href=#><i class=ti-instagram></i></a></li>
+<li><a class="social-icon linkedin-icon text-xs-center" target=_blank href=#><i class=ti-linkedin></i></a></li>
+<li><a class="social-icon email-icon text-xs-center" target=_blank href=#><i class=ti-email></i></a></li>
+</ul>
+</div>
+<div class="bt-1 border-color-1 mb-30"></div>
+<figure class="single-thumnail mb-30">
+<img src=assets/imgs/news/virus.JPG alt=blog width=100%>
+<div class="credit mt-15 font-small color-grey">
+<i class="ti-credit-card mr-5"></i><span>Image credit: additional resources.</span>
+</div>
+</figure>
+<div class=single-excerpt>
+	<p><mark>Japan has recorded more cases of streptococcal toxic shock syndrome already this year than in any other year.</mark></p>
+    <p>The number of potentially deadly infections in Japan is shattering records, which is drawing attention to long-standing, unsolved concerns about the type of bacteria causing the illnesses.
+</p>
+<p>
+The same strain of strep bacteria that causes strep throat and scarlet fever also causes streptococcal toxic shock syndrome (STSS), an uncommon condition. Like STSS, Group A strep can infrequently spread to deep tissue or the circulation. 
+</p>
+<p>30% or more of STSS infections result in death. Usually beginning with fever, chills, aches in the muscles, nausea, or vomiting, the condition can become life-threatening in as little as 24 to 48 hours if treatment is not received. Necrotizing fasciitis, another bacterial infection known as "flesh-eating" because it kills the soft tissue beneath the skin, can also occur concurrently with it.
+</p>
+<p>
+Japan's National Institute of Infectious Diseases released a study earlier this week stating that the country has at least 1,019 instances of STSS so far this year. It has reached a record high of 941, surpassing the previous year's record total.
+</p>
+<p>The extraordinarily high levels of Group A strep bacteria have been observed in both the United States and Japan during the past several years, leading to an increase in illnesses that can be lethal or life-threatening. These unprecedented numbers are drawing attention once again to the puzzling recent behavior of these bacteria. Experts in diseases don't yet fully understand why that is occurring.   
+</p>
+<p>The number of dangerous Group A strep infections in the United States, including STSS, peaked in 2023, according to preliminary statistics from the Centers for Disease Control and Prevention. This was a 20-year high. As of June 8, the CDC had reported 395 cases of STSS this year, surpassing the 390 instances from the previous year with several months remaining. However, the FDA did observe that, as is typical for this time of year, there has been a decline in Group A strep activity in recent months. 
+</p>
+<p>
+In late 2022, there was also a significant outbreak of Group A strep infections in the United Kingdom, which also reported a higher than usual number of cases from September to February.
+</p>
+<p>Experts in disease have partly ascribed the rise to the general post-pandemic resurgence of common bacterial and viral diseases, such as strep throat. When people avoided face-to-face interactions, there were less opportunities for these infections to spread. People might once again be more vulnerable now. 
+</p>
+<p>
+Expert in infectious illnesses at Vanderbilt University Medical Center Dr. William Schaffner remarked, "This may be part of this global phenomenon: the comeback of strep." The majority of these infections were significantly decreased during COVID-19, he continued, "because we stayed at home, wore masks, closed schools, and other measures."
+</p>
+<p>But other viral diseases that spiked when lockdowns loosened and socializing resumed appear to have stabilized, specialists noted, while strep cases are still above average. 
+</p>
+<p>
+"Many infections have returned to standard, typical levels, but strep has surpassed them." We're not really sure why that's occurring, according to Schaffner.
+</p>
+<p>The fact that Group A strep infections were on the rise in the United States for a number of years prior to the emergence of Covid complicates matters further.
+</p>
+<p>
+Thus, specialists believe that the pandemic is not the only possible explanation. Has the bacteria changed to become more harmful? Exist any undiscovered connections between certain viral illnesses and Group A strep? Are some age groups becoming ill now that they didn't in the past? The answers are still unknown.
+</p>
+<p>Who is at risk for STSS? Those with diabetes and older folks are often more susceptible, and sores from shingles or chickenpox can increase a person's chance of getting it. This is due to the fact that Group A strep can enter the body through open wounds and subsequently induce toxic shock syndrome. Additionally, experts believe that certain viruses may impair the immune system or harm the airways, making people more vulnerable to secondary bacterial infections. 
+</p>
+<p>
+However, disease specialists frequently aren't able to ascertain the exact cause of a patient's illness. 
+</p>
+<p>Dr. Lee Harrison, an epidemiology and medicine professor at the University of Pittsburgh, stated, "Unless a patient has a wound, you often don't know how it got into the body when they come in with group A strep in the blood."
+</p>
+<p>
+Dr. Amesh Adalja, an infectious disease physician and senior scholar at the Johns Hopkins Center for Health Security, said that although a population's susceptibility to a particular bacterium may be influenced by a country's baseline immunity to it or even by specific genetic traits, Japan's situation should serve as a reminder to physicians worldwide to keep an eye out for severe strep infections. 
+</p>
+<p>He declared, "It's important for other countries to just be on alert about what happens in Japan." 
+
+This kind of knowledge is essential because STSS patients need to be treated as soon as possible after symptoms start to show up. Treatment options include surgery to remove contaminated tissue or the use of antibiotics. 
+</p>
+<p>
+There is currently no vaccination against group A strep, however there are a few in research, including one that appeared to be safe in a phase 1 trial. According to Harrison, the need for this research may increase given the recent surge in illnesses in the US and Japan.
+</p>
+</div>
+<?php include('../sociallink.php') ?>
+	<?php include('../relatedpost.php') ?>
+
+</div>
+<?php include('../rightside.php') ?></div>
+</div>
+</main>
+<?php include('../footer.php') ?>
+</div>
+<div class=dark-mark></div>
+<script src=assets/js/vendor/modernizr-3.6.0.min.js></script>
+<script src=assets/js/vendor/jquery-3.6.0.min.js></script>
+<script src=assets/js/vendor/popper.min.js></script>
+<script src=assets/js/vendor/bootstrap.min.js></script>
+<script src=assets/js/vendor/jquery.slicknav.js></script>
+<script src=assets/js/vendor/owl.carousel.min.js></script>
+<script src=assets/js/vendor/slick.min.js></script>
+<script src=assets/js/vendor/wow.min.js></script>
+<script src=assets/js/vendor/animated.headline.js></script>
+<script src=assets/js/vendor/jquery.magnific-popup.js></script>
+<script src=assets/js/vendor/jquery.ticker.js></script>
+<script src=assets/js/vendor/jquery.vticker-min.js></script>
+<script src=assets/js/vendor/jquery.scrollUp.min.js></script>
+<script src=assets/js/vendor/jquery.nice-select.min.js></script>
+<script src=assets/js/vendor/jquery.sticky.js></script>
+<script src=assets/js/vendor/perfect-scrollbar.js></script>
+<script src=assets/js/vendor/waypoints.min.js></script>
+<script src=assets/js/vendor/jquery.counterup.min.js></script>
+<script src=assets/js/vendor/jquery.theia.sticky.js></script>
+<script src=../../ionicons%405.0.0/dist/ionicons.js></script>
+<script src=assets/js/main.js></script>
+</body>
+</html>
