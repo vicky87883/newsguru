f98145f3ce02badfec1eb7fb8ae7e4eb7056721a
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-24 09:40:51 +0530

    commit updated

---
+++ china-wary-putin-kim-new-alliance-intl-hnk.php
@@ -0,0 +1,332 @@
+<?php
+require_once('dbcon.php');
+$query = "SELECT * FROM `frontload`   ORDER BY `id` DESC;";
+$query2 = "SELECT * FROM `mostpopular`   ORDER BY `id` DESC;";
+$query3 = "SELECT * FROM `dontmiss`   ORDER BY `id` DESC;";
+$query4 = "SELECT * FROM `toptrending`   ORDER BY `id` DESC;";
+$result = mysqli_query($con,$query);
+$result2 = mysqli_query($con,$query2);
+$result3 = mysqli_query($con,$query3);
+$result4 = mysqli_query($con,$query4);
+?><!DOCTYPE html>
+<html class=no-js lang=en>
+<head>
+<meta charset=utf-8>
+<meta http-equiv=x-ua-compatible content="ie=edge">
+<title>China is keeping a close eye on Putin and Kim's new "alliance."
+</title>
+<meta name=news_keywords content="China is keeping a close eye on Putin and Kim's new "alliance."
+" itemprop="keywords"/>
+<meta name=description content="China is keeping a close eye on Putin and Kim's new "alliance."
+" itemprop="description"/>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<link rel=canonical href=https://www.newsguru.live/china-wary-putin-kim-new-alliance-intl-hnk.php />
+<link rel="shortcut icon" type=image/x-icon href=assets/imgs/favicon.svg>
+<link rel=stylesheet href=assets/css/style.css>
+<link rel=stylesheet href=assets/css/widgets.css>
+<link rel=stylesheet href=assets/css/color.css>
+<link rel=stylesheet href=assets/css/responsive.css>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V6HH2RKGTW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V6HH2RKGTW');
+</script>
+</head>
+<body>
+<div id=preloader-active>
+<div class="preloader d-flex align-items-center justify-content-center">
+<div class="preloader-inner position-relative">
+<div class=text-center>
+<img class="jump mb-50" src=assets/imgs/loading.svg alt>
+<h6>Now Loading</h6>
+<div class=loader>
+<div class="bar bar1"></div>
+<div class="bar bar2"></div>
+<div class="bar bar3"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class=main-wrap>
+<aside id=sidebar-wrapper class="custom-scrollbar offcanvas-sidebar position-right">
+<button class=off-canvas-close><i class=ti-close></i></button>
+<div class=sidebar-inner>
+<div class="siderbar-widget mb-50 mt-30">
+<form action=# method=get class="search-form position-relative">
+<input type=text class=search_field placeholder=Search value name=s>
+<span class=search-icon><i class="ti-search mr-5"></i></span>
+</form>
+</div>
+<div class="sidebar-widget mb-50">
+<div class="widget-header mb-30">
+<h5 class=widget-title>Top <span>Trending</span></h5>
+</div>
+<div class=post-aside-style-2>
+<ul class=list-post>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-2.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Vancouver woman finds pictures and videos of herself online</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>K. Marry</a></span>
+<span class=post-on>4m ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-3.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>4 Things Emotionally Intelligent People Don’t Do</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Mr. John</a></span>
+<span class=post-on>3h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-5.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Reflections from a Token Black Friend</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Kenedy</a></span>
+<span class=post-on>4h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-7.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>How to Identify a Smart Person in 3 Minutes</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Steven</a></span>
+<span class=post-on>5h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-8.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Blackface Minstrel Songs Don’t Belong in Children’s Music Class</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>J.Smith</a></span>
+<span class=post-on>5h30 ago</span>
+</div>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<div class="sidebar-widget widget_tag_cloud mb-50">
+<div class="widget-header tags-close mb-20">
+<h5 class="widget-title mt-5">Tags Cloud</h5>
+</div>
+<div class=tagcloud>
+<a href=#>Beauty</a>
+<a href=#>Book</a>
+<a href=#>Design</a>
+<a href=#>Fashion</a>
+<a href=#>Lifestyle</a>
+<a href=#>Travel</a>
+<a href=#>Science</a>
+<a href=#>Health</a>
+<a href=#>Sports</a>
+<a href=#>Arts</a>
+<a href=#>Books</a>
+<a href=#>Style</a>
+</div>
+</div>
+</div>
+</aside>
+<?php include('header.php') ?>
+<main class=position-relative>
+<div class=container>
+<div class="entry-header entry-header-1 mb-30 mt-50">
+<div class="entry-meta meta-0 font-small mb-30"><a href=#><span class="post-cat bg-success color-white">Global News</span></a></div>
+<h1 class="post-title mb-30">
+China is keeping a close eye on Putin and Kim's new "alliance."
+
+</h1>
+<div class="entry-meta meta-1 font-x-small color-grey text-uppercase">
+<span class=post-by>By Newsguru editors</span>
+<span class=post-on>9:03 am
+Monday, 24 June 2024</span>
+<span class=time-reading>5 mins read</span>
+</div>
+</div>
+<div class="row mb-50">
+<div class="col-lg-8 col-md-12">
+<div class="single-social-share single-sidebar-share mt-30">
+<ul>
+<li><a class="social-icon facebook-icon text-xs-center" target=_blank href=#><i class=ti-facebook></i></a></li>
+<li><a class="social-icon twitter-icon text-xs-center" target=_blank href=#><i class=ti-twitter-alt></i></a></li>
+<li><a class="social-icon pinterest-icon text-xs-center" target=_blank href=#><i class=ti-pinterest></i></a></li>
+<li><a class="social-icon instagram-icon text-xs-center" target=_blank href=#><i class=ti-instagram></i></a></li>
+<li><a class="social-icon linkedin-icon text-xs-center" target=_blank href=#><i class=ti-linkedin></i></a></li>
+<li><a class="social-icon email-icon text-xs-center" target=_blank href=#><i class=ti-email></i></a></li>
+</ul>
+</div>
+<div class="bt-1 border-color-1 mb-30"></div>
+<figure class="single-thumnail mb-30">
+<img src=assets/imgs/news/kim.JPG alt=blog width=100%>
+<div class="credit mt-15 font-small color-grey">
+<i class="ti-credit-card mr-5"></i><span>Image credit: additional resources.</span>
+</div>
+</figure>
+< class=single-excerpt>
+	<p><b>Hong Kong</b>
+The most significant ally of the two autocrats was observing from the sidelines in Beijing, hundreds of miles away, as Russian President Vladimir Putin and his North Korean host Kim Jong Un drove through the crowded streets of Pyongyang atop a luxurious Mercedes-Benz last week.
+</p>
+<p>
+The similar open-top ride was promised to Xi Jinping five years ago when he became the first Chinese leader to visit Pyongyang in fourteen years. The two presidents pledged at the moment to fortify their relationship and increase collaboration, but their words were insignificant in light of Kim and Putin's "breakthrough" new alliance.
+</p>
+<p>North Korea and Russia signed a comprehensive contract encompassing political, commerce, investment, and security cooperation. In the case of an assault, they agreed to utilize all available means to provide quick military assistance to each other.
+
+According to Putin, relations between Russia and North Korea have reached a "new level." In contrast, Kim referred to the new "alliance" as a "watershed moment" in the two countries' relationship.
+</p>
+<p>The United States and its Asian allies were taken aback by the historic new defense agreement reached by the two nuclear-armed countries. Regarding Putin's promise to keep discussing possible military technology collaboration with Pyongyang, Japan expressed "severe concerns." In response, South Korea declared it would now think about supplying Ukraine with weapons and called an emergency national security meeting.
+</p>
+<p>
+China, North Korea's principal economic and political backer, has, in contrast, largely refrained from responding.
+</p>
+<p>China, North Korea's principal economic and political backer, has, in contrast, largely refrained from responding.
+
+China's Foreign Ministry spokeswoman described the deal as a bilateral issue between North Korea and Russia and declined to comment on it.
+
+Analysts speculate that China is, however, probably keeping a cautious eye on things behind the scenes.
+</p>
+<div><strong>China ‘aims to control the situation’</strong>
+<p>As he deals with a number of domestic issues, most notably the slowing economy, Xi needs peace and stability in Northeast Asia, and the growing connections between two drifting autocrats run the potential of causing him further uncertainty.
+</p>
+<p>
+Beijing is concerned that Moscow's support for Pyongyang, particularly with regard to military technology, will give the unstable Kim regime more room and confidence. This has led to a significant acceleration of the development of nuclear weapons and missile programs, according to Liu Dongshu, an assistant professor of Chinese politics at the City University of Hong Kong.
+</p>
+<p>Beijing concerns that a total collapse of North Korea would allow the US to establish direct authority over the country, therefore Liu stated, "China aims to control the situation and prevent escalation, but it also does not want North Korea to completely collapse either."
+</p>
+<p>
+Russia, which has previously supported China's position on the matter to a great extent, now runs the risk of upsetting the delicate balance by pressing North Korea to aid in its grueling conflict in Ukraine.
+</p>
+<p>According to a US declaration in February, Russia has received more than 10,000 shipping containers from North Korea since September, which is the equivalent of 260,000 metric tons of weapons or material related to munitions. North Korea and Russia have both denied the assertion.
+</p>
+<p>
+Even though Beijing has avoided endorsing Kim's nuclear and missile programs and has refrained from giving direct military backing to Putin, the US has accused Beijing of selling dual-use items to Russia that fund the warring nation's military industrial complex.
+</p>
+<p>
+Liu stated, "It will be harder for China to control the situation on the Korean Peninsula if Putin gives more support to North Korea on nuclear issues, including some technical assistance."
+</p>
+<p>Kim and Putin inked a mutual defense pact that echoes a 1961 Cold War treaty between North Korea and the Soviet Union. After the fall of the Soviet Union, the agreement was replaced with one that provided even lower security guarantees.
+</p>
+<p>
+However, despite several extensions, North Korea and China's 1961 mutual defense treaty is still in effect.
+</p>
+<p>
+The only formal military alliance treaty China has signed with another nation is the Sino-North Korea Treaty of Friendship, Cooperation and Mutual Assistance, though Beijing willfully refuses to acknowledge it as such and raises no clear indications as to whether China must automatically defend North Korea in the event of war.
+</p>
+<p>In a similar vein, it's still unclear what the new defense pact will allow Russia and North Korea to do for one another.
+</p>
+<p>
+The signing of the new pact coincides with heightened tensions on the Korean Peninsula as Kim has abandoned a long-standing strategy of pursuing peaceful reunification with South Korea in favor of more aggressive rhetoric. The two Koreas remained officially at war after the Korean War ended in 1953 because a formal peace treaty was never signed between them.
+</p>
+<p>
+However, the pact's political message is very apparent. Motivated by a mutual animosity towards the United States and its allies, the two authoritarian countries aim to weaken and establish a substitute for the global order spearheaded by the West — a goal that China also shares.
+</p>
+<img src="assets/imgs/news/xi.JPG" alt="" width="100%" height="100%">
+<p>Similar jabs at the US were taken by Putin and Xi during the Russian president's visit to Beijing one month prior. The two "old friends" promised to cooperate in opposing the "global security system" that they defined as being based on military alliances sponsored by the US in a broad joint statement.
+</p>
+<p>
+A top US military general recently compared China, Russia, North Korea, and Iran to a new “axis of evil,” and Western commentators have cautioned against this loose but growing coordination of interests.
+</p>
+<p>
+Beijing would be careful to maintain a distance while Moscow and Pyongyang strengthen their cooperation, Liu said, adding that "China certainly doesn't want to be seen as part of a new Axis."
+</p>
+<p>China, however, would have been the unspoken but significant issue at Putin and Kim's encounter.
+</p>
+<p>
+According to Edward Howell, a politics lecturer at the University of Oxford in the United Kingdom who specializes on the Korean Peninsula, "any such meeting will also include discussion of China."
+</p>
+<p>
+"Russia will be well aware that China is far more significant to North Korea than Russia is, and that China does not wish to be excluded from any meaningful negotiations involving North Korea."
+</p>
+<p>
+China does not believe it can control the speed or scope of the increasing level of cooperation between North Korea and Russia, according to Yun Sun, head of the Stimson Center's China program, a think tank located in Washington.
+</p>
+<p>However, she added, "they are aware of the indispensable role China plays for both North Korea and Russia."
+</p>
+<p>
+China continues to be North Korea's and Russia's biggest trading partner, offering a vital lifeline to the severely sanctioned economies. Beijing also provides the two worldwide outcasts with a substantial amount of political assistance and diplomatic cover.
+</p>
+<p>
+According to Liu of the City University of Hong Kong, "China doesn't think that an alliance between Russia and North Korea would be a betrayal."
+</p>
+<p>
+"Neither of the two nations is capable of betraying China. Even with their partnership, they are still dependent on China.
+</p>
+</div>
+</div>
+	<?php include('sociallink.php') ?>
+	<?php include('relatedpost.php') ?>
+</div>
+<?php include('rightside.php') ?></div>
+</div>
+</main>
+<?php include('footer.php') ?>
+</div>
+<div class=dark-mark></div>
+<script src=assets/js/vendor/modernizr-3.6.0.min.js></script>
+<script src=assets/js/vendor/jquery-3.6.0.min.js></script>
+<script src=assets/js/vendor/popper.min.js></script>
+<script src=assets/js/vendor/bootstrap.min.js></script>
+<script src=assets/js/vendor/jquery.slicknav.js></script>
+<script src=assets/js/vendor/owl.carousel.min.js></script>
+<script src=assets/js/vendor/slick.min.js></script>
+<script src=assets/js/vendor/wow.min.js></script>
+<script src=assets/js/vendor/animated.headline.js></script>
+<script src=assets/js/vendor/jquery.magnific-popup.js></script>
+<script src=assets/js/vendor/jquery.ticker.js></script>
+<script src=assets/js/vendor/jquery.vticker-min.js></script>
+<script src=assets/js/vendor/jquery.scrollUp.min.js></script>
+<script src=assets/js/vendor/jquery.nice-select.min.js></script>
+<script src=assets/js/vendor/jquery.sticky.js></script>
+<script src=assets/js/vendor/perfect-scrollbar.js></script>
+<script src=assets/js/vendor/waypoints.min.js></script>
+<script src=assets/js/vendor/jquery.counterup.min.js></script>
+<script src=assets/js/vendor/jquery.theia.sticky.js></script>
+<script src=../../ionicons%405.0.0/dist/ionicons.js></script>
+<script src=assets/js/main.js></script>
+</body>
+</html>
