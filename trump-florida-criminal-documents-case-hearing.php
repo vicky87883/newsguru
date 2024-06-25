23fcb2b2e43e1f1e0af7d67c2d4d793f0faeac59
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-25 11:05:02 +0530

    commit updated

---
+++ trump-florida-criminal-documents-case-hearing.php
@@ -0,0 +1,299 @@
+<?php
+require_once('dbcon.php');
+$query = "SELECT * FROM `frontload`   ORDER BY `id` DESC;";
+$query2 = "SELECT * FROM `mostpopular`   ORDER BY `id` DESC;";
+$query3 = "SELECT * FROM `dontmiss`   ORDER BY `id` DESC;";
+$query4 = "SELECT * FROM `toptrending`   ORDER BY `id` DESC;";
+$result = mysqli_query($con,$query);
+$result2 = mysqli_query($con,$query2);
+$result3 = mysqli_query($con,$query3);
+$result4 = mysqli_query($con,$query4);
+?><!DOCTYPE html>
+<html class=no-js lang=en>
+<head>
+<meta charset=utf-8>
+<meta http-equiv=x-ua-compatible content="ie=edge">
+<title>Will a new gag order be issued for Trump? Jack Smith is hopeful that it will be approved by Judge Aileen Cannon.</title>
+<meta name=news_keywords content="Will a new gag order be issued for Trump? Jack Smith is hopeful that it will be approved by Judge Aileen Cannon.
+" itemprop="keywords"/>
+<meta name=description content="Will a new gag order be issued for Trump? Jack Smith is hopeful that it will be approved by Judge Aileen Cannon.
+" itemprop="description"/>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<link rel=canonical href="https://www.newsguru.live/trump-florida-criminal-documents-case-hearing.php" />
+<link rel="shortcut icon" type=image/x-icon href=assets/imgs/favicon.svg>
+<link rel=stylesheet href=assets/css/style.css>
+<link rel=stylesheet href=assets/css/widgets.css>
+<link rel=stylesheet href=assets/css/color.css>
+<link rel=stylesheet href=assets/css/responsive.css>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V6HH2RKGTW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V6HH2RKGTW');
+</script>
+</head>
+<body>
+<div id=preloader-active>
+<div class="preloader d-flex align-items-center justify-content-center">
+<div class="preloader-inner position-relative">
+<div class=text-center>
+<img class="jump mb-50" src=assets/imgs/loading.svg alt>
+<h6>Now Loading</h6>
+<div class=loader>
+<div class="bar bar1"></div>
+<div class="bar bar2"></div>
+<div class="bar bar3"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class=main-wrap>
+<aside id=sidebar-wrapper class="custom-scrollbar offcanvas-sidebar position-right">
+<button class=off-canvas-close><i class=ti-close></i></button>
+<div class=sidebar-inner>
+<div class="siderbar-widget mb-50 mt-30">
+<form action=# method=get class="search-form position-relative">
+<input type=text class=search_field placeholder=Search value name=s>
+<span class=search-icon><i class="ti-search mr-5"></i></span>
+</form>
+</div>
+<div class="sidebar-widget mb-50">
+<div class="widget-header mb-30">
+<h5 class=widget-title>Top <span>Trending</span></h5>
+</div>
+<div class=post-aside-style-2>
+<ul class=list-post>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-2.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Vancouver woman finds pictures and videos of herself online</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>K. Marry</a></span>
+<span class=post-on>4m ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-3.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>4 Things Emotionally Intelligent People Don’t Do</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Mr. John</a></span>
+<span class=post-on>3h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-5.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Reflections from a Token Black Friend</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Kenedy</a></span>
+<span class=post-on>4h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-7.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>How to Identify a Smart Person in 3 Minutes</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Steven</a></span>
+<span class=post-on>5h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-8.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Blackface Minstrel Songs Don’t Belong in Children’s Music Class</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>J.Smith</a></span>
+<span class=post-on>5h30 ago</span>
+</div>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<div class="sidebar-widget widget_tag_cloud mb-50">
+<div class="widget-header tags-close mb-20">
+<h5 class="widget-title mt-5">Tags Cloud</h5>
+</div>
+<div class=tagcloud>
+<a href=#>Beauty</a>
+<a href=#>Book</a>
+<a href=#>Design</a>
+<a href=#>Fashion</a>
+<a href=#>Lifestyle</a>
+<a href=#>Travel</a>
+<a href=#>Science</a>
+<a href=#>Health</a>
+<a href=#>Sports</a>
+<a href=#>Arts</a>
+<a href=#>Books</a>
+<a href=#>Style</a>
+</div>
+</div>
+<div class="sidebar-widget widget-ads mb-30">
+<div class="widget-header tags-close mb-20">
+<h5 class="widget-title mt-5">Your Ads Here</h5>
+</div>
+<a href=assets/imgs/news-1.jpg class=play-video data-animate=zoomIn data-duration=1.5s data-delay=0.1s>
+<img class=border-radius-10 src=assets/imgs/ads-1.jpg alt>
+</a>
+</div>
+</div>
+</aside>
+<?php include('header.php') ?>
+<main class=position-relative>
+<div class=container>
+<div class="entry-header entry-header-1 mb-30 mt-50">
+<div class="entry-meta meta-0 font-small mb-30"><a href=#><span class="post-cat bg-success color-white">Global News</span></a></div>
+<h1 class="post-title mb-30">
+Will a new gag order be issued for Trump? Jack Smith is hopeful that it will be approved by Judge Aileen Cannon.
+</h1>
+
+<div class="entry-meta meta-1 font-x-small color-grey text-uppercase">
+<span class=post-by>By <a href=#>Newsguru Editors</a></span>
+<span class=post-on>8:03 am
+Saturday, 22 June 2024</span>
+<span class=time-reading>8 mins read</span>
+</div>
+</div>
+<div class="row mb-50">
+<div class="col-lg-8 col-md-12">
+<div class="single-social-share single-sidebar-share mt-30">
+<ul>
+<li><a class="social-icon facebook-icon text-xs-center" target=_blank href=#><i class=ti-facebook></i></a></li>
+<li><a class="social-icon twitter-icon text-xs-center" target=_blank href=#><i class=ti-twitter-alt></i></a></li>
+<li><a class="social-icon pinterest-icon text-xs-center" target=_blank href=#><i class=ti-pinterest></i></a></li>
+<li><a class="social-icon instagram-icon text-xs-center" target=_blank href=#><i class=ti-instagram></i></a></li>
+<li><a class="social-icon linkedin-icon text-xs-center" target=_blank href=#><i class=ti-linkedin></i></a></li>
+<li><a class="social-icon email-icon text-xs-center" target=_blank href=#><i class=ti-email></i></a></li>
+</ul>
+</div>
+<div class="bt-1 border-color-1 mb-30"></div>
+<figure class="single-thumnail mb-30">
+<img src=assets/imgs/news/trump.JPG alt=blog width=100%>
+<div class="credit mt-15 font-small color-grey">
+<i class="ti-credit-card mr-5"></i><span>Image credit: additional resources.</span>
+</div>
+</figure>
+<div class=single-excerpt>
+<p>When the government stated that former President Donald Trump should be placed under gag order after he claimed that President Joe Biden was "locked & loaded" to kill him during an FBI raid of Trump's Mar-a-Lago club, a federal judge in Florida expressed skepticism on Monday.
+</p>
+<p>
+"Joe Biden was prepared to take me out and endanger my family," Trump stated in an email used for campaign fundraising. In his request for the gag order in the federal criminal case in Florida, where Trump is accused of purposefully holding onto material related to national defense, special counsel Jack Smith mentioned only a few utterances, including that one.
+</p>
+<p>Trump referred to the search warrant's language, which said that force could not be used unless there was a good reason to believe the target presented a "imminent danger of death or serious physical injury." That is "routine practice to restrict the use of force," according to Smith's legal team, and the raid was planned for a time when Trump and his family would not be there.
+</p>
+<p>
+In a different fundraising email, Trump added, "They're just itching to do the unthinkable" and stated that it had been "revealed that Biden’s DOJ was authorized to use DEADLY FORCE for their DESPICABLE raid in Mar-a-Lago."
+</p>
+<p>During the hearing, Judge Aileen Cannon expressed her confusion about how Trump's remarks could be interpreted as a threat, considering that FBI agent names are blacked in court documents. According to prosecutor David Harbach, certain names are now known.
+</p>
+<p>Get ready for the polls by using our voter guide to compare the positions of the presidential candidates on important issues.
+</p>
+<p>
+Emil Bove, the attorney for Trump, implied on Monday that Smith's request for a gag order against Trump was motivated by politics in advance of his debate with Biden on Thursday. Bove expressed his dissatisfaction with Smith's excessive independence from Biden's Justice Department during a separate hearing in the case on Friday. Smith was appointed special counsel by Attorney General Merrick Garland and was granted more latitude in the case involving secret documents.
+</p>
+<p>During the hearing, Harbach described Trump's remarks regarding the raid as "flagrantly false" and claimed that the president frequently exhorted his supporters to act on his complaints.
+</p>
+<div><strong>What would the proposed gag order do?</strong></div>
+<p>Smith was designated as special counsel by Garland to oversee two distinct investigations against Trump: one pertaining to the classified materials, and the other to Trump's attempts to rig the 2020 election. Compared to ordinary federal prosecutors, special counsels have greater independence from the leadership of the Justice Department.
+</p>
+<p>In the end, Smith filed charges against Trump in both instances.
+
+Cannon is being asked by the government to issue an injunction barring Trump from making any public remarks "that pose a significant, imminent, and foreseeable danger to the law enforcement agents participating in the investigation and prosecution of this case."
+
+</p>
+<p>
+The government contended in a document filed before to the hearing that "these deceptive and inflammatory claims expose the law enforcement professionals who are involved in this case to unjustified and unacceptable risks."
+</p>
+<p>"Predictably and as he certainly intended, others have amplified Trump’s misleading statements, falsely characterizing the inclusion of the entirely standard use-of-force policy as an effort to 'assassinate' Trump," according to the government.
+
+Trump has maintained that the proposed gag order would infringe upon his right to free expression and, specifically, hinder his ability to communicate with supporters during the presidential campaign.
+</p>
+<p>In at least three other cases—including his federal criminal prosecution involving election meddling and his criminal hush money case in New York—Trump has already been subject to gag orders. After Trump's trial conviction, the judge in his New York case is currently deliberating over a move by the businessman to remove the gag order. In that instance, Trump's sentence is set for July 11.
+</p>
+<div><strong>Trump challenges Jack Smith's funding</strong></div>
+<p>During the Monday session, Trump's attorney Emil Bove contested the financing for Jack Smith's case prosecution as well. Bove noted that legislative cash allocated in the 1970s for independent investigations in politically sensitive areas should not be used by the Justice Department.
+</p>
+<p>
+At the hearing, Bove informed Cannon that "more oversight from Congress is required for the extraordinary things that are going on in these prosecutions."
+</p>
+<div><strong>Three days of hearings</strong></div>
+<p>The case's arguments will be heard over the course of three consecutive business days, starting on Monday.</p>
+<p>Cannon gave credence to Trump's contention on Friday that the case ought to be dropped as special counsel Jack Smith lacked the necessary authority to file it. According to Trump's legal team, the attorney general cannot name a special counsel for an inquiry without the Senate's consent.
+</p>
+<p>Cannon didn't make a ruling on that argument Friday. The judge, who was appointed by Trump and has drawn criticism over friendly rulings to the former president that experts said departed from legal norms, is also holding a closed hearing in the case Tuesday to consider Trump's argument that the search was unconstitutional and therefore seized documents should be excluded from the case.</p>
+</div>
+	<?php include('sociallink.php') ?>
+	<?php include('relatedpost.php') ?>
+</div>
+<?php include('rightside.php') ?></div>
+</div>
+	
+</main>
+<?php include('footer.php') ?>
+</div>
+<div class=dark-mark></div>
+<script src=assets/js/vendor/modernizr-3.6.0.min.js></script>
+<script src=assets/js/vendor/jquery-3.6.0.min.js></script>
+<script src=assets/js/vendor/popper.min.js></script>
+<script src=assets/js/vendor/bootstrap.min.js></script>
+<script src=assets/js/vendor/jquery.slicknav.js></script>
+<script src=assets/js/vendor/owl.carousel.min.js></script>
+<script src=assets/js/vendor/slick.min.js></script>
+<script src=assets/js/vendor/wow.min.js></script>
+<script src=assets/js/vendor/animated.headline.js></script>
+<script src=assets/js/vendor/jquery.magnific-popup.js></script>
+<script src=assets/js/vendor/jquery.ticker.js></script>
+<script src=assets/js/vendor/jquery.vticker-min.js></script>
+<script src=assets/js/vendor/jquery.scrollUp.min.js></script>
+<script src=assets/js/vendor/jquery.nice-select.min.js></script>
+<script src=assets/js/vendor/jquery.sticky.js></script>
+<script src=assets/js/vendor/perfect-scrollbar.js></script>
+<script src=assets/js/vendor/waypoints.min.js></script>
+<script src=assets/js/vendor/jquery.counterup.min.js></script>
+<script src=assets/js/vendor/jquery.theia.sticky.js></script>
+<script src=../../ionicons%405.0.0/dist/ionicons.js></script>
+<script src=assets/js/main.js></script>
+</body>
+</html>
