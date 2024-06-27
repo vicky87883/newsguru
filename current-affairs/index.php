103b24f7b9f296811d6ebd551c99a872afdd9b71
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-27 08:15:45 +0530

    commit updated

--- current-affairs/index.php
+++ current-affairs/index.php
@@ -13,7 +13,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 // Fetch the articles from the database
 $sql = "SELECT id, heading, text FROM article";
 $result = $conn->query($sql);
