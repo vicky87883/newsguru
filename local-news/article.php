e0baab39aa65965b910ae4cdaf89c6ecd8353fea
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-27 11:12:30 +0530

    commit updated

--- local-news/article.php
+++ local-news/article.php
@@ -58,28 +58,281 @@ if (!$article) {
 }
 
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html class=no-js lang=en>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo htmlspecialchars($article['heading']); ?></title>
-    <link rel="stylesheet" href="styles.css">
+<meta charset=utf-8>
+<meta http-equiv=x-ua-compatible content="ie=edge">
+<title><?php echo htmlspecialchars($article['heading']); ?></title>
+<meta name=news_keywords content="<?php echo htmlspecialchars($article['heading']); ?>" itemprop="keywords"/>
+<meta name=description content="<?php echo htmlspecialchars($article['heading']); ?>" itemprop="description"/>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<link rel=canonical href="https://www.newsguru.live/local-news/<?php echo htmlspecialchars($article['heading']); ?>" />
+<link rel="shortcut icon" type=image/x-icon href=assets/imgs/favicon.svg>
+<link rel=stylesheet href=assets/css/style.css>
+<link rel=stylesheet href=assets/css/widgets.css>
+<link rel=stylesheet href=assets/css/color.css>
+<link rel=stylesheet href=assets/css/responsive.css>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V6HH2RKGTW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V6HH2RKGTW');
+</script>
 </head>
 <body>
-    <div class="article-content">
-        <?php if ($article): ?>
-            <h1><?php echo htmlspecialchars($article['heading']); ?></h1>
-            <p><?php echo nl2br(htmlspecialchars($article['content'])); ?></p>
-        <?php else: ?>
-            <p>Article not found.</p>
-        <?php endif; ?>
-    </div>
+<div id=preloader-active>
+<div class="preloader d-flex align-items-center justify-content-center">
+<div class="preloader-inner position-relative">
+<div class=text-center>
+<img class="jump mb-50" src=assets/imgs/loading.svg alt>
+<h6>Now Loading</h6>
+<div class=loader>
+<div class="bar bar1"></div>
+<div class="bar bar2"></div>
+<div class="bar bar3"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class=main-wrap>
+<aside id=sidebar-wrapper class="custom-scrollbar offcanvas-sidebar position-right">
+<button class=off-canvas-close><i class=ti-close></i></button>
+<div class=sidebar-inner>
+<div class="siderbar-widget mb-50 mt-30">
+<form action=# method=get class="search-form position-relative">
+<input type=text class=search_field placeholder=Search value name=s>
+<span class=search-icon><i class="ti-search mr-5"></i></span>
+</form>
+</div>
+<div class="sidebar-widget mb-50">
+<div class="widget-header mb-30">
+<h5 class=widget-title>Top <span>Trending</span></h5>
+</div>
+<div class=post-aside-style-2>
+<ul class=list-post>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-2.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Vancouver woman finds pictures and videos of herself online</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>K. Marry</a></span>
+<span class=post-on>4m ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-3.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>4 Things Emotionally Intelligent People Don’t Do</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Mr. John</a></span>
+<span class=post-on>3h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-5.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Reflections from a Token Black Friend</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Kenedy</a></span>
+<span class=post-on>4h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-7.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>How to Identify a Smart Person in 3 Minutes</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Steven</a></span>
+<span class=post-on>5h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-8.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Blackface Minstrel Songs Don’t Belong in Children’s Music Class</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>J.Smith</a></span>
+<span class=post-on>5h30 ago</span>
+</div>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<div class="sidebar-widget widget_tag_cloud mb-50">
+<div class="widget-header tags-close mb-20">
+<h5 class="widget-title mt-5">Tags Cloud</h5>
+</div>
+<div class=tagcloud>
+<a href=#>Beauty</a>
+<a href=#>Book</a>
+<a href=#>Design</a>
+<a href=#>Fashion</a>
+<a href=#>Lifestyle</a>
+<a href=#>Travel</a>
+<a href=#>Science</a>
+<a href=#>Health</a>
+<a href=#>Sports</a>
+<a href=#>Arts</a>
+<a href=#>Books</a>
+<a href=#>Style</a>
+</div>
+</div>
+
+</div>
+</aside>
+<?php include('header.php') ?>
+<main class="position-relative article-content">
+<div class=container>
+<div class="entry-header entry-header-1 mb-30 mt-50">
+<div class="entry-meta meta-0 font-small mb-30"><a href=#><span class="post-cat bg-success color-white">Global News</span></a></div>
+<h1 class="post-title mb-30">
+Train berth falls on Kerala man, who dies; Railways maintains 'no problem in seat'
+</h1>
+<div class="entry-meta meta-1 font-x-small color-grey text-uppercase">
+<span class=post-by>By <a href=#>Newsguru Editors</a></span>
+<span class=post-on>7:56 am
+Thursday, 27 June 2024</span>
+<span class=time-reading>5 mins read</span>
+</div>
+</div>
+<div class="row mb-50">
+<div class="col-lg-8 col-md-12">
+<div class="single-social-share single-sidebar-share mt-30">
+<ul>
+<li><a class="social-icon facebook-icon text-xs-center" target=_blank href=#><i class=ti-facebook></i></a></li>
+<li><a class="social-icon twitter-icon text-xs-center" target=_blank href=#><i class=ti-twitter-alt></i></a></li>
+<li><a class="social-icon pinterest-icon text-xs-center" target=_blank href=#><i class=ti-pinterest></i></a></li>
+<li><a class="social-icon instagram-icon text-xs-center" target=_blank href=#><i class=ti-instagram></i></a></li>
+<li><a class="social-icon linkedin-icon text-xs-center" target=_blank href=#><i class=ti-linkedin></i></a></li>
+<li><a class="social-icon email-icon text-xs-center" target=_blank href=#><i class=ti-email></i></a></li>
+</ul>
+</div>
+<div class="bt-1 border-color-1 mb-30"></div>
+<figure class="single-thumnail mb-30">
+<img src=assets/imgs/news/train.JPG alt=blog width=100%>
+<div class="credit mt-15 font-small color-grey">
+<i class="ti-credit-card mr-5"></i><span>Image credit: additional resources.</span>
+</div>
+</figure>
+<div class=single-excerpt>
+	<p><mark>An upper berth collapsed on a sixty-two-year-old man as he slept. According to an explanation released by India Railways, the event happened because the seat was not securely chained.
+	</mark></p>
+	<p>After a media story claiming that a man traveling from Kerala to New Delhi died when an occupied upper berth fell on him during his train travel, Indian Railways released an explanation on Wednesday.
+</p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<p>
+The event happened last week as Ali Khan, a 62-year-old passenger, was traveling in the Ernakulam-Hazrat Nizamuddin Millennium Superfast Express (12645) sleeper berth.
+</p>
+<p>Khan was resting on the lower berth when the upper berth, which was occupied by another passenger, suddenly collapsed upon him. Khan was seriously injured by the collision between the passenger's weight and the collapsing berth.
+</p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<p>Railway authorities quickly took him to a hospital in Hyderabad. However, while having surgery, he died from his injuries.
+</p>
+<p>
+A few days after the occurrence, India Railways released an explanation stating that the accident was not brought on by a closed berth. According to the railways, the accident occurred because the passenger had not correctly closed the upper berth using the chains, which was discovered when the berth in issue was inspected by investigators.
+</p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<p>"The individual in question was seated in the lower berth of seat number 57 on the S/6 coach. Because the upper berth's chain was not positioned correctly, the upper berth seat collapsed. A spokesman for the Railway Ministry issued an official explanation, saying, "The seat fell down due to improper chaining of the upper berth by passenger."
+</p>
+<p>
+On the other hand, the station master on duty at Ramagundam station received a communication at approximately 6:34 p.m. (15.6.2024), and promptly arranged for 108 Ambulance to provide aid. This resulted in an unscheduled stoppage of the train at Ramagundam. The passenger was moved from the coach to the ambulance and given a hospital admission nearby, according to the statement.
+</p>
+</div>
+	<?php include('../sociallink.php') ?>
+	<?php include('../relatedpost.php') ?>
+</div>
+<?php include('../rightside.php') ?></div>
+</div>
+	
+</main>
+<?php include('../footer.php') ?>
+</div>
+<div class=dark-mark></div>
+<script src=assets/js/vendor/modernizr-3.6.0.min.js></script>
+<script src=assets/js/vendor/jquery-3.6.0.min.js></script>
+<script src=assets/js/vendor/popper.min.js></script>
+<script src=assets/js/vendor/bootstrap.min.js></script>
+<script src=assets/js/vendor/jquery.slicknav.js></script>
+<script src=assets/js/vendor/owl.carousel.min.js></script>
+<script src=assets/js/vendor/slick.min.js></script>
+<script src=assets/js/vendor/wow.min.js></script>
+<script src=assets/js/vendor/animated.headline.js></script>
+<script src=assets/js/vendor/jquery.magnific-popup.js></script>
+<script src=assets/js/vendor/jquery.ticker.js></script>
+<script src=assets/js/vendor/jquery.vticker-min.js></script>
+<script src=assets/js/vendor/jquery.scrollUp.min.js></script>
+<script src=assets/js/vendor/jquery.nice-select.min.js></script>
+<script src=assets/js/vendor/jquery.sticky.js></script>
+<script src=assets/js/vendor/perfect-scrollbar.js></script>
+<script src=assets/js/vendor/waypoints.min.js></script>
+<script src=assets/js/vendor/jquery.counterup.min.js></script>
+<script src=assets/js/vendor/jquery.theia.sticky.js></script>
+<script src=../../ionicons%405.0.0/dist/ionicons.js></script>
+<script src=assets/js/main.js></script>
 </body>
 </html>
-
-<?php
-// Close connection
-$conn->close();
-?>
