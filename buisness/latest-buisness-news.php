63cc71c10707e9117b51e556a8ba36530c867a58
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-28 20:38:41 +0530

    commit updated

--- buisness/latest-buisness-news.php
+++ buisness/latest-buisness-news.php
@@ -1,4 +1,10 @@
-
+<?php
+require_once('dbcon.php');
+$query2 = "SELECT * FROM `buisness`   ORDER BY `id` DESC;";
+$result = mysqli_query($con,$query);
+$result2 = mysqli_query($con,$query2);
+?>
+<?php include "script.php" ?> 
 <!DOCTYPE html>
 <html class=no-js lang=en>
 <head>
