1c90f47a95fb03deed247a50fc9ac3ddca671d3b
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-29 22:38:46 +0530

    commit updated

--- buisness/latest-buisness-news.php
+++ buisness/latest-buisness-news.php
@@ -276,6 +276,7 @@ $result1 = mysqli_query($con,$query1);
                         {
                             ?>
     <a class="font-small text-muted" href="<?php echo ($row['link']); ?>" style="color:blue !important;"><i class="fa-solid fa-link"></i><?php echo ($row['hlink']); ?></a>
+    <br>
     <?php
                         }
                             ?>
