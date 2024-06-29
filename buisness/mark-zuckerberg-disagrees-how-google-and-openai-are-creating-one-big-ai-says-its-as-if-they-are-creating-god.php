1de77647a23c3b200c76f201f40ea128a8d5f0f7
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-29 22:01:04 +0530

    commit updated

---
+++ buisness/mark-zuckerberg-disagrees-how-google-and-openai-are-creating-one-big-ai-says-its-as-if-they-are-creating-god.php
@@ -0,0 +1,254 @@
+<?php
+require_once('dbcon.php');
+$query2 = "SELECT * FROM `dontmiss`   ORDER BY `id` DESC;";
+$query = "SELECT * FROM `frontload`   ORDER BY `id` DESC;";
+$result = mysqli_query($con,$query);
+$result2 = mysqli_query($con,$query2);
+?>
+<!DOCTYPE html>
+<html class=no-js lang=en>
+<head>
+<meta charset=utf-8>
+<meta http-equiv=x-ua-compatible content="ie=edge">
+<title>Mark Zuckerberg claims that Google and OpenAI are building God, and he disagrees that they are constructing a single, massive AI.
+</title>
+<meta name=news_keywords content="Mark Zuckerberg claims that Google and OpenAI are building God, and he disagrees that they are constructing a single, massive AI.
+" itemprop="keywords"/>
+<meta name=description content="Mark Zuckerberg claims that Google and OpenAI are building God, and he disagrees that they are constructing a single, massive AI.
+" itemprop="description"/>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<link rel=canonical href="https://www.newsguru.live/buisness/mark-zuckerberg-disagrees-how-google-and-openai-are-creating-one-big-ai-says-its-as-if-they-are-creating-god.php" />
+<link rel="shortcut icon" type=image/x-icon href=../assets/imgs/favicon.svg>
+<link rel=stylesheet href=../assets/css/style.css>
+<link rel=stylesheet href=../assets/css/widgets.css>
+<link rel=stylesheet href=../assets/css/color.css>
+<link rel=stylesheet href=../assets/css/responsive.css>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V6HH2RKGTW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V6HH2RKGTW');
+</script>
+</head>
+<body>
+<div id=preloader-active>
+<div class="preloader d-flex align-items-center justify-content-center">
+<div class="preloader-inner position-relative">
+<div class=text-center>
+<img class="jump mb-50" src=../assets/imgs/loading.svg alt>
+<h6>Now Loading</h6>
+<div class=loader>
+<div class="bar bar1"></div>
+<div class="bar bar2"></div>
+<div class="bar bar3"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class=main-wrap>
+<aside id=sidebar-wrapper class="custom-scrollbar offcanvas-sidebar position-right">
+<button class=off-canvas-close><i class=ti-close></i></button>
+<div class=sidebar-inner>
+<div class="siderbar-widget mb-50 mt-30">
+<form action=# method=get class="search-form position-relative">
+<input type=text class=search_field placeholder=Search value name=s>
+<span class=search-icon><i class="ti-search mr-5"></i></span>
+</form>
+</div>
+<div class="sidebar-widget mb-50">
+<div class="widget-header mb-30">
+<h5 class=widget-title>Top <span>Trending</span></h5>
+</div>
+<div class=post-aside-style-2>
+<ul class=list-post>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-2.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Vancouver woman finds pictures and videos of herself online</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>K. Marry</a></span>
+<span class=post-on>4m ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-3.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>4 Things Emotionally Intelligent People Don’t Do</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Mr. John</a></span>
+<span class=post-on>3h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-5.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Reflections from a Token Black Friend</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Kenedy</a></span>
+<span class=post-on>4h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-7.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>How to Identify a Smart Person in 3 Minutes</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Steven</a></span>
+<span class=post-on>5h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=../assets/imgs/thumbnail-8.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Blackface Minstrel Songs Don’t Belong in Children’s Music Class</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>J.Smith</a></span>
+<span class=post-on>5h30 ago</span>
+</div>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<div class="sidebar-widget widget_tag_cloud mb-50">
+<div class="widget-header tags-close mb-20">
+<h5 class="widget-title mt-5">Tags Cloud</h5>
+</div>
+<div class=tagcloud>
+<a href=#>Beauty</a>
+<a href=#>Book</a>
+<a href=#>Design</a>
+<a href=#>Fashion</a>
+<a href=#>Lifestyle</a>
+<a href=#>Travel</a>
+<a href=#>Science</a>
+<a href=#>Health</a>
+<a href=#>Sports</a>
+<a href=#>Arts</a>
+<a href=#>Books</a>
+<a href=#>Style</a>
+</div>
+</div>
+</div>
+</aside>
+<?php include('header.php') ?>
+<main class=position-relative>
+<div class=container>
+<div class="entry-header entry-header-1 mb-30 mt-50">
+<div class="entry-meta meta-0 font-small mb-30"><a href=#><span class="post-cat bg-success color-white">Global News</span></a></div>
+<h1 class="post-title mb-30">
+First batch of "Nagastra-1" suicide drones delivered to the Indian Army heralds a new era in warfare.
+</h1>
+<div class="entry-meta meta-1 font-x-small color-grey text-uppercase">
+<span class=post-by>By Newsguru Editors</span>
+<span class=post-on>11:10 pm
+Saturday, 15 June 2024</span>
+<span class=time-reading>10 mins read</span>
+</div>
+</div>
+<div class="row mb-50">
+<div class="col-lg-8 col-md-12">
+<div class="single-social-share single-sidebar-share mt-30">
+<ul>
+<li><a class="social-icon facebook-icon text-xs-center" target=_blank href=#><i class=ti-facebook></i></a></li>
+<li><a class="social-icon twitter-icon text-xs-center" target=_blank href=#><i class=ti-twitter-alt></i></a></li>
+<li><a class="social-icon pinterest-icon text-xs-center" target=_blank href=#><i class=ti-pinterest></i></a></li>
+<li><a class="social-icon instagram-icon text-xs-center" target=_blank href=#><i class=ti-instagram></i></a></li>
+<li><a class="social-icon linkedin-icon text-xs-center" target=_blank href=#><i class=ti-linkedin></i></a></li>
+<li><a class="social-icon email-icon text-xs-center" target=_blank href=#><i class=ti-email></i></a></li>
+</ul>
+</div>
+<div class="bt-1 border-color-1 mb-30"></div>
+<figure class="single-thumnail mb-30">
+<img src=assets/imgs/news/mark.JPG alt=blog width=100%>
+<div class="credit mt-15 font-small color-grey">
+<i class="ti-credit-card mr-5"></i><span>Image credit: additional resources.</span>
+</div>
+</figure>
+<div class=single-excerpt>
+	<p>With the delivery of its first batch of "suicide drones," built in-house and named "Nagastra-1," the Indian Army has demonstrated the increasing significance of drone technology in contemporary conflict. With these advanced drones, India's defensive capabilities have advanced significantly. They are intended for precise strikes on adversary sites.
+</p>
+<p>
+Produced by Economic Explosives Limited (EEL), a Solar Industries subsidiary, the Nagastra-1 is a kind of "loitering munition." This implies that it has the ability to hover over a predetermined region in order to locate and lock onto targets in order to unleash a precise attack—even in midair.</p>
+<p>The drones have GPS-enabled navigation systems that allow for precise aiming within a 2-meter radius. They weigh about 9 kg. The Nagastra-1 can carry a 1 kg warhead for up to 30 minutes and cover up to 30 km with its updated version able to carry a 2.2 kg payload farther away.
+</p>
+<p>
+Most importantly, these drones can be reused. In the event that a mission is canceled or a target is not found, the Nagastra-1 can land safely by returning to base via a parachute recovery system.
+</p>
+<p>
+The purchase of the Nagastra-1 by the Indian Army fits with a global trend toward drone warfare. Drone attacks on Russian targets by Ukraine and maritime vessel attacks by the Houthi group in Yemen are two recent examples.
+</p>
+<p>India's recent agreement with the US to purchase armed MQ-9B SeaGuardian drones from General Atomics, which will be constructed in India, is additional evidence of the nation's reliance on drone technology and will improve its intelligence, surveillance, and reconnaissance (ISR) capabilities.</p>
+</div>
+<?php include('../sociallink.php') ?>
+<?php include('../relatedpost.php') ?>
+</div>
+	<?php include('dontmiss.php') ?>
+</div>
+</div>
+</main>
+<?php include('../footer.php') ?>
+</div>
+<div class=dark-mark></div>
+<script src=../assets/js/vendor/modernizr-3.6.0.min.js></script>
+<script src=../assets/js/vendor/jquery-3.6.0.min.js></script>
+<script src=../assets/js/vendor/popper.min.js></script>
+<script src=../assets/js/vendor/bootstrap.min.js></script>
+<script src=../assets/js/vendor/jquery.slicknav.js></script>
+<script src=../assets/js/vendor/owl.carousel.min.js></script>
+<script src=../assets/js/vendor/slick.min.js></script>
+<script src=../assets/js/vendor/wow.min.js></script>
+<script src=../assets/js/vendor/animated.headline.js></script>
+<script src=../assets/js/vendor/jquery.magnific-popup.js></script>
+<script src=../assets/js/vendor/jquery.ticker.js></script>
+<script src=../assets/js/vendor/jquery.vticker-min.js></script>
+<script src=../assets/js/vendor/jquery.scrollUp.min.js></script>
+<script src=../assets/js/vendor/jquery.nice-select.min.js></script>
+<script src=../assets/js/vendor/jquery.sticky.js></script>
+<script src=../assets/js/vendor/perfect-scrollbar.js></script>
+<script src=../assets/js/vendor/waypoints.min.js></script>
+<script src=../assets/js/vendor/jquery.counterup.min.js></script>
+<script src=../assets/js/vendor/jquery.theia.sticky.js></script>
+<script src=../../../ionicons%405.0.0/dist/ionicons.js></script>
+<script src=../assets/js/main.js></script>
+</body>
+</html>
