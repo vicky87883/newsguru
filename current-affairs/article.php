b33655b2ebe952b0a6198692ada370de9782ec42
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-30 11:22:15 +0530

    commit updated

--- current-affairs/article.php
+++ current-affairs/article.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
 // Database connection details
 $servername = "localhost";
 $username = "vikram";
--- current-affairs/index.php
+++ current-affairs/index.php
@@ -17,6 +17,8 @@ if ($conn->connect_error) {
 $sql = "SELECT id, heading, text FROM article";
 $result = $conn->query($sql);
 
+$sql2 = "SELECT hlink, link, FROM linker";
+$result = $conn->query($sql2);
 ?>
 <?php include('script.php') ?>
 <!DOCTYPE html>
@@ -272,16 +274,21 @@ $result = $conn->query($sql);
 <div class="col-lg-4 col-md-12 sidebar-right">
 <div class="sidebar-widget p-20 border-radius-15 bg-white widget-text wow fadeIn animated">
 <div class="widget-header mb-30">
-<h5 class=widget-title>Search <span>tips</span></h5>
+<h5 class=widget-title>Search <span>More News</span></h5>
 </div>
 <div>
-<h6>1. Use the tabs</h6>
-<p class="font-small text-muted">The first tip is to use the tabs in Google search. On the top of every search are a number of tabs. Usually you’ll see Web, Image, News, and More. Using these tabs, you can help define what kind of search you need to do.</p>
-<h6>2. Use quotes</h6>
-<p class="font-small text-muted">When searching for something specific, try using quotes to minimize the guesswork for Google search. When you put your search parameters in quotes, it tells the search engine to search for the whole phrase.</p>
-<h6>3. Use a hyphen to exclude words</h6>
-<p class="font-small text-muted">Sometimes you may find yourself searching for a word with an ambiguous meaning. An example is Mustang. When you Google search for Mustang, you may get results for both the car made by Ford or the horse. If you want to cut one out, use the hyphen to tell the engine to ignore content with one of the other. </p>
+<?php
+                        while($row = mysqli_fetch_assoc($result1))
+                        {
+                            ?>
+    <a class="font-small text-muted" href="<?php echo ($row['link']); ?>" style="color:blue !important;"><i class="fa-solid fa-link"></i><?php echo ($row['hlink']); ?></a>
+    <br>
+    <br>
+    <?php
+                        }
+                            ?>
 </div>
+
 </div>
 </div>
 </div>
