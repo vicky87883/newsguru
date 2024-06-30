c8cedf0c10559ee8f5c2de8d45004ebabeedd7cf
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-30 07:51:23 +0530

    commit updated

--- trending.php
+++ trending.php
@@ -17,6 +17,9 @@ $result = mysqli_query($con, $query);
 // Fetching data from the 'education' table
 $query2 = "SELECT * FROM `education` ORDER BY `id` DESC;";
 $result2 = mysqli_query($con, $query2);
+// Fetching data from the 'education' table
+$query3 = "SELECT * FROM `linker` ORDER BY `id` DESC;";
+$result3 = mysqli_query($con, $query3);
 ?>
 
 <?php include "script.php" ?>
@@ -138,20 +141,25 @@ $result2 = mysqli_query($con, $query2);
                                 </div>
                             </div>
                             <div class="col-lg-4 col-md-12 sidebar-right">
-                                <div class="sidebar-widget p-20 border-radius-15 bg-white widget-text wow fadeIn animated">
-                                    <div class="widget-header mb-30">
-                                        <h5 class="widget-title">Search <span>tips</span></h5>
-                                    </div>
-                                    <div>
-                                        <h6>1. Use the tabs</h6>
-                                        <p class="font-small text-muted">The first tip is to use the tabs in Google search. On the top of every search are a number of tabs. Usually you’ll see Web, Image, News, and More. Using these tabs, you can help define what kind of search you need to do.</p>
-                                        <h6>2. Use quotes</h6>
-                                        <p class="font-small text-muted">When searching for something specific, try using quotes to minimize the guesswork for Google search. When you put your search parameters in quotes, it tells the search engine to search for the whole phrase.</p>
-                                        <h6>3. Use a hyphen to exclude words</h6>
-                                        <p class="font-small text-muted">Sometimes you may find yourself searching for a word with an ambiguous meaning. An example is Mustang. When you Google search for Mustang, you may get results for both the car made by Ford or the horse. If you want to cut one out, use the hyphen to tell the engine to ignore content with one of the other.</p>
-                                    </div>
-                                </div>
-                            </div>
+<div class="sidebar-widget p-20 border-radius-15 bg-white widget-text wow fadeIn animated">
+<div class="widget-header mb-30">
+<h5 class=widget-title>Search <span>More News</span></h5>
+</div>
+<div>
+<?php
+                        while($row = mysqli_fetch_assoc($result3))
+                        {
+                            ?>
+    <a class="font-small text-muted" href="<?php echo ($row['link']); ?>" style="color:blue !important;"><i class="fa-solid fa-link"></i><?php echo ($row['hlink']); ?></a>
+    <br>
+    <br>
+    <?php
+                        }
+                            ?>
+</div>
+
+</div>
+</div>
                         </div>
                     </div>
                 </div>
