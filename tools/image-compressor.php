f2993e067e197c18f19398308ba7824733024409
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-07-03 22:14:37 +0530

    commit updated

--- tools/image-compressor.php
+++ tools/image-compressor.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>File Compression Tool</title>
+    <title>High Quality Image Compressor</title>
+    
+<meta name=news_keywords content="image compressor,image to 10kb,image to 20kb,image to 30kb,image to 40kb,image to 50kb,image to 100kb,image under 200kb" itemprop="keywords"/>
+<meta name=description content="Hight Quality image file compressor without losing pixel quality and easy download" itemprop="description"/>
+<link rel=canonical href="https://www.newsguru.live/tools/image-compressor" />
     <style>
         body {
             display: flex;
@@ -251,6 +255,16 @@
             }
         }
     </style>
+    	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V6HH2RKGTW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V6HH2RKGTW');
+</script>
 </head>
 <body>
     <div class="sidebar">
@@ -262,6 +276,15 @@
     </div>
     <div class="main-content">
         <h1>Compress Your Image</h1>
+        <ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
         <div class="upload-area">
             <div class="upload-btn-wrapper">
                 <button class="btn">Upload Image</button>
