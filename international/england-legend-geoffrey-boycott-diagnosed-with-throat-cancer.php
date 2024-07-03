e60c915e4210aa2beecbec0a02bfed377f8093db
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-07-03 09:22:07 +0530

    commit updated

---
+++ international/england-legend-geoffrey-boycott-diagnosed-with-throat-cancer.php
@@ -0,0 +1,146 @@
+<?php
+require_once('dbcon.php');
+$query = "SELECT * FROM `health`   ORDER BY `id` DESC;";
+$query2 = "SELECT * FROM `health`   ORDER BY `id` DESC;";
+$query3 = "SELECT * FROM `health`   ORDER BY `id` DESC;";
+$result = mysqli_query($con,$query);
+$result2 = mysqli_query($con,$query2);
+$result3 = mysqli_query($con,$query3);
+?><!DOCTYPE html>
+<html class=no-js lang=en>
+<head>
+<meta charset=utf-8>
+<meta http-equiv=x-ua-compatible content="ie=edge">
+<title>Legendary Englishman Geoffrey Boycott Has a Throat Cancer Diagnose</title>
+<meta name=news_keywords content="Legendary Englishman Geoffrey Boycott Has a Throat Cancer Diagnose" itemprop="keywords"/>
+<meta name=description content="Legendary Englishman Geoffrey Boycott Has a Throat Cancer Diagnose" itemprop="description"/>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<link rel=canonical href="https://www.newsguru.live/international/england-legend-geoffrey-boycott-diagnosed-with-throat-cancer.php" />
+<link rel="shortcut icon" type=image/x-icon href=assets/imgs/favicon.svg>
+<link rel=stylesheet href=assets/css/style.css>
+<link rel=stylesheet href=assets/css/widgets.css>
+<link rel=stylesheet href=assets/css/color.css>
+<link rel=stylesheet href=assets/css/responsive.css>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V6HH2RKGTW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V6HH2RKGTW');
+</script>
+<style>
+        /* Style for article paragraphs */
+        .single-excerpt p {
+            font-family: 'Georgia', serif; /* Use a serif font for readability */
+            font-size: 1.1rem; /* Slightly increase the font size */
+            line-height: 1.6; /* Increase line height for better readability */
+            margin-bottom: 1em; /* Space out paragraphs */
+            color: #333; /* Use a dark color for good contrast */
+        }
+
+        /* Optional: Adjust for different screen sizes */
+        @media (max-width: 768px) {
+            .single-excerpt p {
+                font-size: 1rem; /* Slightly smaller on mobile devices */
+                line-height: 1.5; /* Adjust line height for smaller screens */
+            }
+        }
+    </style>
+</head>
+<body>
+<div id=preloader-active>
+<div class="preloader d-flex align-items-center justify-content-center">
+<div class="preloader-inner position-relative">
+<div class=text-center>
+<img class="jump mb-50" src=assets/imgs/loading.svg alt>
+<h6>Now Loading</h6>
+<div class=loader>
+<div class="bar bar1"></div>
+<div class="bar bar2"></div>
+<div class="bar bar3"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class=main-wrap>
+<?php include('header.php') ?>
+<main class=position-relative>
+<div class=container>
+<div class="entry-header entry-header-1 mb-30 mt-50">
+<div class="entry-meta meta-0 font-small mb-30"><a href=#><span class="post-cat bg-success color-white">Health News</span></a></div>
+<h1 class="post-title mb-30">
+Legendary Englishman Geoffrey Boycott Has a Throat Cancer Diagnose
+</h1>
+<div class="entry-meta meta-1 font-x-small color-grey text-uppercase">
+<span class=post-by>By Newsguru Editors</span>
+<span class=post-on>9:08 am
+Wednesday, 3 July 2024</span>
+<span class=time-reading>5 mins read</span>
+</div>
+</div>
+<div class="row mb-50">
+<div class="col-lg-8 col-md-12">
+<div class="single-social-share single-sidebar-share mt-30">
+<ul>
+<li><a class="social-icon facebook-icon text-xs-center" target=_blank href=#><i class=ti-facebook></i></a></li>
+<li><a class="social-icon twitter-icon text-xs-center" target=_blank href=#><i class=ti-twitter-alt></i></a></li>
+<li><a class="social-icon pinterest-icon text-xs-center" target=_blank href=#><i class=ti-pinterest></i></a></li>
+<li><a class="social-icon instagram-icon text-xs-center" target=_blank href=#><i class=ti-instagram></i></a></li>
+<li><a class="social-icon linkedin-icon text-xs-center" target=_blank href=#><i class=ti-linkedin></i></a></li>
+<li><a class="social-icon email-icon text-xs-center" target=_blank href=#><i class=ti-email></i></a></li>
+</ul>
+</div>
+<div class="bt-1 border-color-1 mb-30"></div>
+<figure class="single-thumnail mb-30">
+<img src=assets/imgs/news/geofrey.JPG alt=blog width=80%>
+<div class="credit mt-15 font-small color-grey">
+<i class="ti-credit-card mr-5"></i><span>Image credit: additional resources.</span>
+</div>
+</figure>
+<div class=single-excerpt>
+	<p>The startling news is that Geoffrey Boycott, the former cricketer for England, has received a second diagnosis of throat cancer. The 83-year-old veteran of England issued a statement to confirm the situation. Boycott affirmed that he will have surgery to remove the malignancy from his throat. Boycott acknowledges the difficulties of receiving a fatal diagnosis at this stage of life, but he is still hopeful that he can recover.</p>
+	<p>After a few weeks of MRIs, CT scans, PET scans, and two biopsies, it has been determined that I have throat cancer and will need surgery. Experience has taught me that in order to survive cancer a second time, I will require top-notch medical care as well as a great deal of luck. Even in the event that the surgery is successful, every cancer patient must accept the chance that their disease may return. Thus, I'll just go ahead and cross my fingers," an official statement that The Telegraph cited said.
+	</p>
+	<p>At the age of sixty-two, Boycott received his cancer diagnosis in 2005 and was given a three-month prognosis. He then underwent rigorous chemotherapy treatments and overcame the fatal illnesses. Boycott stated in his book "The Corridor of Certainty" that his wife Rachael's support was the reason he was able to survive cancer.
+	</p>
+	<p>"It is truly a show-stopper to be handed three months to live. I'll never understand why I'm still here. The only thing I can truly say with certainty is that without my wife Rachael's support, I could not have made it through."
+	</p>
+	<p>Boycott earned over 8000 runs, including 22 hundreds, over his 108 Test appearances for England. In a 25-year career, he amassed an astounding 151 first-class centuries. Boycott joined the BBC's Test Match Special team upon his retirement from cricket, and he would eventually retire in 2020.
+	</p>
+</div>
+<?php include('../sociallink.php') ?>
+	<?php include('../relatedpost.php') ?>
+</div>
+<?php include('../rightside.php') ?></div>
+</div>
+</main>
+<?php include('../footer.php') ?>
+</div>
+<div class=dark-mark></div>
+<script src=assets/js/vendor/modernizr-3.6.0.min.js></script>
+<script src=assets/js/vendor/jquery-3.6.0.min.js></script>
+<script src=assets/js/vendor/popper.min.js></script>
+<script src=assets/js/vendor/bootstrap.min.js></script>
+<script src=assets/js/vendor/jquery.slicknav.js></script>
+<script src=assets/js/vendor/owl.carousel.min.js></script>
+<script src=assets/js/vendor/slick.min.js></script>
+<script src=assets/js/vendor/wow.min.js></script>
+<script src=assets/js/vendor/animated.headline.js></script>
+<script src=assets/js/vendor/jquery.magnific-popup.js></script>
+<script src=assets/js/vendor/jquery.ticker.js></script>
+<script src=assets/js/vendor/jquery.vticker-min.js></script>
+<script src=assets/js/vendor/jquery.scrollUp.min.js></script>
+<script src=assets/js/vendor/jquery.nice-select.min.js></script>
+<script src=assets/js/vendor/jquery.sticky.js></script>
+<script src=assets/js/vendor/perfect-scrollbar.js></script>
+<script src=assets/js/vendor/waypoints.min.js></script>
+<script src=assets/js/vendor/jquery.counterup.min.js></script>
+<script src=assets/js/vendor/jquery.theia.sticky.js></script>
+<script src=../../ionicons%405.0.0/dist/ionicons.js></script>
+<script src=assets/js/main.js></script>
+</body>
+</html>
---
+++ international/latest-health-and-fitness-news.php
@@ -0,0 +1,196 @@
+<?php
+require_once('dbcon.php');
+$query2 = "SELECT * FROM `health`   ORDER BY `id` DESC;";
+$result2 = mysqli_query($con,$query2);
+?>
+<?php include "script.php" ?>
+<!DOCTYPE html>
+<html class=no-js lang=en>
+<head>
+<meta charset=utf-8>
+<meta http-equiv=x-ua-compatible content="ie=edge">
+<title>Latest Health and Fitness Tips| Yoga | Workout | newsguru.live</title>
+<meta name=description content="Get the most recent updates on Health, Fitness, Diet-plan, Food news from around the world.">
+<meta name=viewport content="width=device-width, initial-scale=1">
+<link rel=manifest href=site.webmanifest>
+<link rel="shortcut icon" type=image/x-icon href=../assets/imgs/favicon.svg>
+<link rel=stylesheet href=../assets/css/style.css>
+<link rel=stylesheet href=../assets/css/widgets.css>
+<link rel=stylesheet href=../assets/css/color.css>
+<link rel=stylesheet href=../assets/css/responsive.css>
+	<script src="https://kit.fontawesome.com/353f0e393d.js" crossorigin="anonymous"></script>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755"
+     crossorigin="anonymous"></script>
+</head>
+<body>
+<div id=preloader-active>
+<div class="preloader d-flex align-items-center justify-content-center">
+<div class="preloader-inner position-relative">
+<div class=text-center>
+<img class="jump mb-50" src=../assets/imgs/loading.svg alt>
+<h6>Now Loading</h6>
+<div class=loader>
+<div class="bar bar1"></div>
+<div class="bar bar2"></div>
+<div class="bar bar3"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class=main-wrap>
+<?php include('header.php') ?>
+<main class=position-relative>
+<div class="archive-header text-center mb-50">
+<div class=container>
+<h2>
+<span class=text-success>Search result for "Health and Fitness News"</span>
+</h2>
+<div class=breadcrumb>
+<span class=no-arrow>we found latest articles for you</span>
+</div>
+</div>
+</div>
+<div class=container>
+<div class=row>
+<?php include('../sidebar.php') ?>
+<div class="col-lg-10 col-md-9 order-1 order-md-2">
+<div class="row mb-50">
+	
+<div class="col-lg-8 col-md-12">
+<div class="latest-post mb-50">
+<div class=loop-list-style-1>
+	<?php
+                        while($row = mysqli_fetch_assoc($result))
+                        {
+                            ?>
+<article class="p-10 background-white border-radius-10 mb-30 wow fadeIn animated">
+	
+	
+<div class="d-md-flex d-block">
+<div class="post-thumb post-thumb-big d-flex mr-15 border-radius-15 img-hover-scale">
+<a class=color-white href="<?php echo ($row['link']); ?>">
+<img class=border-radius-15 src="<?php echo ($row['image']); ?>" alt>
+</a>
+</div>
+<div class="post-content media-body">
+<div class="entry-meta mb-15 mt-10">
+<a class="entry-meta meta-2" href=#><span class="post-in text-danger font-x-small"><?php echo ($row['tag']); ?></span></a>
+</div>
+<h5 class="post-title mb-15 text-limit-2-row">
+<span class=post-format-icon>
+	<i class="fa-solid fa-notes-medical"></i>
+</span>
+<a href="<?php echo ($row['link']); ?>"><?php echo ($row['heading']); ?></a></h5>
+<p class="post-exerpt font-medium text-muted mb-30 d-none d-lg-block"><?php echo ($row['text']); ?></p>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Newsguru Editors</a></span>
+<span class=post-on><?php echo ($row['time']); ?></span>
+<span class=time-reading>12 mins read</span>
+</div>
+</div>
+</div>
+</article>
+	<?php
+                        }
+
+                        ?>
+</div>
+</div>
+<div class="pagination-area mb-30">
+<nav aria-label="Page navigation example">
+<ul class="pagination justify-content-start">
+<?php
+    if(isset($_GET['page-nr']) && $_GET['page-nr'] > 1)
+    {
+        ?>
+<li> <a class=prev href="?page-nr=<?php echo $_GET['page-nr'] - 1?>">Previous</a></li>
+<?php
+    }
+    else{
+        ?>
+<li><a class=prev>Previous</a></li>
+<?php
+    }
+    ?>
+<?php
+        for( $counter= 1; $counter <= $pages; $counter++ )
+        {
+        ?>
+<li class="page-item active"><a href="?page-nr=<?php echo $counter ?>"><?php echo $counter ?></a></li>
+<?php
+
+        }
+        ?>
+<?php
+    if(!isset($_GET['page-nr']))
+    {
+        ?>
+	<li><a href="?page-nr=2" class=next>Next</a></li>
+<?php
+    }else{
+        if($_GET['page-nr']>=$pages)
+        {
+            ?>
+<li><a class=next>Next</a></li>
+<?php
+        }else{
+            ?>
+<li><a class=next href="?page-nr=<?php echo $_GET['page-nr'] + 1 ?>">Next</a></li>
+<?php
+        }
+    }
+    ?>
+</ul>
+</nav>
+</div>
+</div>
+<div class="col-lg-4 col-md-12 sidebar-right">
+<div class="sidebar-widget p-20 border-radius-15 bg-white widget-text wow fadeIn animated">
+<div class="widget-header mb-30">
+<h5 class=widget-title>Search <span>tips</span></h5>
+</div>
+<div>
+<h6>1. Use the tabs</h6>
+<p class="font-small text-muted">The first tip is to use the tabs in Google search. On the top of every search are a number of tabs. Usually you’ll see Web, Image, News, and More. Using these tabs, you can help define what kind of search you need to do.</p>
+<h6>2. Use quotes</h6>
+<p class="font-small text-muted">When searching for something specific, try using quotes to minimize the guesswork for Google search. When you put your search parameters in quotes, it tells the search engine to search for the whole phrase.</p>
+<h6>3. Use a hyphen to exclude words</h6>
+<p class="font-small text-muted">Sometimes you may find yourself searching for a word with an ambiguous meaning. An example is Mustang. When you Google search for Mustang, you may get results for both the car made by Ford or the horse. If you want to cut one out, use the hyphen to tell the engine to ignore content with one of the other. </p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</main>
+<?php include('../footer.php') ?>
+</div>
+<div class=dark-mark></div>
+<script src=../assets/js/vendor/modernizr-3.6.0.min.js></script>
+<script src=../assets/js/vendor/jquery-3.6.0.min.js></script>
+<script src=../assets/js/vendor/popper.min.js></script>
+<script src=../assets/js/vendor/bootstrap.min.js></script>
+<script src=../assets/js/vendor/jquery.slicknav.js></script>
+		<script src="https://kit.fontawesome.com/353f0e393d.js" crossorigin="anonymous"></script>
+
+<script src=../assets/js/vendor/owl.carousel.min.js></script>
+<script src=../assets/js/vendor/slick.min.js></script>
+<script src=../assets/js/vendor/wow.min.js></script>
+<script src=../assets/js/vendor/animated.headline.js></script>
+<script src=../assets/js/vendor/jquery.magnific-popup.js></script>
+<script src=../assets/js/vendor/jquery.ticker.js></script>
+<script src=../assets/js/vendor/jquery.vticker-min.js></script>
+<script src=../assets/js/vendor/jquery.scrollUp.min.js></script>
+<script src=../assets/js/vendor/jquery.nice-select.min.js></script>
+<script src=../assets/js/vendor/jquery.sticky.js></script>
+<script src=../assets/js/vendor/perfect-scrollbar.js></script>
+<script src=../assets/js/vendor/waypoints.min.js></script>
+<script src=../assets/js/vendor/jquery.counterup.min.js></script>
+<script src=../assets/js/vendor/jquery.theia.sticky.js></script>
+<script src=../../ionicons%405.0.0/dist/ionicons.js></script>
+<script src=../assets/js/main.js></script>
+	<script>function updateDateTime(){var d=new Date();var c=d.getDate();var b=d.toLocaleString("en-us",{weekday:"long"});var a=d.getHours();var e=d.getMinutes();var g=d.getSeconds();var f=d.toLocaleString("en-us",{month:"long"});a=(a<10?"0":"")+a;e=(e<10?"0":"")+e;g=(g<10?"0":"")+g;document.getElementById("date").innerHTML=c;document.getElementById("day").innerHTML=b;document.getElementById("time").innerHTML=a+":"+e+":"+g;document.getElementById("month").innerHTML=f}updateDateTime();setInterval(updateDateTime,1000);</script>
+</body>
+</html>
