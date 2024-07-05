fe7547cd722e5fb6c40a179bae076900dd4f8259
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-07-05 16:03:05 +0530

    commit updated

---
+++ anant-ambani-radhika-merchant-wedding-pop-sensation-justin-bieber-arrives-in-mumbai.php
@@ -0,0 +1,279 @@
+<?php
+require_once('dbcon.php');
+$query = "SELECT * FROM `frontload`   ORDER BY `id` DESC;";
+$query2 = "SELECT * FROM `mostpopular`   ORDER BY `id` DESC;";
+$query3 = "SELECT * FROM `dontmiss`   ORDER BY `id` DESC;";
+$query4 = "SELECT * FROM `toptrending`   ORDER BY `id` DESC;";
+$result = mysqli_query($con,$query);
+$result2 = mysqli_query($con,$query2);
+$result3 = mysqli_query($con,$query3);
+$result4 = mysqli_query($con,$query4);
+?>
+<!DOCTYPE html>
+<html class=no-js lang=en>
+
+<head>
+    <meta charset=utf-8>
+    <meta http-equiv=x-ua-compatible content="ie=edge">
+    <title>Anant Ambani and Radhika Merchant's Wedding Arrival of Justin Bieber in Mumbai
+    </title>
+    <meta name=news_keywords content="Anant Ambani and Radhika Merchant's Wedding Arrival of Justin Bieber in Mumbai
+"
+        itemprop="keywords" />
+    <meta name=description
+        content="Anant Ambani and Radhika Merchant's Wedding Arrival of Justin Bieber in Mumbai"
+        itemprop="description" />
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <link rel=canonical
+        href=https://www.newsguru.live/anant-ambani-radhika-merchant-wedding-pop-sensation-justin-bieber-arrives-in-mumbai.php />
+    <link rel="shortcut icon" type=image/x-icon href=assets/imgs/favicon.svg>
+    <link rel=stylesheet href=assets/css/style.css>
+    <link rel=stylesheet href=assets/css/widgets.css>
+    <link rel=stylesheet href=assets/css/color.css>
+    <link rel=stylesheet href=assets/css/responsive.css>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755"
+        crossorigin=anonymous></script><!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-V6HH2RKGTW"></script>
+    <script> window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'G-V6HH2RKGTW');</script>
+</head>
+
+<body>
+    <div id=preloader-active>
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class=text-center><img class="jump mb-50" src=assets/imgs/loading.svg alt>
+                    <h6>Now Loading</h6>
+                    <div class=loader>
+                        <div class="bar bar1"></div>
+                        <div class="bar bar2"></div>
+                        <div class="bar bar3"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class=main-wrap>
+        <aside id=sidebar-wrapper class="custom-scrollbar offcanvas-sidebar position-right"><button
+                class=off-canvas-close><i class=ti-close></i></button>
+            <div class=sidebar-inner>
+                <div class="siderbar-widget mb-50 mt-30">
+                    <form action=# method=get class="search-form position-relative"><input type=text class=search_field
+                            placeholder=Search value name=s><span class=search-icon><i
+                                class="ti-search mr-5"></i></span></form>
+                </div>
+                <div class="sidebar-widget mb-50">
+                    <div class="widget-header mb-30">
+                        <h5 class=widget-title>Top <span>Trending</span></h5>
+                    </div>
+                    <div class=post-aside-style-2>
+                        <ul class=list-post>
+                            <li class="mb-30 wow fadeIn animated">
+                                <div class=d-flex>
+                                    <div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale"><a
+                                            class=color-white href=#><img src=assets/imgs/thumbnail-2.jpg alt></a></div>
+                                    <div class="post-content media-body">
+                                        <h6 class="post-title mb-10 text-limit-2-row"><a href=#>Vancouver woman finds
+                                                pictures and videos of herself online</a></h6>
+                                        <div
+                                            class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+                                            <span class=post-by>By <a href=#>K. Marry</a></span><span class=post-on>4m
+                                                ago</span></div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="mb-30 wow fadeIn animated">
+                                <div class=d-flex>
+                                    <div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale"><a
+                                            class=color-white href=#><img src=assets/imgs/thumbnail-3.jpg alt></a></div>
+                                    <div class="post-content media-body">
+                                        <h6 class="post-title mb-10 text-limit-2-row"><a href=#>4 Things Emotionally
+                                                Intelligent People Don’t Do</a></h6>
+                                        <div
+                                            class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+                                            <span class=post-by>By <a href=#>Mr. John</a></span><span class=post-on>3h
+                                                ago</span></div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="mb-30 wow fadeIn animated">
+                                <div class=d-flex>
+                                    <div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale"><a
+                                            class=color-white href=#><img src=assets/imgs/thumbnail-5.jpg alt></a></div>
+                                    <div class="post-content media-body">
+                                        <h6 class="post-title mb-10 text-limit-2-row"><a href=#>Reflections from a Token
+                                                Black Friend</a></h6>
+                                        <div
+                                            class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+                                            <span class=post-by>By <a href=#>Kenedy</a></span><span class=post-on>4h
+                                                ago</span></div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="mb-30 wow fadeIn animated">
+                                <div class=d-flex>
+                                    <div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale"><a
+                                            class=color-white href=#><img src=assets/imgs/thumbnail-7.jpg alt></a></div>
+                                    <div class="post-content media-body">
+                                        <h6 class="post-title mb-10 text-limit-2-row"><a href=#>How to Identify a Smart
+                                                Person in 3 Minutes</a></h6>
+                                        <div
+                                            class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+                                            <span class=post-by>By <a href=#>Steven</a></span><span class=post-on>5h
+                                                ago</span></div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="wow fadeIn animated">
+                                <div class=d-flex>
+                                    <div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale"><a
+                                            class=color-white href=#><img src=assets/imgs/thumbnail-8.jpg alt></a></div>
+                                    <div class="post-content media-body">
+                                        <h6 class="post-title mb-10 text-limit-2-row"><a href=#>Blackface Minstrel Songs
+                                                Don’t Belong in Children’s Music Class</a></h6>
+                                        <div
+                                            class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+                                            <span class=post-by>By <a href=#>J.Smith</a></span><span class=post-on>5h30
+                                                ago</span></div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="sidebar-widget widget_tag_cloud mb-50">
+                    <div class="widget-header tags-close mb-20">
+                        <h5 class="widget-title mt-5">Tags Cloud</h5>
+                    </div>
+                    <div class=tagcloud><a href=#>Beauty</a><a href=#>Book</a><a href=#>Design</a><a
+                            href=#>Fashion</a><a href=#>Lifestyle</a><a href=#>Travel</a><a href=#>Science</a><a
+                            href=#>Health</a><a href=#>Sports</a><a href=#>Arts</a><a href=#>Books</a><a
+                            href=#>Style</a></div>
+                </div>
+                <div class="sidebar-widget widget-ads mb-30">
+                    <div class="widget-header tags-close mb-20">
+                        <h5 class="widget-title mt-5">Your Ads Here</h5>
+                    </div><a href=assets/imgs/news-1.jpg class=play-video data-animate=zoomIn data-duration=1.5s
+                        data-delay=0.1s><img class=border-radius-10 src=assets/imgs/ads-1.jpg alt></a>
+                </div>
+            </div>
+        </aside>
+        <?php include('header.php') ?>
+        <main class=position-relative>
+            <div class=container>
+                <div class="entry-header entry-header-1 mb-30 mt-50">
+                    <div class="entry-meta meta-0 font-small mb-30"><a href=#><span
+                                class="post-cat bg-success color-white">Global News</span></a></div>
+                    <h1 class="post-title mb-30">Anant Ambani and Radhika Merchant's Wedding Arrival of Justin Bieber in Mumbai</h1>
+                    <div class="entry-meta meta-1 font-x-small color-grey text-uppercase"><span class=post-by>By
+                            Newsguru Editors</span><span class=post-on>3:46 pm
+                            Friday, 5 July 2024</span><span
+                            class=time-reading>12 mins read</span>
+                      
+                    </div>
+                </div>
+                <div class="row mb-50">
+                    <div class="col-lg-8 col-md-12">
+                        <div class="single-social-share single-sidebar-share mt-30">
+                            <ul>
+                                <li><a class="social-icon facebook-icon text-xs-center" target=_blank href=#><i
+                                            class=ti-facebook></i></a></li>
+                                <li><a class="social-icon twitter-icon text-xs-center" target=_blank href=#><i
+                                            class=ti-twitter-alt></i></a></li>
+                                <li><a class="social-icon pinterest-icon text-xs-center" target=_blank href=#><i
+                                            class=ti-pinterest></i></a></li>
+                                <li><a class="social-icon instagram-icon text-xs-center" target=_blank href=#><i
+                                            class=ti-instagram></i></a></li>
+                                <li><a class="social-icon linkedin-icon text-xs-center" target=_blank href=#><i
+                                            class=ti-linkedin></i></a></li>
+                                <li><a class="social-icon email-icon text-xs-center" target=_blank href=#><i
+                                            class=ti-email></i></a></li>
+                            </ul>
+                        </div>
+                        <div class="bt-1 border-color-1 mb-30"></div>
+                        <figure class="single-thumnail mb-30"><img src=assets/imgs/news/beiber.JPG alt=blog
+                                width=100%>
+                            <div class="credit mt-15 font-small color-grey"><i
+                                    class="ti-credit-card mr-5"></i><span>Image credit: additional resources.</span>
+                            </div>
+                        </figure>
+                        <div class=single-excerpt>
+                            <p>Mumbai: After the performances of Rihanna and Katy Perry at Anant Ambani and Radhika Merchant's pre-wedding events, Canadian singer-songwriter Justin Bieber appears to be the next in line to enthrall guests at the highly anticipated wedding.
+</p>
+<p>
+Early on Friday morning, the Canadian pop artist touched down at the Mumbai airport accompanied by his staff.
+</p>
+<p>
+Bieber was spotted getting into a car and driving out of the airport while wearing a red bucket hat, blue baggy leggings, and a pink hoodie.
+</p>
+<p>According to reports, Justin Bieber, who is well-known for hits like "Baby," "Sorry," and "Never Say Never," among others, will be performing during Anant and Radhika's sangeet ceremony.
+</p>
+<p>
+On Friday, July 5, Anant and Radhika's sangeet celebrations are planned.
+On July 12, the couple will exchange promises at Mumbai's Jio World Convention Centre.
+</p>
+<p>2017 saw Beiber's debut performance in India. The singer's poor health forced the cancellation of the tour, which was scheduled to bring him back to the United States in 2022.
+</p>
+<p>
+On July 2, at Swami Vivekanand Vidyamandir in Palghar, Mukesh and Nita Ambani arranged a mass wedding for the impoverished as part of their wedding celebrations.
+</p>
+<p>
+The Ambanis arranged a grandiose Mameru ceremony on July 3. In Gujarati wedding customs, the bride's maternal uncle, or "mama," pays her a visit and presents her with gifts and candies.
+</p>
+<p>The wedding celebrations are organized with great care, following the ancient Hindu Vedic traditions. The wedding, or Shubh Vivah, will kick off the main rituals on Friday, July 12. Based on reports, guests are urged to dress in traditional Indian garb to fully embrace the spirit of the occasion.
+</p>
+<p>
+Shubh Aashirwad, on Saturday, July 13, will bring the festivities to a close. The wedding celebration, or Mangal Utsav, is the last event and is set for Sunday, July 14. It is requested that guests dress in "Indian chic" for this special event.
+</p>
+<p>Radhika Merchant, the daughter of entrepreneur Shaila Merchant and Encore Healthcare CEO Viren Merchant, is expected to become the first of two well-known industrialist families to marry into the Ambani family.
+
+A star-studded guest list from all over the world attended the couple's pre-wedding celebrations in Jamnagar earlier this year.
+</p>
+<p>
+Heads of state, business executives, and Bollywood and Hollywood celebrities graced the occasion.
+</p>
+<p>Ivanka Trump, Microsoft co-founder Bill Gates, and Meta founder Mark Zuckerberg and his wife Priscilla Chan were among the esteemed visitors.
+</p>
+<p>
+Pop sensation Rihanna made her debut in India with an incredible performance that was the highlight of the pre-wedding festivities.
+</p>
+<p>
+World-famous illusionist David Blaine was also a part of the three-day event, captivating guests with his amazing accomplishments.
+</p>
+<p>
+The audience was captivated by actor-singer Diljit Dosanjh's special presence during the sangeet performances, which included Bollywood celebrities and their families. 
+</p>
+                        </div>
+                        <?php include('sociallink.php') ?>
+                        <?php include('relatedpost.php') ?>
+                    </div>
+                    <?php include('rightside.php') ?>
+                </div>
+            </div>
+        </main>
+        <?php include('footer.php') ?>
+    </div>
+    <div class=dark-mark></div>
+    <script src=assets/js/vendor/modernizr-3.6.0.min.js></script>
+    <script src=assets/js/vendor/jquery-3.6.0.min.js></script>
+    <script src=assets/js/vendor/popper.min.js></script>
+    <script src=assets/js/vendor/bootstrap.min.js></script>
+    <script src=assets/js/vendor/jquery.slicknav.js></script>
+    <script src=assets/js/vendor/owl.carousel.min.js></script>
+    <script src=assets/js/vendor/slick.min.js></script>
+    <script src=assets/js/vendor/wow.min.js></script>
+    <script src=assets/js/vendor/animated.headline.js></script>
+    <script src=assets/js/vendor/jquery.magnific-popup.js></script>
+    <script src=assets/js/vendor/jquery.ticker.js></script>
+    <script src=assets/js/vendor/jquery.vticker-min.js></script>
+    <script src=assets/js/vendor/jquery.scrollUp.min.js></script>
+    <script src=assets/js/vendor/jquery.nice-select.min.js></script>
+    <script src=assets/js/vendor/jquery.sticky.js></script>
+    <script src=assets/js/vendor/perfect-scrollbar.js></script>
+    <script src=assets/js/vendor/waypoints.min.js></script>
+    <script src=assets/js/vendor/jquery.counterup.min.js></script>
+    <script src=assets/js/vendor/jquery.theia.sticky.js></script>
+    <script src=../../ionicons%405.0.0/dist/ionicons.js></script>
+    <script src=assets/js/main.js></script>
+</body>
+
+</html>
