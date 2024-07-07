e727b953469768c54377bc80c9590bd9a7a21f77
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-07-07 08:20:54 +0530

    commit updated

--- current-affairs/index.php
+++ current-affairs/index.php
@@ -7,6 +7,7 @@ $dbname = "coder";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+$conn->set_charset("utf8");
 
 // Check connection
 if ($conn->connect_error) {
