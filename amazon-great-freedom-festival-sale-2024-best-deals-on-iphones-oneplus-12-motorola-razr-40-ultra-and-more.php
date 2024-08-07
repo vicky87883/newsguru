685061367acfb25148161a0128873c51f9c490ad
vikram <kumarv38078@gmail.com>
2024-08-07 22:26:32 +0530

    commit updated

---
+++ amazon-great-freedom-festival-sale-2024-best-deals-on-iphones-oneplus-12-motorola-razr-40-ultra-and-more.php
@@ -0,0 +1,271 @@
+<?php
+require_once('dbcon.php');
+$query = "SELECT * FROM `frontload`   ORDER BY `id` DESC;";
+$query2 = "SELECT * FROM `mostpopular`   ORDER BY `id` DESC;";
+$query3 = "SELECT * FROM `dontmiss`   ORDER BY `id` DESC;";
+$query4 = "SELECT * FROM `toptrending`   ORDER BY `id` DESC;";
+$result = mysqli_query($con,$query);
+$result2 = mysqli_query($con,$query2);
+$result3 = mysqli_query($con,$query3);
+$result4 = mysqli_query($con,$query4);
+?>
+<!DOCTYPE html>
+<html class=no-js lang=en>
+<head>
+<meta charset=utf-8>
+<meta http-equiv=x-ua-compatible content="ie=edge">
+<title>Amazon's Great Freedom Festival sale 2024: The best offers on Motorola Razr 40 Ultra, OnePlus 12, iPhones, and more</title>
+<meta name=description content="Amazon's Great Freedom Festival sale 2024: The best offers on Motorola Razr 40 Ultra, OnePlus 12, iPhones, and more">
+<meta name=viewport content="width=device-width, initial-scale=1">
+<link rel=manifest href=site.webmanifest>
+<link rel="shortcut icon" type=image/x-icon href=assets/imgs/favicon.svg>
+<link rel=stylesheet href=assets/css/style.css>
+<link rel=stylesheet href=assets/css/widgets.css>
+<link rel=stylesheet href=assets/css/color.css>
+<link rel=stylesheet href=assets/css/responsive.css>
+	<link rel=canonical href=https://www.newsguru.live/amid-row-with-india.php />
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V6HH2RKGTW"></script>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V6HH2RKGTW');
+</script>
+</head>
+<body>
+<div id=preloader-active>
+<div class="preloader d-flex align-items-center justify-content-center">
+<div class="preloader-inner position-relative">
+<div class=text-center>
+<img class="jump mb-50" src=assets/imgs/loading.svg alt>
+<h6>Now Loading</h6>
+<div class=loader>
+<div class="bar bar1"></div>
+<div class="bar bar2"></div>
+<div class="bar bar3"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class=main-wrap>
+<aside id=sidebar-wrapper class="custom-scrollbar offcanvas-sidebar position-right">
+<button class=off-canvas-close><i class=ti-close></i></button>
+<div class=sidebar-inner>
+<div class="siderbar-widget mb-50 mt-30">
+<form action=# method=get class="search-form position-relative">
+<input type=text class=search_field placeholder=Search value name=s>
+<span class=search-icon><i class="ti-search mr-5"></i></span>
+</form>
+</div>
+<div class="sidebar-widget mb-50">
+<div class="widget-header mb-30">
+<h5 class=widget-title>Top <span>Trending</span></h5>
+</div>
+<div class=post-aside-style-2>
+<ul class=list-post>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-2.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Vancouver woman finds pictures and videos of herself online</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>K. Marry</a></span>
+<span class=post-on>4m ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-3.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>4 Things Emotionally Intelligent People Don’t Do</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Mr. John</a></span>
+<span class=post-on>3h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-5.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Reflections from a Token Black Friend</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Kenedy</a></span>
+<span class=post-on>4h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-7.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>How to Identify a Smart Person in 3 Minutes</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Steven</a></span>
+<span class=post-on>5h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-8.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Blackface Minstrel Songs Don’t Belong in Children’s Music Class</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>J.Smith</a></span>
+<span class=post-on>5h30 ago</span>
+</div>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<div class="sidebar-widget widget_tag_cloud mb-50">
+<div class="widget-header tags-close mb-20">
+<h5 class="widget-title mt-5">Tags Cloud</h5>
+</div>
+<div class=tagcloud>
+<a href=#>Beauty</a>
+<a href=#>Book</a>
+<a href=#>Design</a>
+<a href=#>Fashion</a>
+<a href=#>Lifestyle</a>
+<a href=#>Travel</a>
+<a href=#>Science</a>
+<a href=#>Health</a>
+<a href=#>Sports</a>
+<a href=#>Arts</a>
+<a href=#>Books</a>
+<a href=#>Style</a>
+</div>
+</div>
+</div>
+</aside>
+<?php include('header.php') ?>
+<main class=position-relative>
+<div class=container>
+<div class="entry-header entry-header-1 mb-30 mt-50">
+<div class="entry-meta meta-0 font-small mb-30"><a href=#><span class="post-cat bg-success color-white">Global News</span></a></div>
+<h1 class="post-title mb-30">
+Amazon's Great Freedom Festival sale 2024: The best offers on Motorola Razr 40 Ultra, OnePlus 12, iPhones, and more
+</h1>
+<div class="entry-meta meta-1 font-x-small color-grey text-uppercase">
+<span class=post-by>By Newsguru Editors</span>
+<span class=post-on>10:13 pm
+Wednesday, 7 August 2024</span>
+<span class=time-reading>5 mins read</span>
+</div>
+</div>
+<div class="row mb-50">
+<div class="col-lg-8 col-md-12">
+<div class="single-social-share single-sidebar-share mt-30">
+<ul>
+<li><a class="social-icon facebook-icon text-xs-center" target=_blank href=#><i class=ti-facebook></i></a></li>
+<li><a class="social-icon twitter-icon text-xs-center" target=_blank href=#><i class=ti-twitter-alt></i></a></li>
+<li><a class="social-icon pinterest-icon text-xs-center" target=_blank href=#><i class=ti-pinterest></i></a></li>
+<li><a class="social-icon instagram-icon text-xs-center" target=_blank href=#><i class=ti-instagram></i></a></li>
+<li><a class="social-icon linkedin-icon text-xs-center" target=_blank href=#><i class=ti-linkedin></i></a></li>
+<li><a class="social-icon email-icon text-xs-center" target=_blank href=#><i class=ti-email></i></a></li>
+</ul>
+</div>
+<div class="bt-1 border-color-1 mb-30"></div>
+<figure class="single-thumnail mb-30">
+<img src=assets/imgs/news/amazon.png alt>
+<div class="credit mt-15 font-small color-grey">
+<i class="ti-credit-card mr-5"></i><span>Image credit: additional resources.</span>
+</div>
+</figure>
+<div class=single-excerpt>
+<p><mark>During the Amazon sale, buyers will get an instant discount of 10 per cent on SBI Bank credit cards.</mark></p>
+<p>In addition to Flipkart's Flagship Freedom sale, Amazon is presently holding the Great Freedom Festival sale in India. The last day of the Amazon offer is August 11. Customers purchasing SBI Bank credit cards will receive an instant 10% discount. Customers who use Amazon Pay UPI will also receive Rs 50 cashback on minimum orders of Rs 750.
+</p>
+<div><strong>Best smartphone discounts during the Great Freedom Festival sale on Amazon</strong></div>
+
+<p>These are the best smartphone offers from the current Amazon sale.</p>
+
+<div><strong>The Apple iPhone 13</strong></div> 
+
+<p>The current price of the Apple iPhone 13 is Rs 49,190. With SBI Bank credit cards, buyers would also receive an instant 1,000 discount, bringing the total cost down to Rs 48,190. The color options for it contain Product (Red), Midnight, Starlight, and Pink.</p>
+
+<div><strong>The Apple iPhone 14</strong></div> 
+
+<p>Currently, Amazon is offering the iPhone 14 for Rs 60,900 during their sale. Additionally, buyers using SBI Bank credit cards would receive an instant discount of Rs 1,000. Notably, the iPhone had a starting price of Rs 79,900 when it was introduced in India. </p>
+<div><strong>OnePlus 12 5G</strong></div>
+
+<p>The OnePlus 12 5G, which retails for Rs 64,999, is currently on sale for Rs 59,999 on Amazon. On SBI Bank credit cards, buyers would receive an instant discount of Rs 6,000. As a result, the cost will drop to Rs 53,999. Glacial White, Silky Black, and Flowy Emerald are the color options that are offered.</p>
+
+<div><strong>Honor 200 5G</strong></div>
+
+<p>During the Great Freedom Festival sale, Amazon is offering the Honor 200 5G for Rs 33,998. On all bank credit cards, customers will receive an instant discount of Rs 3,000 and an Amazon coupon worth Rs 1,000. The final price after the discounts is going to be Rs 29,998. Moonlight White and Black color variants are available for purchase.  
+</p>
+<div><strong>The Razr 40 Ultra from Motorola<strong></div>
+
+<p>Originally priced at Rs 89,999, the Motorola Razr 40 Ultra is currently available on Amazon for Rs 46,794. SBI Bank credit cards additionally come with a 1,000 rupee rebate. There are three color options for the foldable flip phone: Glacial Blue, Infinite Black, and Pink.</p>
+
+<p>Notably, the Flipkart Flagship sale is currently taking place in India. Here, you can save money on a variety of smartphones, including the Xiaomi 14 Civi, Realme 13 Pro+, Oppo K12X 5G, and Nothing Phone (2a) Plus.
+</p>
+</div>
+<?php include('sociallink.php') ?>
+	<?php include('relatedpost.php') ?>
+</div>
+	<?php include('rightside.php') ?>
+</div>
+
+</div>
+</main>
+<?php include('footer.php') ?>
+</div>
+<div class=dark-mark></div>
+<script src=assets/js/vendor/modernizr-3.6.0.min.js></script>
+<script src=assets/js/vendor/jquery-3.6.0.min.js></script>
+<script src=assets/js/vendor/popper.min.js></script>
+<script src=assets/js/vendor/bootstrap.min.js></script>
+<script src=assets/js/vendor/jquery.slicknav.js></script>
+<script src=assets/js/vendor/owl.carousel.min.js></script>
+<script src=assets/js/vendor/slick.min.js></script>
+<script src=assets/js/vendor/wow.min.js></script>
+<script src=assets/js/vendor/animated.headline.js></script>
+<script src=assets/js/vendor/jquery.magnific-popup.js></script>
+<script src=assets/js/vendor/jquery.ticker.js></script>
+<script src=assets/js/vendor/jquery.vticker-min.js></script>
+<script src=assets/js/vendor/jquery.scrollUp.min.js></script>
+<script src=assets/js/vendor/jquery.nice-select.min.js></script>
+<script src=assets/js/vendor/jquery.sticky.js></script>
+<script src=assets/js/vendor/perfect-scrollbar.js></script>
+<script src=assets/js/vendor/waypoints.min.js></script>
+<script src=assets/js/vendor/jquery.counterup.min.js></script>
+<script src=assets/js/vendor/jquery.theia.sticky.js></script>
+<script src=../../ionicons%405.0.0/dist/ionicons.js></script>
+<script src=assets/js/main.js></script>
+</body>
+</html>
