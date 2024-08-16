8019a74f69882d70ba6a967b3f1a2ea75d2195e1
Vikram <vikraminps@gmail.com>
2024-08-16 09:56:56 +0530

    commit updated

---
+++ saudi-crown-prince-said-he-fears-assassination-over-israel-normalization.php
@@ -0,0 +1,282 @@
+<?php
+require_once('dbcon.php');
+$query = "SELECT * FROM `frontload`   ORDER BY `id` DESC;";
+$query2 = "SELECT * FROM `mostpopular`   ORDER BY `id` DESC;";
+$query3 = "SELECT * FROM `dontmiss`   ORDER BY `id` DESC;";
+$query4 = "SELECT * FROM `toptrending`   ORDER BY `id` DESC;";
+$result = mysqli_query($con,$query);
+$result2 = mysqli_query($con,$query2);
+$result3 = mysqli_query($con,$query3);
+$result4 = mysqli_query($con,$query4);
+?><!DOCTYPE html>
+<html class=no-js lang=en>
+<head>
+<meta charset=utf-8>
+<meta http-equiv=x-ua-compatible content="ie=edge">
+<title>Saudi Crown Prince expressed his fear of being assassinated because of Israel's normalization. | Latest News - Newsguru.live</title>
+<meta name=news_keywords content="Saudi Crown Prince expressed his fear of being assassinated because of Israel's normalization." itemprop="keywords"/>
+<meta name=description content="Saudi Crown Prince expressed his fear of being assassinated because of Israel's normalization." itemprop="description"/>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<link rel=canonical href=https://www.newsguru.live/saudi-crown-prince-said-he-fears-assassination-over-israel-normalization.php />
+<link rel="shortcut icon" type=image/x-icon href=assets/imgs/favicon.svg>
+<link rel=stylesheet href=assets/css/style.css>
+<link rel=stylesheet href=assets/css/widgets.css>
+<link rel=stylesheet href=assets/css/color.css>
+<link rel=stylesheet href=assets/css/responsive.css>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V6HH2RKGTW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V6HH2RKGTW');
+</script>
+</head>
+<body>
+<div id=preloader-active>
+<div class="preloader d-flex align-items-center justify-content-center">
+<div class="preloader-inner position-relative">
+<div class=text-center>
+<img class="jump mb-50" src=assets/imgs/loading.svg alt>
+<h6>Now Loading</h6>
+<div class=loader>
+<div class="bar bar1"></div>
+<div class="bar bar2"></div>
+<div class="bar bar3"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class=main-wrap>
+<aside id=sidebar-wrapper class="custom-scrollbar offcanvas-sidebar position-right">
+<button class=off-canvas-close><i class=ti-close></i></button>
+<div class=sidebar-inner>
+<div class="siderbar-widget mb-50 mt-30">
+<form action=# method=get class="search-form position-relative">
+<input type=text class=search_field placeholder=Search value name=s>
+<span class=search-icon><i class="ti-search mr-5"></i></span>
+</form>
+</div>
+<div class="sidebar-widget mb-50">
+<div class="widget-header mb-30">
+<h5 class=widget-title>Top <span>Trending</span></h5>
+</div>
+<div class=post-aside-style-2>
+<ul class=list-post>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-2.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Vancouver woman finds pictures and videos of herself online</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>K. Marry</a></span>
+<span class=post-on>4m ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-3.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>4 Things Emotionally Intelligent People Don’t Do</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Mr. John</a></span>
+<span class=post-on>3h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-5.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Reflections from a Token Black Friend</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Kenedy</a></span>
+<span class=post-on>4h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="mb-30 wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-7.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>How to Identify a Smart Person in 3 Minutes</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>Steven</a></span>
+<span class=post-on>5h ago</span>
+</div>
+</div>
+</div>
+</li>
+<li class="wow fadeIn animated">
+<div class=d-flex>
+<div class="post-thumb d-flex mr-15 border-radius-5 img-hover-scale">
+<a class=color-white href=#>
+<img src=assets/imgs/thumbnail-8.jpg alt>
+</a>
+</div>
+<div class="post-content media-body">
+<h6 class="post-title mb-10 text-limit-2-row"><a href=#>Blackface Minstrel Songs Don’t Belong in Children’s Music Class</a></h6>
+<div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase">
+<span class=post-by>By <a href=#>J.Smith</a></span>
+<span class=post-on>5h30 ago</span>
+</div>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<div class="sidebar-widget widget_tag_cloud mb-50">
+<div class="widget-header tags-close mb-20">
+<h5 class="widget-title mt-5">Tags Cloud</h5>
+</div>
+<div class=tagcloud>
+<a href=#>Beauty</a>
+<a href=#>Book</a>
+<a href=#>Design</a>
+<a href=#>Fashion</a>
+<a href=#>Lifestyle</a>
+<a href=#>Travel</a>
+<a href=#>Science</a>
+<a href=#>Health</a>
+<a href=#>Sports</a>
+<a href=#>Arts</a>
+<a href=#>Books</a>
+<a href=#>Style</a>
+</div>
+</div>
+<div class="sidebar-widget widget-ads mb-30">
+<div class="widget-header tags-close mb-20">
+<h5 class="widget-title mt-5">Your Ads Here</h5>
+</div>
+<a href=assets/imgs/news-1.jpg class=play-video data-animate=zoomIn data-duration=1.5s data-delay=0.1s>
+<img class=border-radius-10 src=assets/imgs/ads-1.jpg alt>
+</a>
+</div>
+</div>
+</aside>
+<?php include('header.php') ?>
+<main class=position-relative>
+<div class=container>
+<div class="entry-header entry-header-1 mb-30 mt-50">
+<div class="entry-meta meta-0 font-small mb-30"><a href=#><span class="post-cat bg-success color-white">Global News</span></a></div>
+<h1 class="post-title mb-30">
+Saudi Crown Prince expressed his fear of being assassinated because of Israel's normalization.
+</h1>
+<div class="entry-meta meta-1 font-x-small color-grey text-uppercase">
+<span class=post-by>By <a href=#>Newsguru Editors</a></span>
+<span class=post-on>9:24 am
+Friday, 16 August 2024</span>
+<span class=time-reading>5 mins read</span>
+</div>
+</div>
+<div class="row mb-50">
+<div class="col-lg-8 col-md-12">
+<div class="single-social-share single-sidebar-share mt-30">
+<ul>
+<li><a class="social-icon facebook-icon text-xs-center" target=_blank href=#><i class=ti-facebook></i></a></li>
+<li><a class="social-icon twitter-icon text-xs-center" target=_blank href=#><i class=ti-twitter-alt></i></a></li>
+<li><a class="social-icon pinterest-icon text-xs-center" target=_blank href=#><i class=ti-pinterest></i></a></li>
+<li><a class="social-icon instagram-icon text-xs-center" target=_blank href=#><i class=ti-instagram></i></a></li>
+<li><a class="social-icon linkedin-icon text-xs-center" target=_blank href=#><i class=ti-linkedin></i></a></li>
+<li><a class="social-icon email-icon text-xs-center" target=_blank href=#><i class=ti-email></i></a></li>
+</ul>
+</div>
+<div class="bt-1 border-color-1 mb-30"></div>
+<figure class="single-thumnail mb-30">
+<img src=assets/imgs/news/crown.PNG alt=blog width=100%>
+<div class="credit mt-15 font-small color-grey">
+<i class="ti-credit-card mr-5"></i><span>Image credit: additional resources.</span>
+</div>
+</figure>
+<div class=single-excerpt>
+	<div><strong>In discussions with US politicians, Mohammed bin Salman cited the late peacemaker Anwar Sadat to emphasize his own personal risk, according to a Newsweek story.</strong></div>
+    <p>According to reports, Saudi Crown Prince Mohammed bin Salman revealed to US senators that he fears being killed because he wants to normalize relations with Israel.
+</p>
+<p>
+The crown prince stated that he still plans to proceed with establishing links with the Jewish state, despite his concern that it could endanger his life, according to a Politico story that was published on Wednesday and quoted three sources who were briefed on the discussions.
+</p>
+<p>The killing of Egyptian President Anwar Sadat, who was shot dead by Islamist terrorists in 1981, two years after he signed a peace agreement with Israel, is said to have been mentioned at least once by Bin Salman, also known by his stage as MBS. After the historic peace agreement, MBS questioned his interlocutors about what the US had done to support Sadat.
+</p>
+<p>The possibility of murder was reportedly used by the crown prince to justify the inclusion of a "true path to a Palestinian state" in any normalization agreement between Riyadh and Jerusalem, something that the current Israeli government openly opposes.
+</p>
+<p>
+Politico cited a source familiar with the conversations as saying, "The way he put it was, 'Saudis care very deeply about this, and the street throughout the Middle East cares deeply about this, and my tenure as the keeper of the holy sites of Islam will not be secure if I don't address what is the most pressing issue of justice in our region.'"
+</p>
+<p>How recently the crown prince had discussed his possible assassination was unclear. According to sources within US Congress, there is little chance that an agreement on normalization between the US and Saudi Arabia will be reached before November's presidential election, since the Senate won't have enough time to approve the US-Saudi portion of the agreement before its recess.
+</p>
+<p>
+Politico's senior foreign affairs journalist Nahal Toosi revealed the discussions in a post and implied that the crown prince was "saying his life is in danger to push US officials to raise pressure on Israel to bend to a deal he likes."
+</p>
+<p>Toosi claimed that MBS was gambling when it came to forging diplomatic ties with Israel, even before to the Gaza War.
+</p>
+<p>
+Now that Saudi youth have been "galvanized by the first major conflict between Israelis and Palestinians that many of them have seen in their lives," she said, normalization could cost the crown prince their support.
+</p>
+<p>
+Prime Minister Benjamin Netanyahu has long desired rapprochement between Israel and Saudi Arabia. A future Palestinian state has, however, been consistently rejected by the premier, making any such agreement a challenging and intricate task.
+</p>
+<p>According to US President Joe Biden, Saudi Arabia desires to "fully recognize Israel" in return for Washington's security assurances and the construction of a nuclear reactor for civilian use.
+</p>
+<p>
+The White House announced on Monday that it has lifted a prohibition imposed in 2021 due to human rights concerns and had started selling offensive weapons to Saudi Arabia again. The restart was interpreted as a component of Washington's endeavors to solicit Riyadh's assistance in establishing a ceasefire in Gaza and thwarting an imminent Iranian assault on Israel.
+</p>
+<p>Normalization appeared to be imminent prior to the Gaza War. Just a few days before the conflict began on October 7, when thousands of terrorists headed by Hamas attacked southern Israel, killing almost 1,200 people and taking 251 hostages, two Israeli ministers paid remarkable public visits to the desert country.
+</p>
+</div>
+	<?php include('sociallink.php') ?>
+	<?php include('relatedpost.php') ?>
+</div>
+<?php include('rightside.php') ?></div>
+</div>
+	
+</main>
+<?php include('footer.php') ?>
+</div>
+<div class=dark-mark></div>
+<script src=assets/js/vendor/modernizr-3.6.0.min.js></script>
+<script src=assets/js/vendor/jquery-3.6.0.min.js></script>
+<script src=assets/js/vendor/popper.min.js></script>
+<script src=assets/js/vendor/bootstrap.min.js></script>
+<script src=assets/js/vendor/jquery.slicknav.js></script>
+<script src=assets/js/vendor/owl.carousel.min.js></script>
+<script src=assets/js/vendor/slick.min.js></script>
+<script src=assets/js/vendor/wow.min.js></script>
+<script src=assets/js/vendor/animated.headline.js></script>
+<script src=assets/js/vendor/jquery.magnific-popup.js></script>
+<script src=assets/js/vendor/jquery.ticker.js></script>
+<script src=assets/js/vendor/jquery.vticker-min.js></script>
+<script src=assets/js/vendor/jquery.scrollUp.min.js></script>
+<script src=assets/js/vendor/jquery.nice-select.min.js></script>
+<script src=assets/js/vendor/jquery.sticky.js></script>
+<script src=assets/js/vendor/perfect-scrollbar.js></script>
+<script src=assets/js/vendor/waypoints.min.js></script>
+<script src=assets/js/vendor/jquery.counterup.min.js></script>
+<script src=assets/js/vendor/jquery.theia.sticky.js></script>
+<script src=../../ionicons%405.0.0/dist/ionicons.js></script>
+<script src=assets/js/main.js></script>
+</body>
+</html>
