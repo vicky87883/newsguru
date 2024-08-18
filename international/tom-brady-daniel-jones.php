9acc22687cbe6192a6b15082f3a3c0d09caeaafc
Vikram <vikraminps@gmail.com>
2024-08-18 09:38:38 +0530

    commit updated

---
+++ international/tom-brady-daniel-jones.php
@@ -0,0 +1,207 @@
+<?php
+require_once('dbcon.php');
+$query = "SELECT * FROM `health`   ORDER BY `id` DESC;";
+$query2 = "SELECT * FROM `health`   ORDER BY `id` DESC;";
+$query3 = "SELECT * FROM `health`   ORDER BY `id` DESC;";
+$result = mysqli_query($con,$query);
+$result2 = mysqli_query($con,$query2);
+$result3 = mysqli_query($con,$query3);
+?><!DOCTYPE html>
+<html class=no-js lang=en>
+<head>
+<meta charset=utf-8>
+<meta http-equiv=x-ua-compatible content="ie=edge">
+<title>Tom Brady sparks controversy by criticizing Daniel Jones, the quarterback for the New York Giants</title>
+<meta name=news_keywords content="Tom Brady sparks controversy by criticizing Daniel Jones, the quarterback for the New York Giants" itemprop="keywords"/>
+<meta name=description content="Tom Brady sparks controversy by criticizing Daniel Jones, the quarterback for the New York Giants" itemprop="description"/>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<link rel=canonical href="https://www.newsguru.live/international/tom-brady-daniel-jones.php" />
+<link rel="shortcut icon" type=image/x-icon href=assets/imgs/favicon.svg>
+<link rel=stylesheet href=assets/css/style.css>
+<link rel=stylesheet href=assets/css/widgets.css>
+<link rel=stylesheet href=assets/css/color.css>
+<link rel=stylesheet href=assets/css/responsive.css>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V6HH2RKGTW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V6HH2RKGTW');
+</script>
+<style>
+        /* Style for article paragraphs */
+        .single-excerpt p {
+            font-family: 'Georgia', serif; /* Use a serif font for readability */
+            font-size: 1.1rem; /* Slightly increase the font size */
+            line-height: 1.6; /* Increase line height for better readability */
+            margin-bottom: 1em; /* Space out paragraphs */
+            color: #333; /* Use a dark color for good contrast */
+        }
+
+        /* Optional: Adjust for different screen sizes */
+        @media (max-width: 768px) {
+            .single-excerpt p {
+                font-size: 1rem; /* Slightly smaller on mobile devices */
+                line-height: 1.5; /* Adjust line height for smaller screens */
+            }
+        }
+    </style>
+</head>
+<body>
+<div id=preloader-active>
+<div class="preloader d-flex align-items-center justify-content-center">
+<div class="preloader-inner position-relative">
+<div class=text-center>
+<img class="jump mb-50" src=assets/imgs/loading.svg alt>
+<h6>Now Loading</h6>
+<div class=loader>
+<div class="bar bar1"></div>
+<div class="bar bar2"></div>
+<div class="bar bar3"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class=main-wrap>
+<?php include('header.php') ?>
+<main class=position-relative>
+<div class=container>
+<div class="entry-header entry-header-1 mb-30 mt-50">
+<div class="entry-meta meta-0 font-small mb-30"><a href=#><span class="post-cat bg-success color-white">Health News</span></a></div>
+<h1 class="post-title mb-30">
+Tom Brady sparks controversy by criticizing Daniel Jones, the quarterback for the New York Giants
+</h1>
+<div class="entry-meta meta-1 font-x-small color-grey text-uppercase">
+<span class=post-by>By Newsguru Editors</span>
+<span class=post-on>9:08 am
+Sunday, 18 august 2024</span>
+<span class=time-reading>5 mins read</span>
+</div>
+</div>
+<div class="row mb-50">
+<div class="col-lg-8 col-md-12">
+<div class="single-social-share single-sidebar-share mt-30">
+<ul>
+<li><a class="social-icon facebook-icon text-xs-center" target=_blank href=#><i class=ti-facebook></i></a></li>
+<li><a class="social-icon twitter-icon text-xs-center" target=_blank href=#><i class=ti-twitter-alt></i></a></li>
+<li><a class="social-icon pinterest-icon text-xs-center" target=_blank href=#><i class=ti-pinterest></i></a></li>
+<li><a class="social-icon instagram-icon text-xs-center" target=_blank href=#><i class=ti-instagram></i></a></li>
+<li><a class="social-icon linkedin-icon text-xs-center" target=_blank href=#><i class=ti-linkedin></i></a></li>
+<li><a class="social-icon email-icon text-xs-center" target=_blank href=#><i class=ti-email></i></a></li>
+</ul>
+</div>
+<div class="bt-1 border-color-1 mb-30"></div>
+<figure class="single-thumnail mb-30">
+<img src=assets/imgs/news/daniel.PNG alt=blog width=80%>
+<div class="credit mt-15 font-small color-grey">
+<i class="ti-credit-card mr-5"></i><span>Image credit: additional resources.</span>
+</div>
+</figure>
+<div class=single-excerpt>
+    <p>The crowd heckled the famed NFL quarterback Tom Brady at the NYC Fanaticsfest panel when he made a joke about local hero Daniel Jones of the New York Giants.
+    </p>
+    <ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+    <p>
+Fans wore jerseys honoring Brady's tenure with the Tampa Bay Buccaneers, the New England Patriots, and even his time as a college student at Michigan. As Brady discussed his opinions on criticizing other quarterbacks after hanging up his boots, the mood changed.
+</p>
+<p>Brady, who was well-known for his ability to be critical of himself while playing, was not afraid to share how he evaluated players' NFL performances.
+    </p>
+    <ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+    <p>
+"I had a lot of criticism for my NFL performance. And I have a lot of criticism for the way our team(s) performed in the NFL," he told reporters, notably those from Mirror U.S.
+</p>
+<p>But when Brady revealed the conundrum he expects to face in his broadcasting post regarding taming his sharp judgments, things really got interesting:
+    </p>
+    <ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+    <p>
+The performance standards I held myself to were extremely high. The hardest thing for me to deal with is realizing that something was awful when I saw it. I just can't say that on TV, like," he said frank.
+    </p>
+    <p>
+Tom Brady got into trouble when he talked about integrity in sports commentary. He revealed to moderator Stephen A. Smith that he withholds things because of his family:
+</p>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<p>"Well, I don't want to phrase it that way all the time because there are parents and other family members. I would walk to the sidelines after throwing a particularly poor interception and declare, "You are the worst quarterback in the world." How on earth are you going to make that throw? That was quite awful.
+</p>
+<p>But at the Brady fan-filled gathering, his attempt to highlight his point with Daniel Jones of the New York Giants didn't go as planned. When Brady said, "Let's say Daniel Jones throws an interception," the audience erupted in jeers, demonstrating their unwavering love for New York football despite the team's terrible five-year record under Jones.
+    </p>
+    <ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6958761602872755"
+     data-ad-slot="3917672390"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+    <p>
+Jones recorded 12,512 passing yards, 62 touchdowns, and 40 interceptions during his tenure with the Giants. As a result, he finished his career with an 85.2 career passer rating. Jones had an ACL injury during a game against the Las Vegas Raiders in the eighth week of the previous campaign, and is presently recovering.
+</p>
+<p>He sat out the Giants' first preseason clash with the Detroit Lions, but head coach Brian Daboll hinted that their star player is set to hit the turf in the last preseason match against the Houston Texans. The franchise's faith in Jones was also broadcast on TV during their offseason special "Hard Knocks," where General Manager Joe Schoen opted to stick with the QB over two-time Pro Bowl running back Saquon Barkley.</p>
+</div>
+	<?php include('../relatedpost.php') ?>
+</div>
+<?php include('../rightside.php') ?></div>
+</div>
+</main>
+<?php include('../footer.php') ?>
+</div>
+<div class=dark-mark></div>
+<script src=assets/js/vendor/modernizr-3.6.0.min.js></script>
+<script src=assets/js/vendor/jquery-3.6.0.min.js></script>
+<script src=assets/js/vendor/popper.min.js></script>
+<script src=assets/js/vendor/bootstrap.min.js></script>
+<script src=assets/js/vendor/jquery.slicknav.js></script>
+<script src=assets/js/vendor/owl.carousel.min.js></script>
+<script src=assets/js/vendor/slick.min.js></script>
+<script src=assets/js/vendor/wow.min.js></script>
+<script src=assets/js/vendor/animated.headline.js></script>
+<script src=assets/js/vendor/jquery.magnific-popup.js></script>
+<script src=assets/js/vendor/jquery.ticker.js></script>
+<script src=assets/js/vendor/jquery.vticker-min.js></script>
+<script src=assets/js/vendor/jquery.scrollUp.min.js></script>
+<script src=assets/js/vendor/jquery.nice-select.min.js></script>
+<script src=assets/js/vendor/jquery.sticky.js></script>
+<script src=assets/js/vendor/perfect-scrollbar.js></script>
+<script src=assets/js/vendor/waypoints.min.js></script>
+<script src=assets/js/vendor/jquery.counterup.min.js></script>
+<script src=assets/js/vendor/jquery.theia.sticky.js></script>
+<script src=../../ionicons%405.0.0/dist/ionicons.js></script>
+<script src=assets/js/main.js></script>
+</body>
+</html>
